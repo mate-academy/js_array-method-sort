b7a7739759ef44148ddf0e6476ad2b6b2a58c584
OleksandrHlybchenko <logista@meta.ua>
2019-06-22 01:04:02 +0300

    for review

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,22 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => (`${a}` < `${b}`) ? -1 : 1
+  ) {
+    let forSort = ``;
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          forSort = this[i];
+          this[i] = this[j];
+          this[j] = forSort;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
