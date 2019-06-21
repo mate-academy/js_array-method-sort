2c7fd0222fbaa941eeb90291f98839c11e557649
Iryna <iriskag@ukr.net>
2019-06-21 14:10:57 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,43 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let temp = '';
+    const swap = (first, second) => {
+      let newFirst = first;
+      let newSecond = second;
+      temp = first.toString();
+      newFirst = second.toString();
+      newSecond = temp;
+      return [newFirst, newSecond];
+    };
+
+    if (compareFunction !== undefined) {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          if (compareFunction(this[i].toString(), this[j].toString()) > 0) {
+            [this[i], this[j]] = swap(this[i], this[j]);
+          }
+        }
+        if (!isNaN(Number(this[i]))) {
+          this[i] = Number(this[i]);
+        }
+      }
+    }
+
+    if (compareFunction === undefined) {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          if (this[i].toString() > this[j].toString()) {
+            [this[i], this[j]] = swap(this[i], this[j]);
+          }
+        }
+
+        if (!isNaN(Number(this[i]))) {
+          this[i] = Number(this[i]);
+        }
+      }
+    }
+    return this;
   };
 }
 
