8299372782fb1769869427cbe46d9dcb5553767f
Пользователь <Sviatoslv>
2019-06-30 11:35:29 +0300

    Fixed defolt compare function logic

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -9,7 +9,15 @@ function applyCustomSort() {
     let temp = '';
 
     if (newCompareFunction === undefined) {
-      newCompareFunction = (a, b) => `${a}` >= `${b}` ? 0 : -1;
+      newCompareFunction = (a, b) => {
+        if (`${a}` > `${b}`) {
+          return 1;
+        } else if (`${a}` === `${b}`) {
+          return 0;
+        }
+
+        return -1;
+      };
     }
 
     for (let i = 0; i < this.length; i++) {
