2de04e5c586194071da6b021323fd2d0604b4c9b
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-08-21 08:41:43 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,40 @@
 /**
  * Implement method Sort
  */
+const unicodeSort = (a, b) => {
+  const strA = String(a);
+  const strB = String(b);
+
+  for (let i = 0; i < strA.length; i++) {
+    if (strB[i] === undefined) {
+      return 1;
+    } else if (strA[i] > strB[i]) {
+      return 1;
+    } else if (strA[i] < strB[i]) {
+      return -1;
+    }
+  }
+
+  return 0;
+};
+
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const testFunction = compareFunction === undefined
+      ? unicodeSort
+      : compareFunction;
+
+    for (let j = 0; j < this.length - 1; j++) {
+      for (let i = 0; i < this.length - 1; i++) {
+        if (testFunction(this[i], this[i + 1]) > 0) {
+          const tmp = this[i];
+          this[i] = this[i + 1];
+          this[i + 1] = tmp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
