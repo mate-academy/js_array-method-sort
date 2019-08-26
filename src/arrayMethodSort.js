18a2bb7a14d7816a2c94278d536ad4705be3ab65
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-08-26 18:54:26 +0300

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,27 @@
 /**
  * Implement method Sort
  */
+
+function compareElem(a, b) {
+  return a.toString() > b.toString() ? 1 : -1;
+}
+
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const sort
+      = compareFunction !== undefined ? compareFunction : compareElem;
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (sort(this[j], this[j + 1]) > 0) {
+          const swap = this[j];
+          this[j] = this[j + 1];
+          this[j + 1] = swap;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
