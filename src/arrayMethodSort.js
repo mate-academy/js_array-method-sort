a3064d37cc6d02539dccb20bedcf25580cff80e1
igaryok <igor.sobko@gmail.com>
2019-08-28 17:28:15 +0300

    changes according to review

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,19 +5,22 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    for (let i = 0, endI = this.length - 1; i < endI; i++) {
-      let wasSwap = false;
-      for (let j = 0, endJ = endI - i; j < endJ; j++) {
+    for (let i = 0, endArray = this.length - 1; i < endArray; i++) {
+      let isSwap = false;
+
+      for (let j = 0, endJ = endArray - i; j < endJ; j++) {
         if ((!compareFunction && (String(this[j]) > String(this[j + 1])))
         || (compareFunction && compareFunction(this[j], this[j + 1]) > 0)) {
-          const swap = this[j];
+          const cachedItem = this[j];
           this[j] = this[j + 1];
-          this[j + 1] = swap;
-          wasSwap = true;
+          this[j + 1] = cachedItem;
+          isSwap = true;
         }
       }
-      if (!wasSwap) { break; }
+
+      if (!isSwap) { break; }
     }
+
     return this;
   };
 }
