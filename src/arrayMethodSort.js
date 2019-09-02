7965ab68ed601ff7dba0d26595a8872d09f8812a
Rafler <amuronyk@gmail.com>
2019-09-02 10:48:30 +0300

    naming

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,16 +4,15 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    const sortFunction = compareFunction !== undefined
-      ? compareFunction
-      : (a, b) => (String(a) > String(b) ? 1 : -1);
+    const sortFunction = compareFunction
+      || ((a, b) => (String(a) > String(b) ? 1 : -1));
 
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (sortFunction(this[i], this[j]) > 0) {
-          const tmp = this[i];
+          const cachedValue = this[i];
           this[i] = this[j];
-          this[j] = tmp;
+          this[j] = cachedValue;
         }
       }
     }
