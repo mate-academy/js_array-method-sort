6483e59e76ef692e8962691540b67f9bd32767cd
Vlad Mishchenko <vladislav.a.mishchenko@gmail.com>
2019-11-20 19:28:34 +0200

    develop

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,7 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction =
-  (a, b) => (String(a) < String(b)) ? -1 : 1) {
+  (a, b) => (String(a) < String(b)) ? 0 : 1) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
