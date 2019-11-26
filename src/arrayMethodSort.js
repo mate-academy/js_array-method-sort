4c7ca60a720e336fad79535689039bfbcf002ba7
helmax-y <max_rad@ukr.net>
2019-11-26 19:27:21 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,31 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let temp;
+
+    if (!compareFunction) {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length - i - 1; j++) {
+          if (this[j].toString() > this[j + 1].toString()) {
+            temp = this[j];
+            this[j] = this[j + 1];
+            this[j + 1] = temp;
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length - i - 1; j++) {
+          if (+compareFunction(this[j], this[j + 1]) > 0) {
+            temp = this[j];
+            this[j] = this[j + 1];
+            this[j + 1] = temp;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
