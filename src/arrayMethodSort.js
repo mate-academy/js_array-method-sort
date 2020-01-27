58bd65f3b015ea22d68e4ae3a6a8066076e09f85
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-01-27 22:52:14 +0200

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,52 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (compareFunction === undefined) {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          for (let k = 0; k < String(this[i]).length; k++) {
+            if (String(this[i])[k] !== undefined
+            && String(this[j])[k] !== undefined
+            && String(this[i])[k] !== String(this[j])[k]) {
+              if (String(this[i])[k].charCodeAt(0)
+              > String(this[j])[k].charCodeAt(0)) {
+                const item = this[i];
+
+                this[i] = this[j];
+                this[j] = item;
+                break;
+              }
+            } else if (String(this[i])[k] === undefined
+            && String(this[j])[k] === undefined) {
+              if (this[i].length > this[j].length) {
+                const item = this[i];
+
+                this[i] = this[j];
+                this[j] = item;
+              }
+            }
+          }
+        }
+      }
+
+      return this;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          const item = this[i];
+
+          this[i] = this[j];
+          this[j] = item;
+        };
+      }
+    }
+
+    return this;
   };
 }
+
+applyCustomSort();
 
 module.exports = applyCustomSort;
