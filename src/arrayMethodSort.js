8831e7657567a51b13254fd671cf816df816bd26
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-01-29 02:06:53 +0200

    sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,31 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let tmp;
+
+    if (compareFunction === undefined) {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          if (this[i].toString() > this[j].toString()) {
+            tmp = this[i];
+            this[i] = this[j];
+            this[j] = tmp;
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          if (compareFunction(this[i], this[j]) > 0) {
+            tmp = this[i];
+            this[i] = this[j];
+            this[j] = tmp;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
