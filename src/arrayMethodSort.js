a68647fe7f4e8ab0acbb55d42c5db0f80e605333
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-01-29 12:53:02 +0200

    fix, added comments on github

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -9,24 +9,22 @@ function applyCustomSort() {
       for (let i = 0; i < this.length; i++) {
         for (let j = i + 1; j < this.length; j++) {
           for (let k = 0; k < String(this[i]).length; k++) {
-            if (String(this[i])[k] !== undefined
-            && String(this[j])[k] !== undefined
-            && String(this[i])[k] !== String(this[j])[k]) {
+            if (String(this[i])[k] !== String(this[j])[k]) {
               if (String(this[i])[k].charCodeAt(0)
               > String(this[j])[k].charCodeAt(0)) {
-                const item = this[i];
+                const previousItem = this[i];
 
                 this[i] = this[j];
-                this[j] = item;
+                this[j] = previousItem;
               }
               break;
             } else if (String(this[i])[k] === undefined
             || String(this[j])[k] === undefined) {
               if (String(this[i]).length > String(this[j]).length) {
-                const item = this[i];
+                const previousItem = this[i];
 
                 this[i] = this[j];
-                this[j] = item;
+                this[j] = previousItem;
               }
             }
           }
@@ -39,10 +37,10 @@ function applyCustomSort() {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
-          const item = this[i];
+          const previousItem = this[i];
 
           this[i] = this[j];
-          this[j] = item;
+          this[j] = previousItem;
         };
       }
     }
