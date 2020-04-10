e2640d05304d8cabcd63a88b3d91aeee41ffe2e5
Kira Marks <serafita22@gmail.com>
2020-04-10 17:47:47 +0300

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,16 +4,15 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = (prev, cur) =>
-    String(prev) > String(cur)) {
+  [].__proto__.sort2 = function(compareFunction = (prev, cur) => String(prev) > String(cur)) {
     for (let i = 0; i < this.length; i++) {
       for (let j = 1; j < this.length; j++) {
-        const current = this[i];
-        const previous = this[i - 1];
+        const current = this[j];
+        const previous = this[j - 1];
 
         if (compareFunction(previous, current) > 0) {
-          this[i - 1] = current;
-          this[i] = previous;
+          this[j - 1] = current;
+          this[j] = previous;
         }
       }
     }
