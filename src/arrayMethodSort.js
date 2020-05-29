b429885d9e52959fcef46b28bd225b58f299e982
solaryasha <solaryasha@gmail.com>
2020-05-29 22:28:53 +0300

    Add loop breaker

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,6 +6,8 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     for (let i = 0; i < this.length - 1; i++) {
+      let wasSwapped = false;
+
       for (let j = 0; j < this.length - (i + 1); j++) {
         const defaultMatch = !compareFunction
           && String(this[j]) > String(this[j + 1]);
@@ -15,12 +17,23 @@ function applyCustomSort() {
 
         if (defaultMatch || customMatch) {
           [this[j], this[j + 1]] = [this[j + 1], this[j]];
+          wasSwapped = true;
         }
+      }
+
+      if (!wasSwapped) {
+        break;
       }
     }
 
     return this;
   };
 };
+
+applyCustomSort();
+
+const sort = [3, 12, 2, 11];
+
+sort.sort2();
 
 module.exports = applyCustomSort;
