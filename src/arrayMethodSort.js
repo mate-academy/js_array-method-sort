859305090f1cc5364cb2120682d1dde0f0fb62d4
solaryasha <solaryasha@gmail.com>
2020-05-29 18:20:44 +0300

    Naive buble sort algorithm

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,27 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - (i + 1); j++) {
+        const defaultMatch = !compareFunction
+          && String(this[j]) > String(this[j + 1]);
+
+        const customMatch = compareFunction
+        && compareFunction(this[j], this[j + 1]) > 0;
+
+        if (defaultMatch || customMatch) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+        }
+      }
+    }
+
+    return this;
   };
-}
+};
+applyCustomSort();
+
+const source = [3, 12, 2, 11];
+
+source.sort2();
 
 module.exports = applyCustomSort;
