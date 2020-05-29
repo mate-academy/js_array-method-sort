6b5562f9bac9ff2488bef4555cf17c900d5d8829
aleksey-10 <alexcare1337@gmail.com>
2020-05-29 16:46:19 +0300

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,13 +6,11 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     for (let i = 0; i < this.length - 1; i++) {
-      const item = this[0];
-
       for (let j = 1; j < this.length; j++) {
         const previous = this[j - 1];
         const next = this[j];
 
-        if ((!compareFunction && item.toString() > next.toString())
+        if ((!compareFunction && previous.toString() > next.toString())
           || (compareFunction && compareFunction(previous, next) > 0)) {
           this[j] = previous;
           this[j - 1] = next;
