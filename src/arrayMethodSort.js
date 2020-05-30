b874eda5b39613e5b2a0ccc6ad6e6a77ff5aaf84
DimaDP <dimadp95@gmail.com>
2020-05-30 18:29:57 +0300

    dimadp__solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,35 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let temp;
+
+    if (compareFunction === undefined) {
+      for (let i = this.length - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+          if (this[j].toString() > this[j + 1].toString()) {
+            temp = this[j];
+
+            this[j] = this[j + 1];
+            this[j + 1] = temp;
+          }
+        }
+      }
+
+      return this;
+    }
+
+    for (let i = this.length - 1; i > 0; i--) {
+      for (let j = 0; j < i; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
