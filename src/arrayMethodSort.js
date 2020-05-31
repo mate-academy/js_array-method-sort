0605fe6fa6418c6e2177eec4fa1a58790ffe17c8
nazmeln <nazik100378@gmail.com>
2020-05-31 20:09:56 +0300

    solved

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,8 +1,11 @@
 'use strict';
 
 /**
+ *
  * Implement method Sort
+ *
  */
+
 function applyCustomSort() {
   [].__proto__.sort2 = function(
     compareFunction = (a, b) => a.toString() > b.toString()) {
@@ -10,7 +13,7 @@ function applyCustomSort() {
     let temp;
 
     for (let i = 0; i < this.length; i++) {
-      for (let j = i + 1; j < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
           temp = this[i];
           this[i] = this[j];
