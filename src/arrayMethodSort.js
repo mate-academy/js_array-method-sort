42048db4420e179ec0c19d41715df3dbb4d19b6a
DimaDP <dimadp95@gmail.com>
2020-05-31 22:47:45 +0300

    fix

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,29 +4,14 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction =
+  (x, y) => x.toString() > y.toString()) {
     let temp;
-
-    if (compareFunction === undefined) {
-      for (let i = this.length - 1; i > 0; i--) {
-        for (let j = 0; j < i; j++) {
-          if (this[j].toString() > this[j + 1].toString()) {
-            temp = this[j];
-
-            this[j] = this[j + 1];
-            this[j + 1] = temp;
-          }
-        }
-      }
-
-      return this;
-    }
 
     for (let i = this.length - 1; i > 0; i--) {
       for (let j = 0; j < i; j++) {
         if (compareFunction(this[j], this[j + 1]) > 0) {
           temp = this[j];
-
           this[j] = this[j + 1];
           this[j + 1] = temp;
         }
