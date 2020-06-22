f4bcff10ba12b745c494b159de7db2cf95b5b006
Alex Arabov <arabov@ukr.net>
2020-06-22 20:43:22 +0300

    Fixed after review

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,34 +4,26 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    if (compareFunction === undefined) {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = 0; j < this.length - 1 - i; j++) {
-          if (this[j + 1].toString() < this[j].toString()) {
-            const t = this[j + 1];
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => String(a) > String(b)
+  ) {
+    let swapped;
 
-            this[j + 1] = this[j];
-            this[j] = t;
-          }
+    do {
+      swapped = false;
+
+      for (let j = 0; j < this.length - 1; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          const t = this[j + 1];
+
+          this[j + 1] = this[j];
+          this[j] = t;
+          swapped = true;
         }
       }
+    } while (swapped);
 
-      return this;
-    } else {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = 0; j < this.length - 1 - i; j++) {
-          if (compareFunction(this[j].toString(), this[j + 1].toString()) > 0) {
-            const t = this[j + 1];
-
-            this[j + 1] = this[j];
-            this[j] = t;
-          }
-        }
-      }
-
-      return this;
-    }
+    return this;
   };
 }
 
