2783636e6450af2ed77efa885247e0148280af45
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-08-04 20:32:44 +0300

    Made changes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,17 +4,23 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    let variable;
+  [].__proto__.sort2 = function(compareFunction = (a, b) => {
+    let template;
 
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length; j++) {
         if (this[i].toString() < this[j].toString()) {
-          variable = this[i];
+          template = this[i];
           this[i] = this[j];
-          this[j] = variable;
+          this[j] = template;
         }
+      }
+    }
+  }) {
+    let variable;
 
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length; j++) {
         if (compareFunction !== undefined) {
           if (compareFunction(this[i], this[j]) < 0) {
             variable = this[i];
