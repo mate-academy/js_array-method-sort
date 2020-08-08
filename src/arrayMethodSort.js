6a0202c4a3ee481f8dbe904a90f05f02ae3bc34e
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-08 17:28:41 +0300

    +

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,29 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(callback = (a, b) => {
+    if (String(a) > String(b)) {
+      return 1;
+    } else if (String(a) < String(b)) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }) {
+    let wasTransposition = false;
+
+    for (let i = 1; i < this.length; i++) {
+      if (callback(this[i - 1], this[i]) > 0) {
+        [this[i], this[i - 1]] = [this[i - 1], this[i]];
+        wasTransposition = true;
+      }
+    }
+
+    if (wasTransposition) {
+      this.sort2(callback);
+    }
+
+    return this;
   };
 }
 
