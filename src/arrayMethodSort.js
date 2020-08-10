e3df6455974080a7bc3a6a6eac38bf6d6f8093e1
Alex <oivannikov>
2020-08-10 21:24:00 +0300

    fixed code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -30,18 +30,3 @@ function applyCustomSort() {
 }
 
 module.exports = applyCustomSort;
-
-// do {
-//   count = 0;
-
-//   for (let i = 1; i < this.length; i++) {
-//     const prev = this[i - 1];
-//     const current = this[i];
-
-//     if (compareFunction(prev, current) > 0) {
-//       count++;
-//       this[i - 1] = current;
-//       this[i] = prev;
-//     }
-//   };
-// } while (count > 0);
