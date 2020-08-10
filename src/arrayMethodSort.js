25ec7fc9447eb80bd8b271cb484e91721d721923
Alex <oivannikov>
2020-08-10 21:22:32 +0300

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -13,25 +13,35 @@ function applyCustomSort() {
       return 0;
     }
   }) {
-    let count;
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          const current = this[i];
+          const next = this[j];
 
-    do {
-      count = 0;
-
-      for (let i = 1; i < this.length; i++) {
-        const prev = this[i - 1];
-        const current = this[i];
-
-        if (compareFunction(prev, current) > 0) {
-          count++;
-          this[i - 1] = current;
-          this[i] = prev;
+          this[i] = next;
+          this[j] = current;
         }
-      };
-    } while (count > 0);
+      }
+    }
 
     return this;
   };
 }
 
 module.exports = applyCustomSort;
+
+// do {
+//   count = 0;
+
+//   for (let i = 1; i < this.length; i++) {
+//     const prev = this[i - 1];
+//     const current = this[i];
+
+//     if (compareFunction(prev, current) > 0) {
+//       count++;
+//       this[i - 1] = current;
+//       this[i] = prev;
+//     }
+//   };
+// } while (count > 0);
