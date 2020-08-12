dfb92047fc5923ce022719570af7c78796b92a3b
Zarva <zarva6596@gmail.com>
2020-08-12 12:17:32 +0300

    first commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,38 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const callback = compareFunction
+    || ((a, b) => {
+      const A = String(a);
+      const B = String(b);
+
+      if (A > B) {
+        return 1;
+      } else if (A < B) {
+        return -1;
+      } else if (A === B) {
+        return 0;
+      }
+    });
+
+    let n;
+
+    do {
+      n = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (callback(prev, current) > 0) {
+          this[i] = prev;
+          this[i - 1] = current;
+          n++;
+        }
+      }
+    } while (n > 0);
+
+    return this;
   };
 }
 
