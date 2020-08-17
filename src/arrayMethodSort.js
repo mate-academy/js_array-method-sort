e60a78811d9c9a9a19fe5f65c80fb8bf0ab51a3f
Dima Diachenko <zarg129@gmail.com>
2020-08-17 17:19:41 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -11,11 +11,12 @@ function applyCustomSort() {
       return -1;
     }
   }) {
-    for (let i = 0; i < this.length - 1; i++) {
-      if (compareFunction(this[i], this[i + 1]) > 0) {
-        [this[i], this[i + 1]] = [this[i + 1], this[i]];
-        this.sort2(compareFunction);
-      }
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[j], this[i]] = [this[i], this[j]];
+        };
+      };
     }
 
     return this;
