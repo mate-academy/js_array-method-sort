0a3cee74f54fd16be2adf13f369ced6c91e88038
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-09-29 13:21:38 +0300

    Solution sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,38 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
+function sortFunction(a, b) {
+  const first = String(a);
+  const second = String(b);
+
+  if (first > second) {
+    return 1;
+  } else if (second === first) {
+    return 0;
+  }
+
+  return -1;
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = sortFunction) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        if (compareFunction(this[i - 1], this[i]) > 0) {
+          const prev = this[i - 1];
+          const current = this[i];
+
+          this[i] = prev;
+          this[i - 1] = current;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
