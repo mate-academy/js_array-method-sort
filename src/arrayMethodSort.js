735293a7b59632eb5c2906fdfe6d695f56422e46
Anton <office.sklyarov@gmail.com>
2020-10-05 13:28:20 +0300

    fixes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,7 +4,24 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = stringFunction) {
+  [].__proto__.sort2 = function(compareFunction) {
+    let callback = compareFunction;
+
+    if (compareFunction === undefined) {
+      callback = (a, b) => {
+        const firstString = String(a);
+        const secondString = String(b);
+
+        if (firstString > secondString) {
+          return 1;
+        } else if (secondString === firstString) {
+          return 0;
+        }
+
+        return -1;
+      };
+    }
+
     let count;
 
     while (count !== 0) {
@@ -14,7 +31,7 @@ function applyCustomSort() {
         const secondNumber = this[i];
         const firstNumber = this[i - 1];
 
-        if (compareFunction(this[i - 1], this[i]) > 0) {
+        if (callback(this[i - 1], this[i]) > 0) {
           this[i - 1] = secondNumber;
           this[i] = firstNumber;
 
@@ -24,19 +41,6 @@ function applyCustomSort() {
     }
 
     return this;
-  };
-
-  function stringFunction(a, b) {
-    const firstString = String(a);
-    const secondString = String(b);
-
-    if (firstString > secondString) {
-      return 1;
-    } else if (secondString === firstString) {
-      return 0;
-    }
-
-    return -1;
   };
 }
 
