0449e2e692957aad9b23ee4daaccbcae13dad49f
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-17 15:01:05 +0200

    completed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,34 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let v;
+    let count = 1;
+
+    while (count !== 0) {
+      count = 0;
+
+      if (compareFunction === undefined) {
+        for (let i = 0; i < this.length - 1; i++) {
+          if (`${this[i]}` > `${this[i + 1]}`) {
+            v = this[i];
+            this[i] = this[i + 1];
+            this[i + 1] = v;
+            count++;
+          }
+        }
+      } else {
+        for (let i = 0; i < this.length - 1; i++) {
+          if (compareFunction(this[i], this[i + 1]) > 0) {
+            v = this[i];
+            this[i] = this[i + 1];
+            this[i + 1] = v;
+            count++;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
