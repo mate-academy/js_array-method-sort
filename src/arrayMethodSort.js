7503f29a819303db8fb5618c36d912427194e57d
Oleksandr Stolyarov <Stolyarov_396@icloud.com>
2020-12-18 19:30:05 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,11 +5,11 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    const n = this.length;
+    const arrLength = this.length;
     let rearrangement;
 
-    for (let i = 0; i < n - 1; i++) {
-      for (let j = 0; j < n - 1 - i; j++) {
+    for (let i = 0; i < arrLength - 1; i++) {
+      for (let j = 0; j < arrLength - 1 - i; j++) {
         if (!compareFunction) {
           rearrangement = this[j + 1].toString() < this[j].toString();
         } else {
