d8b74484d3f77b90502e73c2f2af06aa861b2599
tamara ostapets <tamara.ostapec@gmail.com>
2020-12-20 19:14:23 +0200

    Sort Method Implementation

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,30 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (prev, current) =>
+    `${prev}` > `${current}` ? 1
+      : `${prev}` < `${current}` ? -1
+        : 0
+  ) {
+    let counter;
+
+    do {
+      counter = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          this[i] = prev;
+          this[i - 1] = current;
+
+          counter++;
+        }
+      }
+    } while (counter > 0);
+
+    return this;
   };
 }
 
