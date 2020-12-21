01777b3be747933c64d97ec323712b84ef855150
Dmytro Tsehelnyk <34zimnii@gmail.com>
2020-12-21 12:20:13 +0200

    fixes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,17 +3,19 @@
 /**
  * Implement method Sort
  */
-const charsCompare = (charA, charB) => {
-  if (('' + charA) > ('' + charB)) {
+const charsCompare = (a, b) => {
+  if ((String(a)) > (String(b))) {
     return 1;
   }
 };
 
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = charsCompare) {
-    let counter = 0;
+    let counter;
 
-    while (true) {
+    do {
+      counter = 0;
+
       for (let i = 0; i < this.length - 1; i++) {
         const compareResult = compareFunction(this[i], this[i + 1]);
 
@@ -22,18 +24,13 @@ function applyCustomSort() {
 
           this[i] = this[i + 1];
           this[i + 1] = currentEl;
-        } else {
           counter++;
         }
       }
+    } while (counter > 0);
 
-      if (counter === this.length - 1) {
-        return this;
-      }
-
-      counter = 0;
-    }
+    return this;
   };
-}
+};
 
 module.exports = applyCustomSort;
