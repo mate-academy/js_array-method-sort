720b7fed3e613a781b846ba25b862a6aa3e83819
Oleg Bezriadin <muhachov86@gmail.com>
2021-02-19 17:03:00 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,45 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (arguments[0] === undefined) {
+      let minIdx;
+
+      for (let i = 0; i < this.length; i++) {
+        minIdx = i;
+
+        for (let j = i + 1; j < this.length; j++) {
+          if ((this[j]).toString() < (this[minIdx]).toString()) {
+            minIdx = j;
+          }
+        }
+        [this[i], this[minIdx]] = [this[minIdx], this[i]];
+      }
+
+      return this;
+    }
+
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        if (compareFunction(this[i - 1], this[i]) > 0) {
+          [this[i - 1], this[i]] = [this[i], this[i - 1]];
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
+
+// applyCustomSort()
+
+// const source = [3, 12, 2, 11];
+// const result = source.sort2();
+
+// console.log(result);
 
 module.exports = applyCustomSort;
