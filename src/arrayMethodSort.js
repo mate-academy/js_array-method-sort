ee91af26816de78fd0e8d772b91ee62964e3744e
Oleg Bezriadin <muhachov86@gmail.com>
2021-02-22 18:03:06 +0200

    changed from scratch

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,38 +4,32 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // sorted if no sorting function is provided, by default.
-    if (arguments[0] === undefined) {
-      let minIdx;
+  function defaultCompare(current, next) {
+    const currentValue = String(current);
+    const nextValue = String(next);
 
-      for (let i = 0; i < this.length; i++) {
-        minIdx = i;
-
-        for (let j = i + 1; j < this.length; j++) {
-          if ((this[j]).toString() < (this[minIdx]).toString()) {
-            minIdx = j;
-          }
-        }
-        [this[i], this[minIdx]] = [this[minIdx], this[i]];
-      }
-
-      return this;
+    if (currentValue > nextValue) {
+      return 1;
+    } else if (currentValue < nextValue) {
+      return -1;
+    } else {
+      return 0;
     }
-    // sorting with callback.
+  }
 
-    let count;
+  [].__proto__.sort2 = function(compareFunction = defaultCompare) {
+    let isChange;
 
     do {
-      count = 0;
+      isChange = false;
 
-      for (let i = 1; i < this.length; i++) {
-        if (compareFunction(this[i - 1], this[i]) > 0) {
-          [this[i - 1], this[i]] = [this[i], this[i - 1]];
-          count++;
+      for (let i = 0; i < this.length - 1; i++) {
+        if (compareFunction(this[i], this[i + 1]) > 0) {
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
+          isChange = true;
         }
       }
-    } while (count > 0);
+    } while (isChange);
 
     return this;
   };
