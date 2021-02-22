b784a6359075420ecdc1da18611f272c85a0feef
Tetiana Zotova <zotova.tetiana@gmail.com>
2021-02-22 12:20:17 +0200

    Solution revisited

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -17,13 +17,13 @@ function applyCustomSort(a, b) {
     count = 0;
 
     for (let i = 1; i < this.length; i++) {
-      const prev = this[i - 1];
+      const previous = this[i - 1];
       const current = this[i];
 
-      if (compareFunction(prev, current) > 0) {
+      if (compareFunction(previous, current) > 0) {
         count++;
         this[i - 1] = current;
-        this[i] = prev;
+        this[i] = previous;
       }
     }
   } while (count > 0);
