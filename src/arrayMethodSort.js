9eab13d364a254e668fb85fda658c5825b6d1141
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-11 17:22:34 +0200

    Created function applyCustomSort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,39 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  const compareAsStrings = (a, b) => {
+    const firstString = a.toString();
+    const nextString = b.toString();
+
+    if (firstString > nextString) {
+      return 1;
+    } else if (firstString === nextString) {
+      return 0;
+    } else {
+      return -1;
+    }
+  };
+
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
     // write code here
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const current = this[i];
+        const prev = this[i - 1];
+
+        if (compareFunction(prev, current) > 0) {
+          this[i] = prev;
+          this[i - 1] = current;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
