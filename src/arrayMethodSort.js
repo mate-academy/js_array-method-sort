84663a850d4242035098c83adf490a85d26c561c
Peter Shpagin <peter.shpagin@gmail.com>
2021-03-28 12:57:52 +0300

    Solution2

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,9 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(callback = (i, j) =>
-    (i.toString() < j.toString()) ? 0 : 1) {
+  [].__proto__.sort2 = function(
+    callback = (i, j) =>
+      (i.toString() < j.toString()) ? 0 : 1) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (callback(this[i], this[j]) > 0) {
