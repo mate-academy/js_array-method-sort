ab9ab6766e36441e1c0e8d7d6915c98aac9b51f1
Ivan Major <tarstarcas@gmail.com>
2021-04-12 17:11:21 +0300

    Method Sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,41 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let callback = compareFunction;
+
+    if (compareFunction === undefined) {
+      callback = (x, y) => {
+        const stringX = String(x);
+        const stringY = String(y);
+
+        if (stringX > stringY) {
+          return 1;
+        } else if (stringX === stringY) {
+          return 0;
+        } else {
+          return -1;
+        };
+      };
+    };
+
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (callback(prev, current) > 0) {
+          count++;
+          this[i - 1] = current;
+          this[i] = prev;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
