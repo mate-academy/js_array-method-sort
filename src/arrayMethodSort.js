718cb7cde8160949c4712ca889b1ea8c96377568
Anton Kostylov <antoniok003@gmail.com>
2021-04-27 17:34:03 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,6 +6,40 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     // write code here
+    let compare = compareFunction;
+
+    if (!compareFunction) {
+      compare = function(a, b) {
+        if (('' + a) > ('' + b)) {
+          return 1;
+        } else if (('' + a) === ('' + b)) {
+          return 0;
+        } else {
+          return -1;
+        }
+      };
+    }
+
+    let previousValue;
+    let currentValue;
+    let counter;
+
+    do {
+      counter = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        currentValue = this[i];
+        previousValue = this[i - 1];
+
+        if (compare(previousValue, currentValue) > 0) {
+          this[i] = previousValue;
+          this[i - 1] = currentValue;
+          counter++;
+        }
+      }
+    } while (counter > 0);
+
+    return this;
   };
 }
 
