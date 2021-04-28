a2883e77a911e07e5e08e8ae95eaf5867fbe56b2
Ihor Karpyn <igor82200@gmail.com>
2021-04-28 14:43:07 +0300

    made custom sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,42 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let callback = compareFunction;
+    const defaultCallback = (f, s) => {
+      const a = f.toString();
+      const b = s.toString();
+
+      if (a > b) {
+        return 1;
+      } else if (a < b) {
+        return -1;
+      } else {
+        return 0;
+      }
+    };
+
+    if (typeof callback !== 'function') {
+      callback = defaultCallback;
+    }
+
+    let wasChange = false;
+
+    do {
+      wasChange = false;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        const first = this[i];
+        const second = this[i + 1];
+
+        if (callback(first, second) > 0) {
+          this[i] = second;
+          this[i + 1] = first;
+          wasChange = true;
+        }
+      }
+    } while (wasChange);
+
+    return this;
   };
 }
 
