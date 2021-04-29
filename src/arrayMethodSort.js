49b1869897f9c3295596f8bb003f131d3f180fdb
Nazar Matsevych <nazar6192111@gmail.com>
2021-04-29 13:22:54 +0300

    fixed after review

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,33 +4,25 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    let callback = compareFunction;
-    let count;
-
-    if (!callback) {
-      callback = function(a, b) {
-        const strA = a.toString();
-        const strB = b.toString();
-
-        return strA > strB;
-      };
-    }
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => a.toString() > b.toString()
+  ) {
+    let change = false;
 
     do {
-      count = 0;
+      change = false;
 
       for (let i = 1; i < this.length; i++) {
         const prev = this[i - 1];
         const current = this[i];
 
-        if (callback(prev, current) > 0) {
-          count++;
+        if (compareFunction(prev, current) > 0) {
           this[i - 1] = current;
           this[i] = prev;
+          change = true;
         }
       }
-    } while (count > 0);
+    } while (change);
 
     return this;
   };
