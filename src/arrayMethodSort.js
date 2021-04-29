985bff075057d38734bba978430f9fc6afb8df2a
Nazar Matsevych <nazar6192111@gmail.com>
2021-04-29 22:23:56 +0300

    myNewSolution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,24 +5,24 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(
-    compareFunction = (a, b) => a.toString() > b.toString()
+    compareFunction = ((a, b) => a.toString() > b.toString())
   ) {
-    let change = false;
+    let count;
 
     do {
-      change = false;
+      count = 0;
 
       for (let i = 1; i < this.length; i++) {
         const prev = this[i - 1];
         const current = this[i];
 
         if (compareFunction(prev, current) > 0) {
-          this[i - 1] = current;
           this[i] = prev;
-          change = true;
+          this[i - 1] = current;
+          count++;
         }
       }
-    } while (change);
+    } while (count > 0);
 
     return this;
   };
