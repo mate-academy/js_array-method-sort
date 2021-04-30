d202cffe4b730c3ac4e4509ea787201858cc7a0e
Andrey Dorohov <andrey.dorohov8@gmail.com>
2021-04-30 12:54:34 +0300

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,24 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (a, b) => `${a}` > `${b}`) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        if (compareFunction(this[i - 1], this[i]) > 0) {
+          const el = this[i - 1];
+
+          this[i - 1] = this[i];
+          this[i] = el;
+          count++;
+        }
+      }
+    } while (count !== 0);
+
+    return this;
   };
 }
 
