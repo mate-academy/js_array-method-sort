3fe13c1fcd124af1aff8e411129b4012e01d6270
Ruslana Podshyvalova <lanataylorpersonal@gmail.com>
2021-05-03 23:30:49 +0300

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,35 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let comparator = compareFunction;
+
+    if (comparator === undefined) {
+      comparator = function(a, b) {
+        if (String(a) < String(b)) {
+          return -1;
+        }
+
+        if (String(a) > String(b)) {
+          return 1;
+        }
+
+        return 0;
+      };
+    }
+
+    let value;
+
+    for (let i = this.length - 1; i > 0; i--) {
+      for (let j = 0; j < i; j++) {
+        if (comparator(this[j], this[j + 1]) > 0) {
+          value = this[j];
+          this[j] = this[j + 1];
+          this[j + 1] = value;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
