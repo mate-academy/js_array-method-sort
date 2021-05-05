c1209154e9176c9da91b58ad311c87f49db132cf
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-05-05 19:55:08 +0300

    ready for check

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,40 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const compareFunction = (a, b) => {
+    const strA = String(a);
+    const strB = String(b);
+
+    if (strA > strB) {
+      return 1;
+    } else {
+      if (strA === strB) {
+        return 0;
+      } else {
+        return -1;
+      }
+    }
+  };
+
+  [].__proto__.sort2 = function(callback = compareFunction) {
+    let count = 0;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const firstElem = this[i - 1];
+        const secondElem = this[i];
+
+        if (callback(firstElem, secondElem) > 0) {
+          count++;
+          this[i] = firstElem;
+          this[i - 1] = secondElem;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
