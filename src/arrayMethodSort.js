3843afd3d186411edc46b1d90b120c000c0e0bfc
Vadym Liubych <v.liubych@ukr.net>
2021-05-16 12:03:09 +0300

    add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,29 @@
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => a.toString() > b.toString()
+  ) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          this[i - 1] = current;
+          this[i] = prev;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
