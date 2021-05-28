019691f789734be3e0bb0855306454d575c2774f
KostyaPenyavskiy <kornelius9898@gmail.com>
2021-05-28 10:12:38 +0300

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,27 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let wasReplace = false;
+
+    do {
+      wasReplace = false;
+
+      for (let i = 0; i < this.length; i++) {
+        const result = compareFunction
+          ? compareFunction(this[i], this[i + 1])
+          : String(this[i]) > String(this[i + 1]);
+
+        if (result > 0 && i !== this.length - 1) {
+          const next = this[i + 1];
+
+          this[i + 1] = this[i];
+          this[i] = next;
+          wasReplace = true;
+        }
+      }
+    } while (wasReplace);
+
+    return this;
   };
 }
 
