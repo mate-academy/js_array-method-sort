cff271ebeac9d11d04bffa13d160cd6f603454d3
Artem Yemelyanov <ae1666528@gmail.com>
2021-06-24 17:33:33 +0300

    Implemented sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,43 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let functionForComparing = compareFunction;
+
+    if (typeof compareFunction === 'undefined') {
+      functionForComparing = compareAsStrings;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      const key = this[i];
+      let j = i - 1;
+
+      while (j >= 0 && functionForComparing(key, this[j]) < 0) {
+        this[j + 1] = this[j];
+        j--;
+      }
+
+      this[j + 1] = key;
+    }
+
+    return this;
   };
+}
+
+function compareAsStrings(a, b) {
+  const stringA = String(a);
+  const stringB = String(b);
+
+  if (stringA > stringB) {
+    return 1;
+  }
+
+  if (stringA === stringB) {
+    return 0;
+  }
+
+  if (stringA < stringB) {
+    return -1;
+  }
 }
 
 module.exports = applyCustomSort;
