508e644f78937bf209beac7072fa4702011c80c4
David Dohru <tronsmartmy@gmail.com>
2021-06-28 22:08:25 +0300

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,38 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  function compareDefauld(a, b) {
+    const firstPart = a + '';
+    const secondPart = b + '';
+
+    if (firstPart > secondPart) {
+      return 1;
+    } else if (firstPart === secondPart) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+
+  [].__proto__.sort2 = function(compareFunction = compareDefauld) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const previous = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(previous, current) > 0) {
+          this[i] = previous;
+          this[i - 1] = current;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
