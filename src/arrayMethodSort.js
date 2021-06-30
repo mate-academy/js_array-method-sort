54268b0b99c266462b832a345cf35e01784eba7c
Bishko <yurik322412@gmail.com>
2021-06-30 14:10:18 +0300

    rename vers

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -25,12 +25,12 @@ function applyCustomSort() {
       count = 0;
 
       for (let i = 1; i < this.length; i++) {
-        const prev = this[i - 1];
+        const previous = this[i - 1];
         const current = this[i];
 
-        if (compareFunction(prev, current) > 0) {
+        if (compareFunction(previous, current) > 0) {
           count++;
-          this[i] = prev;
+          this[i] = previous;
           this[i - 1] = current;
         }
       }
