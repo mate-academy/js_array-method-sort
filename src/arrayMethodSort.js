267380af54f09a224d467b671971081b015d5e43
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-01 17:40:21 +0300

    task is completed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,6 +6,29 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     // write code here
+    let count;
+    let callback = compareFunction;
+
+    if (compareFunction === undefined) {
+      callback = (a, b) => `${a}` > `${b}` ? 1 : -1;
+    }
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (callback(prev, current) > 0) {
+          this[i] = prev;
+          this[i - 1] = current;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
