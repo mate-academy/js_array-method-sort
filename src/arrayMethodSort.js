ec954b30205cc177f661178ed7efa472c656e965
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-07-02 17:26:40 +0300

    feat: completed task sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,21 @@
 /**
  * Implement method Sort
  */
+const compareFunctiondefault = (a, b) => (String(a) < String(b) ? 0 : 1);
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareFunctiondefault) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
+        } else {
+          continue;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
