9d2e0cc6730972d0b8ac262df48a5f90c6f8ca48
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-07-14 15:37:16 +0300

    Task Done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,38 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const compareStrings = (arg1, arg2) => {
+    const stringA = arg1.toString();
+    const stringB = arg2.toString();
+
+    if (stringA > stringB) {
+      return 1;
+    } else if (stringA < stringB) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
+
+  [].__proto__.sort2 = function(compareFunction = compareStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const start = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(start, current) > 0) {
+          this[i - 1] = current;
+          this[i] = start;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
