56285c9120af5d28b6319985be4861b06cf6a237
Yulia <shabelna28@ukr.net>
2021-07-14 15:52:51 +0300

    newSolution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -9,17 +9,23 @@ function applyCustomSort() {
       return this;
     }
 
-    for (let j = 0; j < this.length; j++) {
+    let isUpdated;
+    const defCompareFunc = compareFunction
+    || ((a, b) => (a + '') > (b + '') ? 1 : -1);
+
+    do {
+      isUpdated = false;
+
       for (let i = 0; i < this.length - 1; i++) {
-        if ((compareFunction && compareFunction(this[i], this[i + 1]) > 0)
-        || (!compareFunction && (this[i] + '' > this[i + 1] + ''))) {
+        if (defCompareFunc(this[i], this[i + 1]) > 0) {
           const a = this[i];
 
           this[i] = this[i + 1];
           this[i + 1] = a;
+          isUpdated = true;
         }
       }
-    }
+    } while (isUpdated);
 
     return this;
   };
