8703720f567aefdf5f9abf8dfd36bd2ed3b22c18
Elya Alekhina <elya.alekhina@gmail.com>
2021-07-21 09:46:02 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,26 +1,48 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(
-    compareFunction = (current, next) => String(current) > String(next)
-  ) {
-    let helpElementToMove;
+  const compareAsStrings = (a, b) => {
+    const current = String(a);
+    const next = String(b);
 
-    for (let i = 0; i < this.length; i++) {
-      for (let j = i + 1; j < this.length; j++) {
-        if (compareFunction(this[i], this[j]) > 0) {
-          helpElementToMove = this[i];
-          this[i] = this[j];
-          this[j] = helpElementToMove;
+    if (current > next) {
+      return 1;
+    }
+
+    if (current < next) {
+      return -1;
+    }
+
+    if (current === next) {
+      return 0;
+    }
+  };
+
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let isSorted;
+
+    do {
+      isSorted = false;
+
+      for (let i = 1; i < this.length; i++) {
+        const previous = this[i - 1];
+        const next = this[i];
+
+        if (compareFunction(previous, next) > 0) {
+          isSorted = true;
+          this[i - 1] = next;
+          this[i] = previous;
+        }
+
+        if (compareFunction(previous, next) < 0) {
+          this[i - 1] = previous;
+          this[i] = next;
         }
       }
-    }
+    } while (isSorted === true);
 
     return this;
   };
-};
+}
 
 module.exports = applyCustomSort;
