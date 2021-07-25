2143c075fb0ccd1d32f65686107cc38ed0dcd1f0
Valentyna Makarycheva <valentina.kravch14@gmail.com>
2021-07-25 19:15:40 +0300

    Solution SortArray

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,33 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareStr = (a, b) => {
+    if (String(a) > String(b)) {
+      return 1;
+    } else if (String(a) < String(b)) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const curEl = this[i];
+        const prevEl = this[i - 1];
+
+        if (compareStr(prevEl, curEl) > 0) {
+          this[i - 1] = curEl;
+          this[i] = prevEl;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
