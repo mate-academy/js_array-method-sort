51b3f90efc6f47f82513c0b7e11d2c7c5be98460
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-08-06 00:12:22 +0300

    sort done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,26 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const comparator = compareFunction !== undefined
+      ? compareFunction
+      : (a, b) => String(a) > String(b) ? 1 : String(a) < String(b) ? -1 : 0;
+    let changes = 1;
+
+    while (changes !== 0) {
+      changes = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        if (comparator(this[i - 1], this[i]) > 0) {
+          const tempPrev = this[i - 1];
+
+          this[i - 1] = this[i];
+          this[i] = tempPrev;
+          changes++;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
