1e546893fc95bcac4d6b9ebe3006de36ecbd91e7
Sasha Lifashkin <alifashkin@outlook.com>
2021-08-07 14:14:15 +0300

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,49 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let count = 0;
+    let prev = null;
+    let now = null;
+
+    if (!compareFunction) {
+      do {
+        count = 0;
+
+        for (let i = 1; i < this.length; i++) {
+          prev = String(this[i - 1]);
+          now = String(this[i]);
+
+          if (prev > now) {
+            this[i - 1] = now;
+            this[i] = prev;
+            count++;
+          }
+        }
+      } while (count > 0);
+
+      for (let i = 0; i < this.length; i++) {
+        if (!isNaN(this[i])) {
+          this[i] = +this[i];
+        }
+      }
+    } else {
+      do {
+        count = 0;
+
+        for (let i = 1; i < this.length; i++) {
+          prev = this[i - 1];
+          now = this[i];
+
+          if (compareFunction(prev, now) > 0) {
+            this[i - 1] = now;
+            this[i] = prev;
+            count++;
+          }
+        }
+      } while (count > 0);
+    }
+
+    return this;
   };
 }
 
