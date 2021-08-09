1e49bd7d1485a2969ca9b4b064e49547e8c7bb9d
Moroz Dmytro <dmytro101100@gmail.com>
2021-08-09 13:27:05 +0300

    Sort method was implemented

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,35 @@
 /**
  * Implement method Sort
  */
+const compareAsStrings = function(a, b) {
+  const firstElement = String(a);
+  const secondElement = String(b);
+
+  if (firstElement > secondElement) {
+    return 1;
+  }
+
+  return secondElement > firstElement ? -1 : 0;
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let itemsSorted = 0;
+    let swapCheck;
+
+    do {
+      swapCheck = false;
+
+      for (let i = 1; i < this.length - itemsSorted; i++) {
+        if (compareFunction(this[i - 1], this[i]) > 0) {
+          [this[i - 1], this[i]] = [this[i], this[i - 1]];
+          swapCheck = true;
+        }
+      }
+      itemsSorted++;
+    } while (swapCheck);
+
+    return this;
   };
 }
 
