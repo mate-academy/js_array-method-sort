3a5f3c0ec618d736e2d1881dcbd05ac542653882
darynakravchuk <darinakravchuk@gmail.com>
2021-08-10 17:52:22 +0300

    sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,28 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const result = (a, b) => (a.toString() > b.toString());
+
+  [].__proto__.sort2 = function(compareFunction = result) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const x = this[i];
+
+        if (compareFunction(prev, x) > 0) {
+          this[i - 1] = x;
+          this[i] = prev;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
-}
+};
 
 module.exports = applyCustomSort;
