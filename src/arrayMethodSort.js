0bc78e4c8ee83d953f7403e4e57b8a012b7a113c
NastyaVovk <winc4ester@gmail.com>
2021-08-10 17:50:49 +0300

    initial

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,40 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
+const compareAsStrings = (a, b) => {
+  const stringA = String(a);
+  const stringB = String(b);
+
+  if (stringA > stringB) {
+    return 1;
+  } else if (stringB > stringA) {
+    return -1;
+  } else {
+    return 0;
+  }
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let counter;
+
+    do {
+      const lastIndex = this.length - 1;
+
+      counter = 0;
+
+      for (let i = 0; i < lastIndex; i++) {
+        const current = this[i];
+        const next = this[i + 1];
+
+        if (compareFunction(current, next) > 0) {
+          this[i] = next;
+          this[i + 1] = current;
+          counter++;
+        }
+      }
+    } while (counter > 0);
+
+    return this;
   };
 }
 
