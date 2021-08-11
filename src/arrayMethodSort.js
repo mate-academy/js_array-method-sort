b1568907dc72992137154310f856f4c5a479646c
Ihor Nahnybida <igor653279@gmail.com>
2021-08-11 14:19:13 +0300

    sorted

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,25 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let counter = 1;
+
+    while (counter) {
+      counter = 0;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        if (compareFunction(this[i], this[i + 1]) > 0) {
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
+          counter++;
+        }
+      }
+    }
+
+    return this;
+  };
+
+  const compareAsStrings = (itemOne, itemTwo) => {
+    return String(itemOne) > String(itemTwo) ? 1 : -1;
   };
 }
 
