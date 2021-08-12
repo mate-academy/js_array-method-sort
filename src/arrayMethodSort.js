247963fb4da3799be4d9a752446adb131046a6e7
Taras Holiuk <taras.holiuk@gmail.com"dsveagj 3rm>
2021-08-12 17:42:28 +0300

    initial commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,36 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  function defaultCompareFunction(a, b) {
+    const stringA = String(a);
+    const stringB = String(b);
+
+    return stringA > stringB
+      ? 1
+      : stringA < stringB
+        ? -1
+        : 0;
+  }
+
+  [].__proto__.sort2 = function(compareFunction = defaultCompareFunction) {
+    let wasChanged = true;
+
+    while (wasChanged === true) {
+      wasChanged = false;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          this[i] = prev;
+          this[i - 1] = current;
+          wasChanged = true;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
