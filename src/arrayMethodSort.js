c72f42fd4c97a4915bb5b5acd08f6e84edd7064d
Yevhenii Sviderskyi <evgen1sider@gmail.com>
2021-08-13 16:15:55 +0300

    must work

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,40 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  function compareString(a, b) {
+    const fiirstString = String(a);
+    const secondString = String(b);
+
+    if (fiirstString > secondString) {
+      return 1;
+    }
+
+    if (fiirstString === secondString) {
+      return 0;
+    }
+
+    return -1;
+  }
+
+  [].__proto__.sort2 = function(compareFunction = compareString) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        const firstElement = this[i];
+        const secondElement = this[i + 1];
+
+        if (compareFunction(firstElement, secondElement) > 0) {
+          this[i] = secondElement;
+          this[i + 1] = firstElement;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
