4551391121e8413866c27edefc1ac87c370802e8
Clon <clonvhs@gmail.com>
2021-08-18 12:08:38 +0300

    fixes after Review
    v1.1

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,30 +5,25 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    const sortedArray = this;
-
-    let waschange = false;
+    let wasChanged = false;
 
     do {
-      waschange = false;
+      wasChanged = false;
 
-      for (let i = 1; i < sortedArray.length; i++) {
+      for (let i = 1; i < this.length; i++) {
         if (compareFunction === undefined) {
-          if (sortedArray[i - 1].toString() > sortedArray[i].toString()) {
-            waschange = true;
-
-            sortedArray.splice(i - 1, 2, sortedArray[i], sortedArray[i - 1]);
+          if (this[i - 1].toString() > this[i].toString()) {
+            wasChanged = true;
+            this.splice(i - 1, 2, this[i], this[i - 1]);
           }
-        } else {
-          if (compareFunction(sortedArray[i - 1], sortedArray[i]) > 0) {
-            waschange = true;
-            sortedArray.splice(i - 1, 2, sortedArray[i], sortedArray[i - 1]);
-          }
+        } else if (compareFunction(this[i - 1], this[i]) > 0) {
+          wasChanged = true;
+          this.splice(i - 1, 2, this[i], this[i - 1]);
         }
       }
-    } while (waschange);
+    } while (wasChanged);
 
-    return sortedArray;
+    return this;
   };
 }
 
