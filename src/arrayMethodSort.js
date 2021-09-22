71f47d68c301296100476c94f6015e1fc3965742
Igor <i.kornienko1199@gmail.com>
2021-09-22 17:26:36 +0300

    solution git commit -am solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,28 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    // write code her
+    if (compareFunction) {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length; j++) {
+          if (compareFunction(this[i], this[j]) < 0) {
+            [this[i], this[j]] = [this[j], this[i]];
+          }
+        }
+      }
+
+      return this;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (this[j].toString() < this[i].toString()) {
+          [this[i], this[j]] = [this[j], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
