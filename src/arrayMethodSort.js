2ff947354de240c0c30bfda2a2d3010cb225c983
Igor <i.kornienko1199@gmail.com>
2021-09-29 14:41:33 +0300

    refactoring

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,33 +1,39 @@
 'use strict';
 
-/**
+/** [this[i], this[i - 1]] = [this[i - 1], this[i]];
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction = compare) {
     // write code her
-    if (compareFunction) {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = 0; j < this.length; j++) {
-          if (compareFunction(this[i], this[j]) < 0) {
-            [this[i], this[j]] = [this[j], this[i]];
-          }
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        if (compareFunction(this[i - 1], this[i]) > 0) {
+          [this[i - 1], this[i]] = [this[i], this[i - 1]];
+          count++;
         }
       }
-
-      return this;
-    }
-
-    for (let i = 0; i < this.length; i++) {
-      for (let j = i + 1; j < this.length; j++) {
-        if (this[j].toString() < this[i].toString()) {
-          [this[i], this[j]] = [this[j], this[i]];
-        }
-      }
-    }
+    } while (count > 0);
 
     return this;
   };
+
+  function compare(a, b) {
+    const prev = a.toString();
+    const current = b.toString();
+
+    if (prev < current) {
+      return -1;
+    } else if (prev > current) {
+      return 1;
+    }
+
+    return 0;
+  }
 }
 
 module.exports = applyCustomSort;
