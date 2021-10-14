848012407912c5ef63015564472c128c3ec04fa2
Igor Kovalev <bizzzy0@gmail.com>
2021-10-14 12:16:06 +0300

    Task was solved

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,39 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
+function elementCompare(firstElement, secondElement) {
+  if (String(firstElement) > String(secondElement)) {
+    return 1;
+  }
+
+  if (String(firstElement) < String(secondElement)) {
+    return -1;
+  }
+
+  if (String(firstElement) === String(secondElement)) {
+    return 0;
+  }
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = elementCompare) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const previous = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(previous, current)) {
+          this[i - 1] = current;
+          this[i] = previous;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
