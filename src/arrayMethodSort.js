f2b4ecdeff38c8943eacce5cbed69cde66a69ac5
Misha Rosa <misharosa.lviv@gmail.com>
2021-10-16 16:22:08 +0300

    sort was created

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,39 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (compareFunction !== undefined) {
+      let counter;
+
+      do {
+        counter = 0;
+
+        for (let i = 1; i < this.length; i++) {
+          const prev = this[i - 1];
+          const current = this[i];
+
+          if (compareFunction(prev, current) > 0) {
+            counter++;
+            this[i - 1] = current;
+            this[i] = prev;
+          }
+        }
+      } while (counter > 0);
+
+      return this;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length; j++) {
+        if (this[i].toString() < this[j].toString()) {
+          const a = this[j];
+
+          this[j] = this[i];
+          this[i] = a;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
