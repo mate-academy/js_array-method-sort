6fe1213adfada8db70358d8c0be4f946a2b95264
Misha Rosa <misharosa.lviv@gmail.com>
2021-10-20 23:14:19 +0300

    changes was added

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,39 +3,35 @@
 /**
  * Implement method Sort
  */
+
+const compareAsStrings = (a, b) => {
+  if (a.toString() > b.toString()) {
+    return 1;
+  } else if (a.toString() < b.toString()) {
+    return -1;
+  }
+
+  return 0;
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    if (compareFunction !== undefined) {
-      let counter;
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let counter;
 
-      do {
-        counter = 0;
+    do {
+      counter = 0;
 
-        for (let i = 1; i < this.length; i++) {
-          const prev = this[i - 1];
-          const current = this[i];
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
 
-          if (compareFunction(prev, current) > 0) {
-            counter++;
-            this[i - 1] = current;
-            this[i] = prev;
-          }
-        }
-      } while (counter > 0);
-
-      return this;
-    }
-
-    for (let i = 0; i < this.length; i++) {
-      for (let j = 0; j < this.length; j++) {
-        if (this[i].toString() < this[j].toString()) {
-          const element = this[j];
-
-          this[j] = this[i];
-          this[i] = element;
+        if (compareFunction(prev, current) > 0) {
+          counter++;
+          this[i - 1] = current;
+          this[i] = prev;
         }
       }
-    }
+    } while (counter > 0);
 
     return this;
   };
