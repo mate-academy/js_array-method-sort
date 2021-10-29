cc794871aa8a7557cfe963ab55fabf70472dec80
Yuriy Titov <yuranus@gmail.com>
2021-10-29 19:32:33 +0300

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,39 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let count = 0;
+
+    const strCompare = (a, b) => {
+      const stringA = String(a);
+      const stringB = String(b);
+
+      return stringA > stringB
+        ? 1
+        : stringA === stringB
+          ? 0
+          : -1;
+    };
+
+    const callBack = compareFunction === undefined
+      ? strCompare
+      : compareFunction;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const curr = this[i];
+
+        if (callBack(prev, curr) > 0) {
+          this[i - 1] = curr;
+          this[i] = prev;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
