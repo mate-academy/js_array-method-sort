075277baf81eb2e347478f85f46adf2293e90083
Andriy Yurkiv <88943977+andriy-yurkiv@users.noreply.github.com>
2021-11-01 12:01:08 +0200

    sort_method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,40 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
-  };
-}
+  function sortString(a, b) {
+    const stringA = String(a);
+    const stringB = String(b);
+
+    if (stringA > stringB) {
+      return 1;
+    } else if (stringA === stringB) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+
+  [].__proto__.sort2 = function(compareFunction = sortString) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const curr = this[i];
+
+        if (compareFunction(prev, curr) > 0) {
+          count++;
+          this[i - 1] = curr;
+          this[i] = prev;
+        }
+      }
+    } while (count > 0);
+
+    return this;
+  }
+};
+
 
 module.exports = applyCustomSort;
