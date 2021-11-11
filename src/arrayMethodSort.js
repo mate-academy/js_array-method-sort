694043f692058ea0498326530ea00a9272a916f5
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-11 08:51:07 +0200

    Implement my own custon sort method using bubble sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,50 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const callback = compareFunction || compareAsStrings;
+
+    while (true) {
+      let wasMoved = false;
+
+      for (let i = 1; i < this.length; i++) {
+        const previous = this[i - 1];
+        const current = this[i];
+
+        if (callback(previous, current) > 0) {
+          this[i] = previous;
+          this[i - 1] = current;
+
+          wasMoved = true;
+        }
+      }
+
+      if (!wasMoved) {
+        return this;
+      }
+    }
   };
+}
+
+/**
+ *
+ * @param {string|number} a
+ * @param {string|number} b
+ *
+ * @returns {number}
+ */
+function compareAsStrings(a, b) {
+  const element1 = String(a);
+  const element2 = String(b);
+
+  if (element1 > element2) {
+    return 1;
+  }
+
+  if (element1 < element2) {
+    return -1;
+  }
+
+  return 0;
 }
 
 module.exports = applyCustomSort;
