4150be3ba8761eb712c97ed83c87efaed28c1258
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-11-14 22:28:34 +0200

    solution was added

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,39 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = defaultComparison) {
+    let countChanges;
+
+    do {
+      countChanges = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const previousElement = this[i - 1];
+        const currentElement = this[i];
+
+        if (compareFunction(previousElement, currentElement) > 0) {
+          this[i - 1] = currentElement;
+          this[i] = previousElement;
+          countChanges++;
+        }
+      }
+    } while (countChanges > 0);
+
+    return this;
   };
 }
+
+const defaultComparison = function(firstElement, secondElement) {
+  const elementOne = String(firstElement);
+  const elementTwo = String(secondElement);
+
+  if (elementOne > elementTwo) {
+    return 1;
+  } else if (elementOne < elementTwo) {
+    return -1;
+  } else {
+    return 0;
+  }
+};
 
 module.exports = applyCustomSort;
