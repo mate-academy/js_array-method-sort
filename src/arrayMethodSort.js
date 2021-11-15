830c832b6c1be8ae1a0eda0a7a200f7ce1e1175a
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-11-15 11:24:37 +0200

    made changes to defaultComparison function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -32,11 +32,13 @@ const defaultComparison = function(firstElement, secondElement) {
 
   if (elementOne > elementTwo) {
     return 1;
-  } else if (elementOne < elementTwo) {
-    return -1;
-  } else {
-    return 0;
   }
+ 
+ if (elementOne < elementTwo) {
+    return -1;
+  }
+  
+  return 0;
 };
 
 module.exports = applyCustomSort;
