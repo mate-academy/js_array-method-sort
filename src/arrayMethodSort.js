4845ef6a4d8b378b744120eca3b86b7784e44c20
Mariia Bila <freiheit.maria@gmail.com>
2021-11-29 18:07:53 +0200

    default function was fixed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,44 +4,43 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction = sortAsDefault) {
     let result = 0;
     let first, next;
     let count = 0;
 
-    if (compareFunction === undefined) {
-      do {
-        count = 0;
+    do {
+      count = 0;
 
-        for (let i = 0; i < this.length - 1; i++) {
-          if (String(this[i]) > String(this[i + 1])) {
-            count++;
-            next = this[i];
-            first = this[i + 1];
-            this[i] = first;
-            this[i + 1] = next;
-          }
+      for (let i = 1; i < this.length; i++) {
+        result = compareFunction(this[i - 1], this[i]);
+
+        if (result > 0) {
+          count++;
+          next = this[i - 1];
+          first = this[i];
+          this[i - 1] = first;
+          this[i] = next;
         }
-      } while (count > 0);
-    } else {
-      do {
-        count = 0;
-
-        for (let i = 0; i < this.length; i++) {
-          result = compareFunction(this[i], this[i + 1]);
-
-          if (result > 0) {
-            count++;
-            next = this[i];
-            first = this[i + 1];
-            this[i] = first;
-            this[i + 1] = next;
-          }
-        }
-      } while (count > 0);
-    }
+      }
+    } while (count > 0);
 
     return this;
   };
 }
+
+function sortAsDefault(a, b) {
+  const prev = String(a);
+  const current = String(b);
+
+  if (prev > current) {
+    return 1;
+  }
+
+  if (prev < current) {
+    return -1;
+  }
+
+  return 0;
+};
 module.exports = applyCustomSort;
