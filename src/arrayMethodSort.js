bce0f15ccb1f5715369c926aae6f9e226a825f0b
KHTRE <KHTREE@gmail.com>
2021-12-19 10:12:15 +0200

    works

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,31 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let worked = 1;
+
+    while (worked === 1) {
+      worked = 0;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        let result = 0;
+
+        if (compareFunction !== undefined) {
+          result = compareFunction(this[i], this[i + 1]);
+        } else if (String(this[i]) > String(this[i + 1])) {
+          result = 1;
+        }
+
+        if (result > 0) {
+          const temp = this[i];
+
+          this[i] = this[i + 1];
+          this[i + 1] = temp;
+          worked = 1;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
