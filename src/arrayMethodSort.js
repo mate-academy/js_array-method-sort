bfd265e68d283c7867d5c983135f53706862e07b
michael valiahmetov <michael.valiahmetov@gmail.com>
2021-12-22 12:53:56 +0200

    Method-sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,44 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let count;
+    let callbackFunction;
+    const defaultCompareFunction = (a, b) => {
+      const stringA = String(a);
+      const stringB = String(b);
+
+      if (stringA > stringB) {
+        return 1;
+      } else if (stringA === stringB) {
+        return 0;
+      } else {
+        return -1;
+      }
+    };
+
+    if (compareFunction === undefined) {
+      callbackFunction = defaultCompareFunction;
+    } else {
+      callbackFunction = compareFunction;
+    };
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (callbackFunction(prev, current) > 0) {
+          count++;
+
+          this[i - 1] = current;
+          this[i] = prev;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
