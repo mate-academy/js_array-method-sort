86f45d20b459809b288e7451dc96db3b6f78bba5
Taras Zherebetkiy <tarasj76@gmail.com>
2021-12-22 13:51:26 +0200

    sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,42 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let again = 1;
+    let func = (a, b) => {
+      const strA = String(a);
+      const strB = String(b);
+
+      if (strA > strB) {
+        return 1;
+      }
+
+      if (strA < strB) {
+        return -1;
+      }
+
+      return 0;
+    };
+
+    if (compareFunction !== undefined) {
+      func = compareFunction;
+    }
+
+    while (again !== 0) {
+      again = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (func(prev, current) > 0) {
+          again = 1;
+          this[i - 1] = current;
+          this[i] = prev;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
