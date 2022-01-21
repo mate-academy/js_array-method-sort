28823c01f389a716a1bde6fccd01844ea8c0ed03
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-01-21 18:59:40 +0200

    done!

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,31 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = func) {
+    for (let i = 0; i < this.length - 1; i++) {
+      let maybeDone = false;
+
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+          maybeDone = true;
+        }
+      }
+
+      if (!maybeDone) {
+        break;
+      }
+    }
+
+    return this;
   };
+}
+
+function func(a, b) {
+  const strA = '' + a;
+  const strB = '' + b;
+
+  return strA > strB ? 1 : strA < strB ? -1 : 0;
 }
 
 module.exports = applyCustomSort;
