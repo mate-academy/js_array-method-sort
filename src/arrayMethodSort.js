6d2b8d28a3fa45b12ca97ca8ff90946e332f379e
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-01-24 15:25:52 +0200

    gotta love that O(n**2)

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,33 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let shift;
+
+    do {
+      shift = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        if (compareFunction(this[i - 1], this[i]) > 0) {
+          const temp = this[i];
+
+          this[i] = this[i - 1];
+          this[i - 1] = temp;
+          shift++;
+        }
+      }
+    } while (shift);
+
+    return this;
   };
+}
+
+function compareAsStrings(a, b) {
+  if (String(a) > String(b)) {
+    return 1;
+  }
+
+  return 0;
 }
 
 module.exports = applyCustomSort;
