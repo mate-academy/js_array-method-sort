8a7756c623c535f4892e24ec004166bc12e98942
Oleksii Pinchuk <oleksii.pin4uk@gmail.com>
2022-01-26 17:36:06 +0300

    sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,44 @@
 /**
  * Implement method Sort
  */
+function compareAsString(a, b) {
+  const aAsString = String(a);
+  const bAsString = String(b);
+
+  if (aAsString > bAsString) {
+    return 1;
+  }
+
+  if (aAsString < bAsString) {
+    return -1;
+  }
+
+  if (aAsString === bAsString) {
+    return 0;
+  }
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(callback = compareAsString) {
     // write code here
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (callback(prev, current) > 0) {
+          this[i] = prev;
+          this[i - 1] = current;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
