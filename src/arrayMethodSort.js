ada6043fb473218aa84ae26c3cbd40ab83e4c654
Misha Novikov <novikov1807@gmail.com>
2022-01-28 17:07:18 +0200

    Complite

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,40 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(callback = stringSort) {
     // write code here
+    let countChange;
+
+    do {
+      countChange = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (callback(prev, current) > 0) {
+          this[i] = prev;
+          this[i - 1] = current;
+          countChange++;
+        }
+      }
+    } while (countChange > 0);
+
+    return this;
   };
+}
+
+function stringSort(a, b) {
+  const stringA = String(a);
+  const stringB = String(b);
+
+  if (stringA > stringB) {
+    return 1;
+  } else if (stringA < stringB) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
 module.exports = applyCustomSort;
