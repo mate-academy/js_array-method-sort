c6497b105745de5496868fcc8f524c8d7af723c5
Max Shlikhta <maxok.vibe@gmail.com>
2022-02-16 20:54:09 +0200

    Sort method implementation

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,40 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const compareAsStrings = (current, next) => {
+    const currentElement = String(current);
+    const nextElement = String(next);
+
+    if (currentElement > nextElement) {
+      return 1;
+    }
+
+    if (currentElement < nextElement) {
+      return -1;
+    }
+
+    return 0;
+  };
+
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          count++;
+          this[i - 1] = current;
+          this[i] = prev;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
