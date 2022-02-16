2ce100c11e62f23dea3ea261c1d97bc89f01ca70
Denis Stepanov <tt.denis.stepanov1996@gmail.com>
2022-02-17 01:35:30 +0200

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,39 @@
 /**
  * Implement method Sort
  */
+const compareAsString = (a, b) => {
+  const stringA = String(a);
+  const stringB = String(b);
+
+  if (stringA > stringB) {
+    return 1;
+  } else if (stringA < stringB) {
+    return -1;
+  } else {
+    return 0;
+  }
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsString) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const temp = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(temp, current) > 0) {
+          count++;
+          this[i - 1] = current;
+          this[i] = temp;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
