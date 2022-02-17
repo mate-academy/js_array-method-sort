c48a539409682c28ea2e38e71b625964a9ddbb8a
Anna Serdechna <anserdechna@gmail.com>
2022-02-17 07:04:13 +0200

    sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,10 +3,41 @@
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          count++;
+          this[i - 1] = current;
+          this[i] = prev;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
+
+const compareStrings = (a, b) => {
+  const firstStr = a.toString();
+  const secondStr = b.toString();
+
+  if (firstStr > secondStr) {
+    return 1;
+  } else if (firstStr === secondStr) {
+    return 0;
+  } else {
+    return -1;
+  }
+};
 
 module.exports = applyCustomSort;
