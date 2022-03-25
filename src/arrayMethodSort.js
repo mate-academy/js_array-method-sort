ab34e1f24cc063555d536e5bb7e49a0b6851f8c3
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-03-25 21:47:31 +0200

    js-array-method-sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,26 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(
+    callback = (a, b) => String(a) > String(b)
+  ) {
+    let sorted;
+
+    do {
+      sorted = false;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        if (callback(this[i], this[i + 1]) > 0) {
+          const temp = this[i + 1];
+
+          this[i + 1] = this[i];
+          this[i] = temp;
+          sorted = true;
+        }
+      }
+    } while (sorted);
+
+    return this;
   };
 }
 
