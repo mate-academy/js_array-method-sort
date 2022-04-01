50d17cad2b6805d2c8a0e70de5aa97913b3210b7
Vlad Skoromnyi <VectraSton@gmail.com>
2022-04-01 15:05:26 +0300

    sort method was implemented

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,38 @@
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (a, b) => {
+    const prevString = String(a);
+    const currentString = String(b);
+
+    if (prevString > currentString) {
+      return 1;
+    } else if (prevString === currentString) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          this[i - 1] = current;
+          this[i] = prev;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
