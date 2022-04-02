cb860d8219db8ec94527af6cac3bd5207248839d
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-04-02 16:40:45 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,40 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const curr = this[i];
+
+        if (compareFunction) {
+          if (this.every(elem => typeof elem === 'number')) {
+            if (curr < prev) {
+              count++;
+              this[i - 1] = curr;
+              this[i] = prev;
+            }
+          } else if (this.every(elem => typeof elem === 'string')) {
+            if (curr.localeCompare(prev) === -1) {
+              count++;
+              this[i - 1] = curr;
+              this[i] = prev;
+            }
+          }
+        } else {
+          if (curr.toString() < prev.toString()) {
+            count++;
+            this[i - 1] = curr;
+            this[i] = prev;
+          }
+        }
+      }
+    } while (count !== 0);
+
+    return this;
   };
 }
 
