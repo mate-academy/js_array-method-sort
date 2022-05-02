4aecc419786b21d4c092ceda77c3ed5b599e2c2d
Rashaad Ebrahim <rmebrahim@gmail.com>
2022-05-02 17:13:35 +0200

    Task complete

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,44 @@
 /**
  * Implement method Sort
  */
+function campareAsStrings(first, second) {
+  let a = first;
+  let b = second;
+
+  a = String(a);
+  b = String(b);
+
+  if (a > b) {
+    return 1;
+  }
+
+  if (a < b) {
+    return -1;
+  }
+
+  return 0;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = campareAsStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const previous = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(previous, current) > 0) {
+          count++;
+          this[i - 1] = current;
+          this[i] = previous;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
