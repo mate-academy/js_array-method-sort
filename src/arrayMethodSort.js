9ad68d3754e77a5670412dd49cd313d3d28b91f9
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-05-30 16:32:19 +0300

    first try

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,27 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let count = 0;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const a = this[i - 1];
+        const b = this[i];
+
+        const compare = (arguments.length === 0)
+          ? (`${a}` > `${b}`)
+          : compareFunction(a, b) > 0;
+
+        if (compare) {
+          [this[i - 1], this[i]] = [this[i], this[i - 1]];
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
