a408234d3aa973c2696449adae4c194114a6e71e
Vitalii-Heiko <papus@ukr.net.com>
2022-05-30 09:36:43 +0300

    Solution chenged

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,32 +6,25 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     // write code here
+    let count = 0;
 
-    if (!arguments.length) {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = 1; j < this.length; j++) {
-          const a = this[j - 1].toString();
-          const b = this[j].toString();
+    do {
+      count = 0;
 
-          if (a > b) {
-            [this[j - 1], this[j]] = [this[j], this[j - 1]];
-          }
+      for (let i = 1; i < this.length; i++) {
+        const a = this[i - 1];
+        const b = this[i];
+
+        const compare = (arguments.length === 0)
+          ? (`${a}` > `${b}`)
+          : compareFunction(a, b) > 0;
+
+        if (compare) {
+          [this[i - 1], this[i]] = [this[i], this[i - 1]];
+          count++;
         }
       }
-
-      return this;
-    }
-
-    for (let i = 0; i < this.length; i++) {
-      for (let j = 1; j < this.length; j++) {
-        const a = this[j - 1];
-        const b = this[j];
-
-        if (compareFunction(a, b) > 0) {
-          [this[j - 1], this[j]] = [this[j], this[j - 1]];
-        }
-      }
-    }
+    } while (count > 0);
 
     return this;
   };
