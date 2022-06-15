11cdfe0f2fc6667141641c1a13696eb5b1d6522e
SAMAEV <andreysamaev@gmail.com>
2022-06-15 15:55:01 +0300

    sort with strings

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,28 +3,6 @@
 /**
  * Implement method Sort
  */
-function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
-    let count;
-
-    do {
-      count = 0;
-
-      for (let i = 1; i < this.length; i++) {
-        const prev = this[i - 1];
-        const current = this[i];
-
-        if (compareFunction(prev, current)) {
-          this[i - 1] = current;
-          this[i] = prev;
-          count++;
-        }
-      };
-    } while (count > 0);
-
-    return this;
-  };
-}
 
 const compareAsStrings = (a, b) => {
   const strA = String(a);
@@ -38,5 +16,28 @@ const compareAsStrings = (a, b) => {
     return -1;
   }
 };
+
+function applyCustomSort() {
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          this[i - 1] = current;
+          this[i] = prev;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
+  };
+}
 
 module.exports = applyCustomSort;
