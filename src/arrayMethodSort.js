0a0055fc487da9d90f70f65d2248567ccb5ab29b
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-18 19:00:29 +0300

    first commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,37 @@
 'use strict';
-
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     // write code here
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i; j < this.length; j++) {
+        const changeHandler = (a, b) => {
+          const temp = this[a];
+
+          this[a] = this[b];
+          this[b] = temp;
+        };
+
+        if (compareFunction) {
+          if (compareFunction(this[i], this[j]) < 0) {
+            changeHandler(i, j);
+          }
+
+          if (compareFunction(this[i], this[j]) > 0) {
+            changeHandler(j, i);
+          }
+        } else {
+          if (`${this[i]}` > `${this[j]}`) {
+            changeHandler(i, j);
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
