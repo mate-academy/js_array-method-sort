74a4bed364f655a25e7a54eddcf4f68da877ec36
Oleg Vetrov <oleg94038@gmail.com>
2022-07-12 14:12:33 +0300

    Added custom sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let operations = -1;
+
+    while (operations !== 0) {
+      operations = 0;
+
+      if (!compareFunction) {
+        for (let i = 0; i <= this.length - 1; i++) {
+          if (String(this[i]) > String(this[i + 1]) && this[i + 1]) {
+            [this[i], this[i + 1]] = [this[i + 1], this[i]];
+            operations++;
+          }
+        }
+      } else {
+        for (let i = 0; i <= this.length - 1; i++) {
+          if (compareFunction(this[i], this[i + 1]) > 0) {
+            [this[i], this[i + 1]] = [this[i + 1], this[i]];
+            operations++;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
