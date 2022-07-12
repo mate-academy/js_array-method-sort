cf9795cef2c0ff9ceafd3fbb25d9881f3c3cf201
Oleksandr-Pustovit <Pustovit.o2020@gmail.com>
2022-07-12 18:05:37 +0300

    added custom sort function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,52 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let compFunc;
+
+    if (arguments.length === 0) {
+      compFunc = defaultCompareFunction;
+    } else {
+      compFunc = compareFunction;
+    }
+
+    while (!isSorted(this, compFunc)) {
+      for (let i = 0; i < this.length - 1; i++) {
+        if (compFunc(this[i], this[i + 1]) > 0) {
+          switchPlaces(this, i, i + 1);
+        }
+      }
+    }
+
+    return this;
   };
+}
+
+function switchPlaces(arr, index1, index2) {
+  const memory = arr[index1];
+
+  arr[index1] = arr[index2];
+  arr[index2] = memory;
+}
+
+function isSorted(arr, callback) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (callback(arr[i], arr[i + 1]) > 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function defaultCompareFunction(a, b) {
+  const first = '' + a;
+  const second = '' + b;
+
+  switch (true) {
+    case first < second: return -1;
+    case first > second: return 1;
+    case first === second: return 0;
+  }
 }
 
 module.exports = applyCustomSort;
