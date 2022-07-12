ede967fa118bbb540ec4b815c420d542cb3c4a2b
Denys Korotkoruchko <denenergii@gmail.com>
2022-07-12 22:13:23 +0300

    ready

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,17 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (a, b) =>
+    String(a) > String(b) ? 1 : -1) {
+    for (let index = 1; index < this.length; index++) {
+      for (let i = 0; i < this.length - index; i++) {
+        if (compareFunction(this[i], this[i + 1]) > 0) {
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
