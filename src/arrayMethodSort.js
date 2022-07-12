15a6452116e6035971fb010e2019ca26bade0727
Leoned Pustovit <lrerednawl@gmail.com>
2022-07-12 18:19:53 +0300

    implemented some seen hints to my code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -7,34 +7,26 @@ function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     let madeSwitches = false;
     let replacedTemporary;
+    let comparement;
 
     if (arguments.length === 0) {
-      do {
-        madeSwitches = false;
-
-        for (let i = 0; i < this.length - 1; i++) {
-          if (this[i].toString() > this[i + 1].toString()) {
-            replacedTemporary = this[i];
-            this[i] = this[i + 1];
-            this[i + 1] = replacedTemporary;
-            madeSwitches = true;
-          }
-        }
-      } while (madeSwitches);
+      comparement = (first, second) => first.toString() > second.toString();
     } else {
-      do {
-        madeSwitches = false;
-
-        for (let i = 0; i < this.length - 1; i++) {
-          if (compareFunction(this[i], this[i + 1]) > 0) {
-            replacedTemporary = this[i];
-            this[i] = this[i + 1];
-            this[i + 1] = replacedTemporary;
-            madeSwitches = true;
-          }
-        }
-      } while (madeSwitches);
+      comparement = compareFunction;
     }
+
+    do {
+      madeSwitches = false;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        if (comparement(this[i], this[i + 1]) > 0) {
+          replacedTemporary = this[i];
+          this[i] = this[i + 1];
+          this[i + 1] = replacedTemporary;
+          madeSwitches = true;
+        }
+      }
+    } while (madeSwitches);
 
     return this;
   };
