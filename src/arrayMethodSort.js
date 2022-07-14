5866d720d4e0f5a0a5439835d9a014f22a9a364c
Mariya Sukhanova <suhanovamasha21@gmail.com>
2022-07-15 00:50:12 +0300

    Added sort implementation

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,38 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const compareStrings = (a, b) => {
+    const aStr = String(a);
+    const bStr = String(b);
+
+    if (aStr > bStr) {
+      return 1;
+    } else if (aStr === bStr) {
+      return 0;
+    } else {
+      return -1;
+    }
+  };
+
+  [].__proto__.sort2 = function(compareFunction = compareStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(this[i - 1], this[i]) > 0) {
+          this[i - 1] = current;
+          this[i] = prev;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
