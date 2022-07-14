d8929b74c55e303f631f79974ffdf42eee16e9cf
Yuliia Dronova <106536680+yuliia-dronova@users.noreply.github.com>
2022-07-14 20:44:05 +0300

    Sort method was implemented

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,41 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let count = 0;
+    let compareFunctionForUse;
+
+    const defaultFunction = (previous, current) => {
+      if (previous.toString() > current.toString()) {
+        return 1;
+      }
+
+      if (previous.toString() < current.toString()) {
+        return -1;
+      }
+
+      return 0;
+    };
+
+    arguments.length < 1
+      ? compareFunctionForUse = defaultFunction
+      : compareFunctionForUse = compareFunction;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const previous = this[i - 1];
+        const current = this[i];
+
+        if (compareFunctionForUse(previous, current) > 0) {
+          this[i] = previous;
+          this[i - 1] = current;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
