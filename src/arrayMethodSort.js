d414c054997f5de5642bd12ec4be551846be05ec
Volodymyr Shymanskyi <v.shimanskyi@bnb.net.ua>
2022-07-19 17:44:59 +0300

    1-st commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,32 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = stringCompare) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          count++;
+          this[i - 1] = current;
+          this[i] = prev;
+        }
+      }
+    } while (count > 0);
+
+    return this;
+  };
+
+  const stringCompare = (a, b) => {
+    const stringA = String(a);
+    const stringB = String(b);
+
+    return (stringA > stringB) ? 1 : ((stringA < stringB) ? -1 : 0);
   };
 }
 
