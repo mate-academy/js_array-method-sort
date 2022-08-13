42aaad87ab803d5a888d7979fee48fae8eea78d0
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-08-13 12:02:40 +0300

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 'use strict';
 
 /**
@@ -18,22 +19,19 @@ const compareAsStrings = (a, b) => {
 
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
-    let count = 0;
+    let count;
 
     for (let i = 1; i < this.length; i++) {
+      count = 0;
+
       const prev = this[i - 1];
       const current = this[i];
 
-      if (compareFunction(prev, current) > 0) {
-        this[i - 1] = current;
-        this[i] = prev;
-        count++;
-      }
+      compareFunction(prev, current) > 0
+        ? (this[i - 1] = current, this[i] = prev, count++)
+        : count--;
 
-      if (count > 0) {
-        count = 0;
-        i = 0;
-      }
+      count > 0 ? i = 0 : i;
     }
 
     return this;
