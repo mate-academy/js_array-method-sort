b9de33113b10815e609e02b8e96258fa8c202154
Kamal Tufekcic <kamal.tufekcic@gmail.com>
2022-09-20 15:32:42 +0300

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,37 @@
 /**
  * Implement method Sort
  */
+const compareAsStrings = (a, b) => {
+  if (String(a) > String(b)) {
+    return 1;
+  } else if (String(a) === String(b)) {
+    return 0;
+  } else {
+    return -1;
+  }
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          this[i - 1] = current;
+          this[i] = prev;
+
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
