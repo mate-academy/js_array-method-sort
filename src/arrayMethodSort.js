fded28891801379eb8d22860c8382698b8852646
Hlazman <glazman.b@gmail.com>
2022-09-20 15:54:49 +0300

    Implement array  method Sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,38 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const compareString = (itemA, itemB) => {
+    const valueOne = itemA.toString();
+    const valueTwo = itemB.toString();
+
+    if (valueOne > valueTwo) {
+      return 1;
+    } else if (valueOne < valueTwo) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
+
+  [].__proto__.sort2 = function(compareFunction = compareString) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prevValue = this[i - 1];
+        const currentValue = this[i];
+
+        if (compareFunction(prevValue, currentValue) > 0) {
+          this[i - 1] = currentValue;
+          this[i] = prevValue;
+          count++;
+        }
+      }
+    } while (count);
+
+    return this;
   };
 }
 
