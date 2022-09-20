ba6f8356b0fba7b7c87e549cac1172ccf163b6e1
fatihsamur <a.fatihsamur@gmail.com>
2022-09-20 23:32:36 +0300

    bubble sort implemented

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,35 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (this.length < 2) {
+      return this;
+    }
+
+    if (compareFunction) {
+      for (let j = 0; j < this.length; j++) {
+        for (let i = 0; i < this.length - j - 1; i++) {
+          if (compareFunction(String(this[i]), String(this[i + 1])) > 0) {
+            const temp = this[i];
+
+            this[i] = this[i + 1];
+            this[i + 1] = temp;
+          }
+        }
+      }
+    } else {
+      for (let j = 0; j < this.length; j++) {
+        for (let i = 0; i < this.length - j - 1; i++) {
+          if (String(this[i]) > String(this[i + 1])) {
+            const temp = this[i];
+
+            this[i] = this[i + 1];
+            this[i + 1] = temp;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
