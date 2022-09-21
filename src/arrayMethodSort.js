55c5b010997aa3a4f1f54387ca030797bdc473b3
Denys Kleimenov <denyskleimenovit@gmail.com>
2022-09-21 14:33:28 +0300

    Sort method was implemented

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,39 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          this[i - 1] = current;
+          this[i] = prev;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
+
+const compareAsStrings = (a, b) => {
+  const firstString = a.toString();
+  const secondString = b.toString();
+
+  if (firstString > secondString) {
+    return 1;
+  } else if (firstString < secondString) {
+    return -1;
+  }
+
+  return 0;
+};
 
 module.exports = applyCustomSort;
