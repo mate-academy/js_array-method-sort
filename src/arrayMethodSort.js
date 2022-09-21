40369fbaa6bce932410c0763c534614adadea2be
Andriy Hladkevych <andriyglad@gmail.com>
2022-09-21 17:05:03 +0300

    Fixed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,16 +3,22 @@
 /**
  * Implement method Sort
  */
+
+const defaultCompareFunction = (a, b) => {
+  const strA = String(a);
+  const strB = String(b);
+
+  if (strA > strB) {
+    return 1;
+  } else if (strA < strB) {
+    return -1;
+  } else {
+    return 0;
+  }
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = (a, b) => {
-    if (`${a}` > `${b}`) {
-      return 1;
-    } else if (`${a}` < `${b}`) {
-      return -1;
-    } else {
-      return 0;
-    }
-  }) {
+  [].__proto__.sort2 = function(compareFunction = defaultCompareFunction) {
     let isSwapped = true;
 
     while (isSwapped) {
@@ -20,7 +26,7 @@ function applyCustomSort() {
 
       for (let i = 1; i < this.length; i++) {
         if (compareFunction(this[i - 1], this[i]) > 0) {
-          this[i] = [this[i - 1], this[i - 1] = this[i]][0];
+          [this[i], this[i - 1]] = [this[i - 1], this[i]];
           isSwapped = true;
         }
       }
