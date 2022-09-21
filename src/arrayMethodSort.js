a032023009ed46918535ab33f12beab3edbcc0ec
fatihsamur <a.fatihsamur@gmail.com>
2022-09-21 03:00:43 +0300

    got rid of the repeated code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,31 +4,27 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compare) {
     if (this.length < 2) {
       return this;
     }
 
-    if (compareFunction) {
-      for (let j = 0; j < this.length; j++) {
-        for (let i = 0; i < this.length - j - 1; i++) {
-          if (compareFunction(String(this[i]), String(this[i + 1])) > 0) {
-            const temp = this[i];
+    const isCompare = arguments.length > 0;
 
-            this[i] = this[i + 1];
-            this[i + 1] = temp;
-          }
+    for (let j = 0; j < this.length; j++) {
+      for (let i = 0; i < this.length - j - 1; i++) {
+        let swap = false;
+
+        if (!isCompare && String(this[i]) > String(this[i + 1])) {
+          swap = true;
         }
-      }
-    } else {
-      for (let j = 0; j < this.length; j++) {
-        for (let i = 0; i < this.length - j - 1; i++) {
-          if (String(this[i]) > String(this[i + 1])) {
-            const temp = this[i];
 
-            this[i] = this[i + 1];
-            this[i + 1] = temp;
-          }
+        if (isCompare && compare(String(this[i]), String(this[i + 1])) > 0) {
+          swap = true;
+        }
+
+        if (swap) {
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
         }
       }
     }
