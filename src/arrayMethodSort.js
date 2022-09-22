ef660a3f1ce2b0eeb0f430d1c4a7abe72b761e79
Bibicutu <pauleugen@gmail.com>
2022-09-22 20:21:23 +0300

    First commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,34 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let switchElements = false;
+
+    for (let i = 1; i < this.length; i++) {
+      switch (arguments.length) {
+        case 0:
+          if ('' + this[i - 1] > '' + this[i]) {
+            switchElements = true;
+          }
+          break;
+
+        default:
+          if ((compareFunction(this[i - 1], this[i])) > 0) {
+            switchElements = true;
+          }
+      }
+
+      if (switchElements) {
+        [this[i - 1], this[i]] = [this[i], this[i - 1]];
+
+        if (i > 1) {
+          i -= 2;
+        }
+      }
+
+      switchElements = false;
+    }
+
+    return this;
   };
 }
 
