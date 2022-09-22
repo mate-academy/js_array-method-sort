7a58fbf2aef5da0b3531a74de9ea9b27054f7b71
Vlad Malinin <vladmalinin1999@gmail.com>
2022-09-22 16:52:31 +0300

    change function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,10 +3,24 @@
 /**
  * Implement method Sort
  */
+
+function compareString(a, b) {
+  const firstString = String(a);
+  const secondString = String(b);
+
+  if (firstString > secondString) {
+    return 1;
+  }
+
+  if (firstString === secondString) {
+    return 0;
+  }
+
+  return -1;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = ((a, b) => (
-    a.toString() > b.toString()
-  ))) {
+  [].__proto__.sort2 = function(compareFunction = compareString) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
