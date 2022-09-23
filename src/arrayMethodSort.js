af65b243926518f14745b871fc921b21b36e1509
naveed-shoukat <shoukat.naveed@gmail.com>
2022-09-23 23:09:40 +0300

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      let currentElement = String(this[i]);
+
+      for (let j = i + 1; j < this.length; j++) {
+        const nextElement = String(this[j]);
+        const swapCheck = compareFunction === undefined
+          ? currentElement > nextElement
+          : compareFunction(this[i], this[j]) > 0;
+
+        if (swapCheck) {
+          [
+            this[i],
+            this[j],
+          ] = [
+            this[j],
+            this[i],
+          ];
+          currentElement = String(this[i]);
+        }
+      }
+    }
+
+    return this;
   };
 }
 
