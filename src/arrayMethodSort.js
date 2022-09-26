1a9407d701bc928d996240e855b3e7491e42dc3b
fatihsamur <a.fatihsamur@gmail.com>
2022-09-26 17:49:09 +0300

    default compare funct added

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,26 +4,14 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compare) {
+  [].__proto__.sort2 = function(compare = defaultCompare) {
     if (this.length < 2) {
       return this;
     }
 
-    const isCompare = arguments.length > 0;
-
     for (let j = 0; j < this.length; j++) {
       for (let i = 0; i < this.length - j - 1; i++) {
-        let swap = false;
-
-        if (!isCompare && String(this[i]) > String(this[i + 1])) {
-          swap = true;
-        }
-
-        if (isCompare && compare(String(this[i]), String(this[i + 1])) > 0) {
-          swap = true;
-        }
-
-        if (swap) {
+        if (compare(String(this[i]), String(this[i + 1])) > 0) {
           [this[i], this[i + 1]] = [this[i + 1], this[i]];
         }
       }
@@ -31,6 +19,10 @@ function applyCustomSort() {
 
     return this;
   };
+}
+
+function defaultCompare(str1, str2) {
+  return str1 > str2 ? 1 : -1;
 }
 
 module.exports = applyCustomSort;
