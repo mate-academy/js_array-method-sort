a74ef3e734b26dffec3543408b3ca7aa5dba69f1
naveed-shoukat <shoukat.naveed@gmail.com>
2022-09-26 22:45:15 +0300

    solution fix

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,24 +5,23 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    for (let i = 0; i < this.length; i++) {
-      let currentElement = String(this[i]);
+    const defaultCompare = (firstElement, secondElement, compareFn) => {
+      if (compareFunction) {
+        return compareFn(firstElement, secondElement) > 0;
+      }
 
+      return String(firstElement) > String(secondElement);
+    };
+
+    for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        const nextElement = String(this[j]);
-        const swapCheck = compareFunction === undefined
-          ? currentElement > nextElement
-          : compareFunction(this[i], this[j]) > 0;
+        const swapCheck = defaultCompare(this[i], this[j], compareFunction);
 
         if (swapCheck) {
-          [
-            this[i],
-            this[j],
-          ] = [
-            this[j],
-            this[i],
-          ];
-          currentElement = String(this[i]);
+          const swapValue = this[i];
+
+          this[i] = this[j];
+          this[j] = swapValue;
         }
       }
     }
