3506bf60163b6bd5a7e0f68a4afb2488dc19c514
Vlad Malinin <vladmalinin1999@gmail.com>
2022-09-26 09:08:57 +0300

    Added do while loop

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -21,16 +21,22 @@ function compareString(a, b) {
 
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compareString) {
-    for (let i = 0; i < this.length; i++) {
-      for (let j = i + 1; j < this.length; j++) {
-        if (compareFunction(this[i], this[j]) > 0) {
-          const swap = this[i];
+    let count;
 
-          this[i] = this[j];
-          this[j] = swap;
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          count++;
+          this[i - 1] = current;
+          this[i] = prev;
         }
       }
-    }
+    } while (count > 0);
 
     return this;
   };
