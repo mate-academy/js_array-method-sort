bf1e9f06d3bcafd7d11fedcf91c4985adadd505b
fatihsamur <a.fatihsamur@gmail.com>
2022-09-27 18:22:03 +0300

    requested changes applied

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -11,7 +11,7 @@ function applyCustomSort() {
 
     for (let j = 0; j < this.length; j++) {
       for (let i = 0; i < this.length - j - 1; i++) {
-        if (compare(String(this[i]), String(this[i + 1])) > 0) {
+        if (compare((this[i]), (this[i + 1])) > 0) {
           [this[i], this[i + 1]] = [this[i + 1], this[i]];
         }
       }
@@ -21,7 +21,14 @@ function applyCustomSort() {
   };
 }
 
-function defaultCompare(str1, str2) {
+function defaultCompare(toCompare1, toCompare2) {
+  const str1 = String(toCompare1);
+  const str2 = String(toCompare2);
+
+  if (str1 === str2) {
+    return 0;
+  }
+
   return str1 > str2 ? 1 : -1;
 }
 
