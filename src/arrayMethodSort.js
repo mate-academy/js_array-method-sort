50167b93326f2f76685118760eb0f22d73cd879e
metin <metinbicaksiz@gmail.com>
2022-10-21 00:19:36 +0300

    sorted without eslint no-param

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,43 @@
 /**
  * Implement method Sort
  */
+function compareAsString(a, b) {
+  /* eslint-disable no-param-reassign */
+  a = String(a);
+  b = String(b);
+
+  if (a > b) {
+    return 1;
+  }
+
+  if (a < b) {
+    return -1;
+  }
+
+  return 0;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction = compareAsString) {
     // write code here
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          count++;
+          this[i - 1] = current;
+          this[i] = prev;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
