a4abe2611ea94c6711e9af62e5f7553fdf57b4d4
Oleksandr Lytvynenko <olek.lytvynenko@gmail.com>
2022-10-20 21:40:17 +0300

    resolve

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,33 +4,35 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  const defaultAsString = (a, b) => {
+  const compareStrings = (a, b) => {
     const strA = String(a);
     const strB = String(b);
 
     if (strA > strB) {
       return 1;
-    } else if (strA === strB) {
+    }
+
+    if (strA === strB) {
       return 0;
     }
 
     return -1;
   };
 
-  [].__proto__.sort2 = function(compareFunction = defaultAsString) {
+  [].__proto__.sort2 = function(compareFunction = compareStrings) {
     let count;
 
     do {
       count = 0;
 
       for (let i = 1; i < this.length; i++) {
-        const firstElement = this[i - 1];
-        const secondElement = this[i];
+        const previous = this[i - 1];
+        const current = this[i];
 
-        if (compareFunction(firstElement, secondElement) > 0) {
+        if (compareFunction(previous, current) > 0) {
           count++;
-          this[i - 1] = secondElement;
-          this[i] = firstElement;
+          this[i - 1] = current;
+          this[i] = previous;
         }
       }
     } while (count > 0);
