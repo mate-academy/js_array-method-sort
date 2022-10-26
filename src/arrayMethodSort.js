7829a2439e4010af94723e9b2e9e8d9e19d740d6
Iryna Lelyk <irunochka771@gmail.com>
2022-10-26 22:30:38 +0300

    Method sort was implemented

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,43 @@
 /**
  * Implement method Sort
  */
+
+const compareAsStrings = (prev, current) => {
+  const stringPrev = prev.toString();
+  const stringCurrent = current.toString();
+
+  if (stringPrev > stringCurrent) {
+    return 1;
+  }
+
+  if (stringPrev < stringCurrent) {
+    return -1;
+  }
+
+  return 0;
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prevElem = this[i - 1];
+        const currentElem = this[i];
+
+        if (compareFunction(prevElem, currentElem) > 0) {
+          this[i - 1] = currentElem;
+          this[i] = prevElem;
+
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
