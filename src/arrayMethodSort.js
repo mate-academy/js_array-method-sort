1127ebbbfb9c5d9968d11e65e99c3a1277c08945
Masha <teenderrr@gmail.com>
2022-10-31 09:14:30 +0200

    sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,25 +3,15 @@
 /**
  * Implement method Sort
  */
-const compareAsStrings = (a, b) => {
-  const stringA = String(a);
-  const stringB = String(b);
-
-  if (stringA > stringB) {
-    return 1;
-  } else if (stringA === stringB) {
-    return 0;
-  } else {
-    return -1;
-  }
-};
 
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
     // write code here
-    let count = 0;
+    let count;
 
     do {
+      count = 0;
+
       for (let i = 1; i < this.length; i++) {
         const prev = this[i - 1];
         const current = this[i];
@@ -35,6 +25,19 @@ function applyCustomSort() {
     } while (count > 0);
 
     return this;
+  };
+
+  const compareAsStrings = (a, b) => {
+    const stringA = String(a);
+    const stringB = String(b);
+
+    if (stringA > stringB) {
+      return 1;
+    } else if (stringA === stringB) {
+      return 0;
+    } else {
+      return -1;
+    }
   };
 }
 
