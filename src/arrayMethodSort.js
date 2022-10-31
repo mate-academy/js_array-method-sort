31ac09ecbe9d2634378a9146e55f84e11acffa65
Taras Pustovyi <taraselnik@gmail.com>
2022-10-31 22:56:03 +0200

    solution with 2 variants

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,49 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let internalExternalSwitchFunc = compareFunction;
+
+    if (internalExternalSwitchFunc === undefined) {
+      internalExternalSwitchFunc = (a, b) => {
+        if (`${a}` > `${b}`) {
+          return 1;
+        } else if (`${a}` === `${b}`) {
+          return 0;
+        }
+
+        return -1;
+      };
+    }
+
+    // for (let i = 0; i < this.length; i++) {
+    //   for (let j = i + 1; j < this.length; j++) {
+    //     if (internalExternalSwitchFunc(this[i], this[j]) > 0) {
+    //       const temp = this[i];
+    //       this[i] = this[j];
+    //       this[j] = temp;
+    //     }
+    //   }
+    // }
+
+    let counter;
+
+    do {
+      counter = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prevElem = this[i - 1];
+        const currentElem = this[i];
+
+        if (internalExternalSwitchFunc(prevElem, currentElem) > 0) {
+          this[i] = prevElem;
+          this[i - 1] = currentElem;
+
+          counter++;
+        }
+      }
+    } while (counter > 0);
+
+    return this;
   };
 }
 
