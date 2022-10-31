dd00e5c1543936ad25c387340d9d3ed07a4f98eb
Taras Pustovyi <taraselnik@gmail.com>
2022-11-01 00:23:00 +0200

    solution fixed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -11,7 +11,9 @@ function applyCustomSort() {
       internalExternalSwitchFunc = (a, b) => {
         if (`${a}` > `${b}`) {
           return 1;
-        } else if (`${a}` === `${b}`) {
+        }
+
+        if (`${a}` === `${b}`) {
           return 0;
         }
 
