75f7008a2b2a930625af4d33cdf6ddf98de3a173
Taras Pustovyi <taraselnik@gmail.com>
2022-11-02 23:20:42 +0200

    solution3

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,33 +4,34 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    let internalExternalSwitchFunc = compareFunction;
-
-    if (internalExternalSwitchFunc === undefined) {
-      internalExternalSwitchFunc = (a, b) => {
-        if (`${a}` > `${b}`) {
-          return 1;
-        }
-
-        if (`${a}` === `${b}`) {
-          return 0;
-        }
-
-        return -1;
-      };
+  const compairingStrings = (a, b) => {
+    if (`${a}` > `${b}`) {
+      return 1;
+    } else if (`${a}` === `${b}`) {
+      return 0;
     }
 
-    for (let i = 0; i < this.length; i++) {
-      for (let j = i + 1; j < this.length; j++) {
-        if (internalExternalSwitchFunc(this[i], this[j]) > 0) {
-          const temp = this[i];
+    return -1;
+  };
 
-          this[i] = this[j];
-          this[j] = temp;
+  [].__proto__.sort2 = function(compareFunction = compairingStrings) {
+    let counter;
+
+    do {
+      counter = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prevElem = this[i - 1];
+        const currentElem = this[i];
+
+        if (compareFunction(prevElem, currentElem) > 0) {
+          this[i] = prevElem;
+          this[i - 1] = currentElem;
+
+          counter++;
         }
       }
-    }
+    } while (counter > 0);
 
     return this;
   };
