3845da894e3009d289e06860c07e8f511b0ce458
Spirit <baolt.king@gmail.com>
2022-11-03 12:46:10 +0200

    complete

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let x = 0; x < this.length; x++) {
+      for (let y = 0; y < (this.length - x - 1); y++) {
+        const currentIndex = String(this[y]);
+        const nextIndex = String(this[y + 1]);
+
+        let compareResult = false;
+
+        if (typeof (compareFunction) === 'function') {
+          compareResult = compareFunction(this[y], this[y + 1]) > 0;
+        } else {
+          compareResult = (currentIndex > nextIndex);
+        };
+
+        if (compareResult) {
+          const temp = this[y];
+
+          this[y] = this[y + 1];
+          this[y + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
