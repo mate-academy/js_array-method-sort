22bb16bf6cf31748af84406c3e6f6fe8c99d7813
Kukharuk <its.up.to.you.dk@gmail.com>
2022-11-04 10:51:45 +0200

    sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,39 @@
 /**
  * Implement method Sort
  */
+function asString(a, b) {
+  const strA = String(a);
+  const strB = String(b);
+
+  if (strA > strB) {
+    return 1;
+  } else if (strA === strB) {
+    return 0;
+  }
+
+  return -1;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = asString) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const pre = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(pre, current) > 0) {
+          this[i] = pre;
+          this[i - 1] = current;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
