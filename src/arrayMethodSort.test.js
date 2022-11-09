69f27df63a8a0fd4f403912ff4f38937b7170ab9
Maks <comanderius@gmail.com>
2022-11-09 14:16:30 +0100

    Method sort part 2

--- src/arrayMethodSort.test.js
+++ src/arrayMethodSort.test.js
@@ -55,6 +55,6 @@ test('custom compare function, numbers: [3, 12, 2, 11].sort2((a, b) => a - b)',
 test(
   `custom compare function, strings: 
   ['ёж','як','аист'].sort2((a, b) => a.localeCompare(b))`, () => {
-    expect(['ёж', 'як', 'аист'].sort2((a, b) => a. localeCompare(b)))
+    expect(['ёж', 'як', 'аист'].sort2((a, b) => a.localeCompare(b)))
       .toEqual(['аист', 'ёж', 'як']);
   });
