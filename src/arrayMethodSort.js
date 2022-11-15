7941d37b24aeb2ca4fce3c0e8168ecca91bd4174
Anastasiia Horidieieva <gordeevaisgreat@gmail.com>
2022-11-15 15:15:33 +0200

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,36 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let toRepeat = true;
+
+    const bubbleSort = () => {
+      const defaultCheck = (a, b) => {
+        if (String(a) > String(b)) {
+          return 1;
+        }
+
+        return -1;
+      };
+
+      const checkCondition = compareFunction || defaultCheck;
+
+      for (let i = 1; i < this.length; i++) {
+        if (checkCondition(this[i - 1], this[i]) > 0) {
+          const oldValue = this[i - 1];
+
+          this[i - 1] = this[i];
+          this[i] = oldValue;
+          toRepeat = true;
+        }
+      }
+    };
+
+    while (toRepeat) {
+      toRepeat = false;
+      bubbleSort();
+    }
+
+    return this;
   };
 }
 
