7bb6c7ab8e7dea176a701c9c23d666cfab6cc1b5
zbyn3k <coding707@mailbox.org>
2022-11-17 15:07:04 +0100

    Added helper function to be used as a default comparison function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,6 +3,19 @@
 /**
  * Implement method Sort
  */
+
+function compareAsStrings(a, b) {
+  if (String(a) > String(b)) {
+    return 1;
+  }
+
+  if (String(a) < String(b)) {
+    return -1;
+  }
+
+  return 0;
+}
+
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     // write code here
