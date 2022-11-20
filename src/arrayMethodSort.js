dee9e816978551b371f7640f7ec57eeab5210610
Panianchuk M <panianchuk@gmail.com>
2022-11-20 22:26:48 +0200

    changes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -32,11 +32,13 @@ const compareAsStrings = (a, b) => {
 
   if (stringA > stringB) {
     return 1;
-  } else if (stringA === stringB) {
-    return 0;
-  } else {
-    return -1;
   }
+
+  if (stringA === stringB) {
+    return 0;
+  }
+
+  return -1;
 };
 
 module.exports = applyCustomSort;
