9382187c32db111e44d13f4bfad46fb9bda2bbdf
Yevhenii Stanchenko <galagystew@gmail.com>
2022-11-21 17:37:43 +0100

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,10 +3,52 @@
 /**
  * Implement method Sort
  */
+function compareStrings(previos, next) {
+  const previosElem = String(previos);
+  const nextElem = String(next);
+
+  if (previosElem > nextElem) {
+    return 1;
+  }
+
+  return -1;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const previousValue = this[i - 1];
+        const nextValue = this[i];
+
+        if (compareFunction(previousValue, nextValue) > 0) {
+          this[i - 1] = nextValue;
+          this[i] = previousValue;
+
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
 module.exports = applyCustomSort;
+// function sortFunction(a, b){
+//   2
+//     if(a меньше, чем b по некоторому критерию)
+//   3
+//        return -1 // Или любое число, меньшее нуля
+//   4
+//     if(a больше, чем b по некоторому критерию)
+//   5
+//        return 1  // Или любое число, большее нуля
+//   6
+//     // в случае а = b вернуть 0
+//   7
+//     return 0
