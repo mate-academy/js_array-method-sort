6dd7691a3d344e3dec131a0595f6966302e04dd8
Demut <uddarendeli1@gmail.com>
2022-11-24 21:08:39 +0300

    defaultCompare function defined

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,25 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = (a, b) =>
-    a.toString().localeCompare(b.toString())
-  ) {
+  const defaultCompare = (can1, can2) => {
+    const can1Str = typeof can1 === 'string' ? can1 : can1.toString();
+    const can2Str = typeof can2 === 'string' ? can2 : can2.toString();
+    let currentDif;
+
+    for (let i = 0; i < can1Str.length; i++) {
+      currentDif = can1Str.charCodeAt(i) - can2Str.charCodeAt(i);
+
+      if (currentDif > 0) {
+        return 1;
+      } else if (currentDif === 0) {
+        continue;
+      } else {
+        return -1;
+      }
+    }
+  };
+
+  [].__proto__.sort2 = function(compareFunction = defaultCompare) {
     let count;
     let prev;
 
