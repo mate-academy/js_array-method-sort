7c984c30d37e9903f80ae4aebde5ecc35f4641e4
Demut <uddarendeli1@gmail.com>
2022-11-24 06:54:20 +0300

    Solution, it seemed to me that there is a bug in the tests, I already reported it.

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,43 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let count;
+    let current;
+    let prev;
+
+    if (!compareFunction) {
+      do {
+        count = 0;
+
+        for (let i = 1; i < this.length; i++) {
+          current = this[i];
+          prev = this[i - 1];
+
+          if (current.toString().localeCompare(prev.toString()) < 0) {
+            this[i] = prev;
+            this[i - 1] = current;
+            count++;
+          }
+        }
+      } while (count > 0);
+    } else {
+      do {
+        count = 0;
+
+        for (let i = 1; i < this.length; i++) {
+          current = this[i];
+          prev = this[i - 1];
+
+          if (compareFunction(current, prev) < 0) {
+            this[i] = prev;
+            this[i - 1] = current;
+            count++;
+          }
+        }
+      } while (count > 0);
+    }
+
+    return this;
   };
 }
 
