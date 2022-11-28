ff9bd26a789018764617a815c556581c1c837422
Lefteris Loizides <loizides.lef@gmail.com>
2022-11-29 00:51:47 +0200

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,25 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const haveArgs = arguments.length > 0;
+    const isCyrillic = (/^[\u0400-\u04FF]+$/).test(this);
+
+    for (let iReverse = this.length - 1; iReverse >= 0; iReverse--) {
+      for (let i = 0; i < iReverse; i++) {
+        const current = this[i];
+        const next = this[i + 1];
+
+        if ((haveArgs && compareFunction(current, next) > 0)
+        || (!haveArgs && !isCyrillic && `${current}` > `${next}`)
+        || (!haveArgs && isCyrillic && current > next)) {
+          this[i] = next;
+          this[i + 1] = current;
+        }
+      }
+    }
+
+    return this;
   };
-}
+};
 
 module.exports = applyCustomSort;
