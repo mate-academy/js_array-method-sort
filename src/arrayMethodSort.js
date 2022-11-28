648062e4cac22bf008e490c66138fa529381059f
Lefteris Loizides <loizides.lef@gmail.com>
2022-11-29 00:56:04 +0200

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -8,8 +8,8 @@ function applyCustomSort() {
     const haveArgs = arguments.length > 0;
     const isCyrillic = (/^[\u0400-\u04FF]+$/).test(this);
 
-    for (let iReverse = this.length - 1; iReverse >= 0; iReverse--) {
-      for (let i = 0; i < iReverse; i++) {
+    for (let iterator = this.length - 1; iterator >= 0; iterator--) {
+      for (let i = 0; i < iterator; i++) {
         const current = this[i];
         const next = this[i + 1];
 
