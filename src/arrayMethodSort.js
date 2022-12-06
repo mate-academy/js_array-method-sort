dd00213beb1aa993aa29c67f07cfba46251edf67
Iorga Nicu <ioghigithublab@gmail.com>
2022-12-06 14:21:57 +0200

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,39 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
+function defaultCompare(str1, str2) {
+  const a = String(str1);
+  const b = String(str2);
+
+  switch (true) {
+    case a > b:
+      return 1;
+    case a < b:
+      return -1;
+    default:
+      return 0;
+  }
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = defaultCompare) {
+    let counter;
+
+    do {
+      counter = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          this[i - 1] = current;
+          this[i] = prev;
+          counter++;
+        }
+      }
+    } while (counter > 0);
+
+    return this;
   };
 }
 
