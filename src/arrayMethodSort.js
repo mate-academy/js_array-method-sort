dc407e0a5dafdbf07a75cd99e61fd8a8d0e08cee
Jack Ashkar <jackelashkar@gmail.com>
2022-12-14 01:41:22 +0300

    final edited

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,35 +3,38 @@
 /**
  * Implement method Sort
  */
-const compareDefault = (a, b) => {
-  const strA = String(a);
-  const strB = String(b);
-
-  if (strA > strB) {
-    return 1;
-  } else if (strA === strB) {
-    return 0;
-  } else {
-    return -1;
-  }
-};
-
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = compareDefault) {
-    for (let i = 1; i < this.length; i++) {
-      for (let j = i; j > 0; j--) {
-        const prev = this[j - 1];
-        const current = this[j];
+  const compareStrings = (a, b) => {
+    if (String(a) > String(b)) {
+      return 1;
+    }
+
+    if (String(a) < String(b)) {
+      return -1;
+    }
+
+    return 0;
+  };
+
+  [].__proto__.sort2 = function(compareFunction = compareStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
 
         if (compareFunction(prev, current) > 0) {
-          this[j - 1] = current;
-          this[j] = prev;
+          this[i - 1] = current;
+          this[i] = prev;
+          count++;
         }
       }
-    }
+    } while (count > 0);
 
     return this;
   };
 }
-
 module.exports = applyCustomSort;
