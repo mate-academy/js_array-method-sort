d2eda2af91fad93277d7a164504adb0a19e3a05c
EhorSaprykin <astralik1993@gmail.com>
2022-12-19 09:52:53 +0200

    Changes done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,28 +6,30 @@
 function stringSortFunction(a, b) {
   if (String(a) < String(b)) {
     return -1;
-  } else if (String(a) > String(b)) {
-    return 1;
-  } else {
-    return 0;
   }
+
+  if (String(a) > String(b)) {
+    return 1;
+  }
+
+  return 0;
 }
 
 function applyCustomSort() {
-  [].__proto__.sort2 = function(
-    compareFunction = stringSortFunction) {
+  [].__proto__.sort2 = function(compareFunction = stringSortFunction
+  ) {
     let count;
 
     do {
       count = 0;
 
       for (let i = 1; i < this.length; i++) {
-        const prew = this[i - 1];
+        const prev = this[i - 1];
         const current = this[i];
 
-        if (compareFunction(prew, current) > 0) {
+        if (compareFunction(prev, current) > 0) {
           count++;
-          this[i] = prew;
+          this[i] = prev;
           this[i - 1] = current;
         }
       }
