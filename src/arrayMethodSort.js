e552facd1830fd89ce5e38f1491d747751cc59d0
ihorivna007 <emmakorovnik@gmail.com>
2022-12-22 14:19:57 +0200

    initial commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,39 @@
 /**
  * Implement method Sort
  */
+function compareFunction(current, next) {
+  const firstValue = String(current);
+  const secondValue = String(next);
+
+  if (firstValue > secondValue) {
+    return 1;
+  } else if (firstValue === secondValue) {
+    return 0;
+  }
+
+  return -1;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(callback = compareFunction) {
+    let counter = 0;
+
+    do {
+      counter = 0;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        const current = this[i];
+        const next = this[i + 1];
+
+        if (callback(current, next) > 0) {
+          this[i] = next;
+          this[i + 1] = current;
+          counter++;
+        }
+      }
+    } while (counter > 0);
+
+    return this;
   };
 }
 
