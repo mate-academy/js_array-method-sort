57d8d8e926d76777e2dde4b8cdcfbe5a8975da13
Karadzha Mykola <nicholaskaradzha@gmail.com>
2022-12-27 23:28:16 +0000

    sorting method implemented

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,38 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const defaultSorting = (a, b) => {
+    const stringA = String(a);
+    const stringB = String(b);
+
+    if (stringA > stringB) {
+      return 1;
+    } else if (stringA === stringB) {
+      return 0;
+    } else {
+      return -1;
+    }
+  };
+
+  [].__proto__.sort2 = function(compareFunction = defaultSorting) {
+    let isChanged;
+
+    do {
+      isChanged = false;
+
+      for (let i = 1; i < this.length; i++) {
+        const previousValue = this[i - 1];
+        const currentValue = this[i];
+
+        if (compareFunction(previousValue, currentValue) > 0) {
+          this[i] = previousValue;
+          this[i - 1] = currentValue;
+          isChanged = true;
+        }
+      }
+    } while (isChanged === true);
+
+    return this;
   };
 }
 
