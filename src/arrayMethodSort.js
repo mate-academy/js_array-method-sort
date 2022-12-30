ea5b74fc4bcd55734624dd6dc61ef4f829fc5637
Taras Baran <baranprivat22@gmail.com>
2022-12-30 20:54:43 +0200

    finished

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,42 @@
 /**
  * Implement method Sort
  */
+const compareFunctionAddition = (a, b) => {
+  const strA = a.toString();
+  const strB = b.toString();
+
+  if (strA > strB) {
+    return 1;
+  }
+
+  if (strA === strB) {
+    return 0;
+  }
+
+  return -1;
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareFunctionAddition) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const a = this[i - 1];
+        const b = this[i];
+
+        if (compareFunction(a, b) > 0) {
+          this[i] = a;
+          this[i - 1] = b;
+
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
