bed87e53ae8f96be12d046897af27290babed171
Yuliia Andrieieva <andrieieva.y@gmail.com>
2023-01-02 17:59:40 +0200

    Sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,40 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = sorted) {
+    let done = false;
+
+    while (!done) {
+      done = true;
+
+      for (let i = 1; i < this.length; i++) {
+        const first = this[i - 1];
+        const second = this[i];
+
+        if (compareFunction(first, second) > 0) {
+          done = false;
+          this[i - 1] = second;
+          this[i] = first;
+        }
+      }
+    }
+
+    return this;
   };
 }
+
+const sorted = (a, b) => {
+  const strFirst = a.toString();
+  const strSecond = b.toString();
+
+  if (strFirst > strSecond) {
+    return 1;
+  } else
+  if (strFirst < strSecond) {
+    return -1;
+  } else {
+    return 0;
+  }
+};
 
 module.exports = applyCustomSort;
