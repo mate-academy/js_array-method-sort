7d46d4b73c157486346de7662e0092963aa0cd08
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2023-01-04 17:36:23 +0200

    array sort - completed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,44 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let callback = compareFunction;
+    let prev;
+    let curr;
+    let count;
+
+    const defaultFunction = function(a, b) {
+      const aStr = String(a);
+      const bStr = String(b);
+
+      if (aStr > bStr) {
+        return 1;
+      } else if (aStr === bStr) {
+        return 0;
+      } else {
+        return -1;
+      }
+    };
+
+    if (arguments.length === 0) {
+      callback = defaultFunction;
+    }
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        prev = this[i - 1];
+        curr = this[i];
+
+        if (callback(prev, curr) > 0) {
+          this[i - 1] = curr;
+          this[i] = prev;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
