82558a341643450050f201bfcaa641b090980422
Tetiana.Boiar <taniavasylchyk@gmail.com>
2023-01-05 21:11:52 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,41 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const compareAsStrings = (a, b) => {
+      const stringA = String(a);
+      const stringB = String(b);
+
+      if (stringA > stringB) {
+        return 1;
+      }
+
+      if (stringA < stringB) {
+        return -1;
+      }
+
+      return 0;
+    };
+
+    const compare = !compareFunction ? compareAsStrings : compareFunction;
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compare(prev, current) > 0) {
+          this[i - 1] = current;
+          this[i] = prev;
+
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
