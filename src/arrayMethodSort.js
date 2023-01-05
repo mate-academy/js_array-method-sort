a20fb883388980010bf569c6a46af0fa5f73eb1d
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-01-05 12:39:00 +0200

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,37 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
+function compareAsString(a, b) {
+  if (String(a) > String(b)) {
+    return 1;
+  }
+
+  if (String(a) < String(b)) {
+    return -1;
+  }
+
+  return 0;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compare = compareAsString) {
+    let counter;
+
+    do {
+      counter = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const previous = this[i - 1];
+        const curent = this[i];
+
+        if (compare(previous, curent) > 0) {
+          this[i - 1] = curent;
+          this[i] = previous;
+          counter++;
+        }
+      }
+    } while (counter > 0);
+
+    return this;
   };
 }
 
