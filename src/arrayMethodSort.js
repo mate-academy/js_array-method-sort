9bc04556340e4fb51d276dd5d9d2624c137018af
svitjojo <kornotavr@gmail.com>
2023-01-05 16:22:53 +0200

    Result

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,21 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < (this.length - i - 1); j++) {
+        let sortMethod = this[j].toString() > this[j + 1].toString();
+
+        if (arguments.length > 0) {
+          sortMethod = compareFunction(this[j], this[j + 1]) > 0;
+        }
+
+        if (sortMethod) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
