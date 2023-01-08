e12106c1b621e2f7c10b3fefea0dc7fdb89ceba7
Tretiak Serhii <tretyak8936@gmail.com>
2023-01-08 18:21:26 +0200

    solved task

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,40 @@
 /**
  * Implement method Sort
  */
+
+const compareAsFunction = (a, b) => {
+  const first = String(a);
+  const second = String(b);
+
+  if (first > second) {
+    return 1;
+  } else if (first === second) {
+    return 0;
+  }
+
+  return -1;
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsFunction) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prevElement = this[i - 1];
+        const currentElement = this[i];
+
+        if (compareFunction(prevElement, currentElement) >= 0) {
+          this[i - 1] = currentElement;
+          this[i] = prevElement;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
