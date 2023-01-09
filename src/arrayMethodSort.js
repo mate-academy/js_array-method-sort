e85135e82ddb21da99b7ede8b6baf62d77178fdc
Ihor Mariash <imariash86@gmail.com>
2023-01-09 19:56:38 +0200

    if else was changed to is statements

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -10,11 +10,13 @@ function applyCustomSort() {
 
     if (stringA > stringB) {
       return 1;
-    } else if (stringA < stringB) {
+    }
+
+    if (stringA < stringB) {
       return -1;
-    } else {
-      return 0;
-    };
+    }
+
+    return 0;
   };
 
   [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
