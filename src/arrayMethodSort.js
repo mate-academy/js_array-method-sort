c17bec2daa2ac72fa86f8bdd49594c349d3550ff
Jaxxxon78 <jaxxxon78@ukr.net>
2023-01-20 20:58:25 +0200

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,33 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareStrings) {
+    for (let i = 0, temp; i < this.length; i++) {
+      for (let j = i; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          temp = this[j];
+          this[j] = this[i];
+          this[i] = temp;
+        }
+      }
+    }
+
+    return this;
+  };
+
+  const compareStrings = (a, b) => {
+    const stringA = String(a);
+    const stringB = String(b);
+
+    if (stringA > stringB) {
+      return 1;
+    }
+
+    if (stringA === stringB) {
+      return 0;
+    }
+
+    return -1;
   };
 }
-
 module.exports = applyCustomSort;
