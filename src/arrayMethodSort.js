622fe4801dcb19b0c31ae2c08b31f66bd5b27438
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-01-25 20:37:58 +0200

    Solution method sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -18,22 +18,22 @@ function applyCustomSort() {
   };
 
   [].__proto__.sort2 = function(compareFunction = compareAsString) {
-    let count;
+    let step;
 
     do {
-      count = 0;
+      step = 0;
 
       for (let i = 1; i < this.length; i++) {
         const prev = this[i - 1];
         const current = this[i];
 
         if (compareFunction(prev, current) > 0) {
-          count++;
+          step++;
           this[i - 1] = current;
           this[i] = prev;
         }
       };
-    } while (count > 0);
+    } while (step > 0);
 
     return this;
   };
