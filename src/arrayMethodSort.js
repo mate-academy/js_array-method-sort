164089bc78adac2998116e93159c32eef3fe07f0
bir3791 <bir3971@gmail.com>
2023-01-28 12:07:09 +0100

    resolve

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,39 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareStrings) {
+    if (this.length === 1) {
+      return this;
+    }
+
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        if (compareFunction(this[i - 1], this[i]) > 0) {
+          [[this[i - 1]], [this[i]]] = [[this[i]], [this[i - 1]]];
+          count++;
+        };
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
+
+const compareStrings = function(a, b) {
+  const stringA = String(a);
+  const stringB = String(b);
+
+  if (stringA > stringB) {
+    return 1;
+  } else if (stringA === stringB) {
+    return 0;
+  } else {
+    return -1;
+  }
+};
 
 module.exports = applyCustomSort;
