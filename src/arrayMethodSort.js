a4e32b5f860574b1afd67813fc8826ccdc7f4a08
bir3791 <bir3971@gmail.com>
2023-01-28 19:42:20 +0100

    fixed coments

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -12,15 +12,20 @@ function applyCustomSort() {
     let count;
 
     do {
-      count = 0;
+      count = false;
 
       for (let i = 1; i < this.length; i++) {
         if (compareFunction(this[i - 1], this[i]) > 0) {
-          [[this[i - 1]], [this[i]]] = [[this[i]], [this[i - 1]]];
-          count++;
+          const previous = this[i - 1];
+          const current = this[i];
+
+          this[i - 1] = current;
+          this[i] = previous;
+
+          count = true;
         };
       }
-    } while (count > 0);
+    } while (count);
 
     return this;
   };
@@ -32,11 +37,13 @@ const compareStrings = function(a, b) {
 
   if (stringA > stringB) {
     return 1;
-  } else if (stringA === stringB) {
-    return 0;
-  } else {
-    return -1;
   }
+
+  if (stringA === stringB) {
+    return 0;
+  }
+
+  return -1;
 };
 
 module.exports = applyCustomSort;
