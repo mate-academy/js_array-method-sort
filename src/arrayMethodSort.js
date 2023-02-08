8ead2f7f3a5c77af21c591992307fd236bb9b5ca
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2023-02-09 00:32:01 +0200

    develop

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,28 @@
 /**
  * Implement method Sort
  */
+const compareStrings = (a, b) => a.toString() > b.toString();
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareStrings) {
+    let flag;
+
+    do {
+      flag = true;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          this[i] = prev;
+          this[i - 1] = current;
+          flag = false;
+        }
+      }
+    } while (!flag);
+
+    return this;
   };
 }
 
