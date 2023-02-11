e05a89860ebc753490f4f9fccc6f1fa1581a6f07
ivanm2706 <ivanmorhun664@gmail.com>
2023-02-11 18:16:57 +0200

    updated code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -10,30 +10,32 @@ function applyCustomSort() {
 
     if (strA > strB) {
       return 1;
-    } else if (strA === strB) {
-      return 0;
-    } else {
-      return -1;
     }
+    
+    if (strA === strB) {
+      return 0;
+    }
+    
+    return -1;
   };
 
   [].__proto__.sort2 = function(compareFunction = compareDefault) {
     let check;
-    
+
     do {
       check = false;
 
       for (let i = 1; i < this.length; i++) {
-        let prev = this[i - 1];
-        let curr = this[i]
-  
+        const prev = this[i - 1];
+        const curr = this[i];
+
         if (compareFunction(curr, prev) < 0) {
           this[i] = prev;
           this[i - 1] = curr;
           check = true;
         }
       }
-    } while (check)
+    } while (check);
 
     return this;
   };
