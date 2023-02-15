b33764e32c33c207a53e1235da031b6a78049edc
Serhii Nikolin <sergeynikolin@gmail.com>
2023-02-15 12:19:42 +0200

    Task done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,31 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let step;
+    let callback = compareFunction;
+
+    if (!compareFunction) {
+      callback = function(curr, prev) {
+        return String(curr) < String(prev) ? -1 : 1;
+      };
+    }
+
+    do {
+      step = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const curr = this[i];
+        const prev = this[i - 1];
+
+        if (callback(curr, prev) < 0) {
+          this[i - 1] = curr;
+          this[i] = prev;
+          step++;
+        }
+      }
+    } while (step > 0);
+
+    return this;
   };
 }
 
