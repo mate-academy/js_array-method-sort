fe6c2b0ccc2d08c3808e6e3167befd5bbea7c8f9
Eduard <Eduard@eduards-macbook-pro.home>
2023-02-16 14:04:43 +0100

    sorted1

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -21,14 +21,16 @@ function applyCustomSort() {
     let count = 0;
 
     do {
+      count = 0;
+
       for (let i = 1; i < this.length; i++) {
         const prev = this[i - 1];
         const current = this[i];
 
         if (compareFunction(prev, current) > 0) {
+          count++;
           this[i - 1] = current;
           this[i] = prev;
-          count++;
         }
       }
     } while (count > 0);
