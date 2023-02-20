8b32647037e88fe1289c71aab670c78dfb7b3152
Ch0p3r84 <marin.iliev@yahoo.com>
2023-02-20 12:44:12 +0200

    Solution revisited

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -20,22 +20,22 @@ function compareAsStrings(first, second) {
 
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
-    let counter;
+    let isContinue;
 
     do {
-      counter = 0;
+      isContinue = false;
 
       for (let i = 1; i < this.length; i++) {
         const prev = this[i - 1];
         const current = this[i];
 
         if (compareFunction(prev, current) > 0) {
-          counter++;
+          isContinue = true;
           this[i - 1] = current;
           this[i] = prev;
         }
       }
-    } while (counter > 0);
+    } while (isContinue === true);
 
     return this;
   };
