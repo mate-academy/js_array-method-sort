6a3fb9baf52ce928090a65a53a4a3d92edb54102
Oleg-qaMar21 <olegka.zagorodnii.com>
2023-02-21 20:38:39 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,6 +6,51 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     // write code here
+    let a;
+    let count;
+
+    if (compareFunction === undefined) {
+      do {
+        count = 0;
+
+        for (let i = 1; i < this.length; i++) {
+          if (this[i].toString() < this[i - 1].toString()) {
+            count++;
+            a = this[i];
+            this[i] = this[i - 1];
+            this[i - 1] = a;
+          }
+        }
+      } while (count > 0);
+    } else if (typeof this[0] === 'number') {
+      do {
+        count = 0;
+
+        for (let i = 1; i < this.length; i++) {
+          if (this[i] < this[i - 1]) {
+            count++;
+            a = this[i];
+            this[i] = this[i - 1];
+            this[i - 1] = a;
+          }
+        }
+      } while (count > 0);
+    } else {
+      do {
+        count = 0;
+
+        for (let i = 1; i < this.length; i++) {
+          if (this[i].localeCompare(this[i - 1]) < 0) {
+            count++;
+            a = this[i];
+            this[i] = this[i - 1];
+            this[i - 1] = a;
+          }
+        }
+      } while (count > 0);
+    }
+
+    return this;
   };
 }
 
