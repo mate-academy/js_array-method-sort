6f880c2c933cc5f3fc865b592cd99188ba75f600
Sergey Lipikhin <football3@ukr.net>
2023-02-23 14:51:44 +0200

    Create sort2

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,34 @@
 /**
  * Implement method Sort
  */
+const compareAsStrings = (a, b) => (
+  String(a) > String(b)
+);
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    if (this.length < 2) {
+      return this;
+    }
+
+    let isChanged;
+
+    do {
+      isChanged = false;
+
+      for (let i = 1; i < this.length; i++) {
+        if (compareFunction(this[i - 1], this[i]) > 0) {
+          const temp = this[i - 1];
+
+          this[i - 1] = this[i];
+          this[i] = temp;
+
+          isChanged = true;
+        }
+      }
+    } while (isChanged);
+
+    return this;
   };
 }
 
