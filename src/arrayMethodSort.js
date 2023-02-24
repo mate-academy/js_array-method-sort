b36c102f968b22a92f15f47346f1a04da10c7b18
Oleksandr Nedobezhkin <nedobezhkin_ad@yahoo.com>
2023-02-24 14:49:52 +0200

    Array method Sort v.0.1 - Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let sort = compareFunction;
+
+    if (!sort) {
+      sort = function(a, b) {
+        if (String(a) > String(b)) {
+          return 1;
+        } else if (String(a) < String(b)) {
+          return -1;
+        }
+
+        return 0;
+      };
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (sort(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
