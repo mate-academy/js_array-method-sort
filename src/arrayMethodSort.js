0deb417743dc19591c884854f1d20e07bcf2009a
gretoq <dovhun3d@gmail.com>
2023-02-24 22:29:54 +0200

    My implementation the iterator array method "sort"

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,24 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const defaultCompare = (a, b) => {
+      return a.toString() > b.toString();
+    };
+
+    const useFunction = compareFunction || defaultCompare;
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (useFunction(this[i], this[j]) > 0) {
+          const firstElement = this[i];
+
+          this[i] = this[j];
+          this[j] = firstElement;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
