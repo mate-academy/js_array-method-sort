315c9dbc10b070e75f1f54b212020f8c2d48dbb2
Iryna Serniuk <irynaserniuk@gmail.com>
2023-02-27 18:50:03 +0200

    Some changes are added

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,21 +4,9 @@
  * Implement method Sort
  */
 
-const compareAsStrings = (a, b) => {
-  const stringA = String(a);
-  const stringB = String(b);
-
-  if (stringA > stringB) {
-    return 1;
-  } else if (stringA === stringB) {
-    return 0;
-  } else {
-    return -1;
-  }
-};
-
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+  [].__proto__.sort2 = function(compareFunction =
+  (a, b) => String(a) > String(b)) {
     let count;
 
     do {
