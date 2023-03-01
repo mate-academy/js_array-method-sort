12afba1ab4345497352362b61653f7ffa71aaa87
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-03-01 17:02:04 +0200

    sort method was implement

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,22 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        const isBigger = compareFunction
+          ? compareFunction(this[j], this[j + 1]) > 0
+          : this[j].toString() > this[j + 1].toString();
+
+        if (isBigger) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
