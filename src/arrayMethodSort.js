f0f505df6e4170bbc94c569d0d3e47fd2b573e87
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-03-02 18:25:04 +0200

    counter was become boolen

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -8,7 +8,7 @@ function applyCustomSort() {
     let count;
 
     do {
-      count = 0;
+      count = false;
 
       for (let i = 1; i < this.length; i++) {
         const prev = this[i - 1];
@@ -17,10 +17,10 @@ function applyCustomSort() {
         if (compareFunction(prev, current) > 0) {
           this[i - 1] = current;
           this[i] = prev;
-          count++;
+          count = true;
         }
       }
-    } while (count > 0);
+    } while (count);
 
     return this;
   };
