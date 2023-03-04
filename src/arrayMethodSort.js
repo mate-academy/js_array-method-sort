9481ce0b935e29b4ad0aa964c1810dad6e49756b
Serhii Lysenko <diom34@gmail.com>
2023-03-04 14:43:24 +0200

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,10 +3,40 @@
 /**
  * Implement method Sort
  */
+const compareByDefault = (a, b) => {
+  const stringA = String(a);
+  const stringB = String(b);
+
+  if (stringA > stringB) {
+    return 1;
+  } else if (stringA === stringB) {
+    return 0;
+  } else {
+    return -1;
+  }
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(callback = compareByDefault) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const previousItem = this[i - 1];
+        const currentItem = this[i];
+
+        if (callback(previousItem, currentItem) > 0) {
+          count++;
+          this[i - 1] = currentItem;
+          this[i] = previousItem;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
-}
+};
 
 module.exports = applyCustomSort;
