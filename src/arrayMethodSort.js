29a2120d36dcdd75569577e449ae60dbe396bf3f
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-03-06 23:49:39 +0200

    make variables and implement destructuring assigment

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -7,15 +7,15 @@ function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
+        const currentEl = this[j];
+        const nextEl = this[j + 1];
+
         const isBigger = compareFunction
-          ? compareFunction(this[j], this[j + 1]) > 0
-          : this[j].toString() > this[j + 1].toString();
+          ? compareFunction(currentEl, nextEl) > 0
+          : currentEl.toString() > nextEl.toString();
 
         if (isBigger) {
-          const temp = this[j];
-
-          this[j] = this[j + 1];
-          this[j + 1] = temp;
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
         }
       }
     }
