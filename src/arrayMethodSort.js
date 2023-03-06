0f9becb7e0d13c01a492dd1dfc2a952a0724aefa
Maksym <maksymmaliuk98@gmail.com>
2023-03-06 12:53:04 +0200

    count changed to changePosition

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,30 +5,27 @@
  */
 
 const compareStrings = (a, b) => {
-  const stringA = String(a);
-  const stringB = String(b);
-
-  return stringA > stringB;
+  return String(a) > String(b);
 };
 
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compareStrings) {
-    let count;
+    let changePosition;
 
     do {
-      count = 0;
+      changePosition = 0;
 
       for (let i = 1; i < this.length; i++) {
         const previous = this[i - 1];
         const current = this[i];
 
         if (compareFunction(previous, current) > 0) {
-          count++;
+          changePosition++;
           this[i - 1] = current;
           this[i] = previous;
         }
       }
-    } while (count > 0);
+    } while (changePosition > 0);
 
     return this;
   };
