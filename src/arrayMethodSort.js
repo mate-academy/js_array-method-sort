27e48d5074ec4b060b8fd667c2469b0daee829e7
Serhii Lysenko <diom34@gmail.com>
2023-03-07 12:42:20 +0200

    fixed solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -9,31 +9,33 @@ const compareByDefault = (a, b) => {
 
   if (stringA > stringB) {
     return 1;
-  } else if (stringA === stringB) {
-    return 0;
-  } else {
-    return -1;
   }
+
+  if (stringA === stringB) {
+    return 0;
+  }
+
+  return -1;
 };
 
 function applyCustomSort() {
   [].__proto__.sort2 = function(callback = compareByDefault) {
-    let count;
+    let isContinue;
 
     do {
-      count = 0;
+      isContinue = false;
 
       for (let i = 1; i < this.length; i++) {
         const previousItem = this[i - 1];
         const currentItem = this[i];
 
         if (callback(previousItem, currentItem) > 0) {
-          count++;
+          isContinue = true;
           this[i - 1] = currentItem;
           this[i] = previousItem;
         }
       }
-    } while (count > 0);
+    } while (isContinue);
 
     return this;
   };
