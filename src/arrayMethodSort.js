1f59388a50a988b57c676aa9fd754f57d6f6d78b
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-03-16 21:46:48 +0200

    method sort() was created

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,39 @@
 /**
  * Implement method Sort
  */
+const localeCompare = function(a, b) {
+  const firstString = String(a);
+  const secondString = String(b);
+
+  if (firstString > secondString) {
+    return 1;
+  } else if (firstString === secondString) {
+    return 0;
+  } else {
+    return -1;
+  }
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = localeCompare) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        const prev = this[i];
+
+        if (compareFunction(prev, this[i + 1]) > 0) {
+          count++;
+
+          this[i] = this[i + 1];
+          this[i + 1] = prev;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
