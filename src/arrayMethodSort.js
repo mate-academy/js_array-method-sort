904a6efb1a0c98c97af0582ce1dc7db81dc6ac41
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-03-21 00:09:29 +0200

    Implemented sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,24 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const compareByDefault = (a, b) => String(a) <= String(b) ? 0 : 1;
+
+  [].__proto__.sort2 = function(compareFunction = compareByDefault) {
+    let shift = 0;
+
+    for (let j = 0; j < this.length; j++) {
+      for (let i = 0; i < this.length - 1 - shift; i++) {
+        const compareResult = compareFunction(this[i], this[i + 1]);
+
+        if (compareResult > 0) {
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
+        }
+      }
+
+      shift++;
+    }
+
+    return this;
   };
 }
 
