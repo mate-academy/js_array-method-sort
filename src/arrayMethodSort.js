816fb830607266a3c697ecb19a8613beef108b5a
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-03-22 12:23:27 +0200

    upgraded sort algorythm

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -8,18 +8,22 @@ function applyCustomSort() {
 
   [].__proto__.sort2 = function(compareFunction = compareByDefault) {
     let shift = 0;
+    let counter = 0;
 
-    for (let j = 0; j < this.length; j++) {
+    do {
+      counter = 0;
+
       for (let i = 0; i < this.length - 1 - shift; i++) {
         const compareResult = compareFunction(this[i], this[i + 1]);
 
         if (compareResult > 0) {
+          counter++;
           [this[i], this[i + 1]] = [this[i + 1], this[i]];
         }
       }
 
       shift++;
-    }
+    } while (counter > 0);
 
     return this;
   };
