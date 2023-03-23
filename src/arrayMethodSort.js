526ae5042eb27238f294a24c59fbabb126a1b492
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-03-23 18:12:44 +0200

    sort function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,35 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const defaultSort = (elementA, elemenrB) => {
+      return String(elementA) > String(elemenrB)
+        ? 1
+        : String(elementA) === String(elemenrB)
+          ? 0
+          : -1;
+    };
+
+    const currentSort = compareFunction === undefined
+      ? defaultSort
+      : compareFunction;
+
+    let count = 0;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        if (currentSort(this[i - 1], this[i]) > 0) {
+          const transferValue = this[i - 1];
+
+          this[i - 1] = this[i];
+          this[i] = transferValue;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
