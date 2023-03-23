6f7113b05e893441895fd581396058e9b37627bc
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-03-23 12:46:16 +0100

    fix code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -20,12 +20,12 @@ const compareAsStrings = function(a, b) {
 
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
-    let count;
+    let count = 0;
 
     do {
       count = 0;
 
-      for (let i = 0; i < this.length; i++) {
+      for (let i = 0; i < this.length - 1; i++) {
         const prev = this[i];
         const current = this[i + 1];
 
