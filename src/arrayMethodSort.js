12b5fc9c7508716e9e98aba290c0418e49173ab3
Nazarii Chepil <nazzikk1998@gmail.com>
2023-03-24 14:33:34 +0200

    divided the conditions

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -7,7 +7,9 @@ function applyCustomSort() {
 
     if (stringA > stringB) {
       return 1;
-    } else if (stringA === stringB) {
+    }
+
+    if (stringA === stringB) {
       return 0;
     }
 
