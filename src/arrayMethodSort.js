0db6c13295a18a22643c9fe2ce5967f3a3bedafd
Daria <hamdsad8@gmail.com>
2023-03-25 12:45:08 +0200

    add custom sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,30 @@
 /**
  * Implement method Sort
  */
+const defaultCompare = (prev, curr) => String(prev) > String(curr) ? 1 : -1;
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(
+    compareFunction = defaultCompare
+  ) {
+    for (let i = 0; i < this.length - 1; i++) {
+      let minIndex = i;
+
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[j], this[minIndex]) < 0) {
+          minIndex = j;
+        }
+      }
+
+      if (minIndex !== i) {
+        const temp = this[i];
+
+        this[i] = this[minIndex];
+        this[minIndex] = temp;
+      }
+    }
+
+    return this;
   };
 }
 
