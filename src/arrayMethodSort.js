eebde972b1468e059e3ad078e31c14cf7617a64f
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-03-25 17:13:35 +0300

    task solved

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,25 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (a, b) => String(a) > String(b)) { // eslint-disable-line
+    let count = 0;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          this[i - 1] = current;
+          this[i] = prev;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
