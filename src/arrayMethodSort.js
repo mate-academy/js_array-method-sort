56ec55ddcce6c5c399907d996b08b2969e93ad8a
Kyryl <kika_domination@yahoo.com>
2023-03-26 15:28:58 +0300

    add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,28 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(callback = sortWithoutParams) {
+    for (let i = 1; i < this.length; i++) {
+      for (let j = 0; j < this.length; j++) {
+        const current = this[i];
+        const prev = this[j];
+
+        if (callback(prev, current) > 0) {
+          this[i] = prev;
+          this[j] = current;
+        }
+      }
+    }
+
+    return this;
   };
+}
+
+function sortWithoutParams() {
+  const firstString = `${arguments[0]}`;
+  const secondString = `${arguments[1]}`;
+
+  return firstString > secondString ? 1 : -1;
 }
 
 module.exports = applyCustomSort;
