791aed1c7531530ccd443cb96508277c348b226d
victor buldenko <gookidoo@gmail.com>
2023-03-28 23:28:37 +0300

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,43 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const defCallback = (a, b) => {
+    let first = a;
+    let second = b;
+
+    if (typeof a === 'number') {
+      first = String(a);
+      second = String(b);
+    }
+
+    if (first > second) {
+      return 1;
+    };
+
+    if (first < second) {
+      return -1;
+    };
+
+    return 0;
+  };
+
+  [].__proto__.sort2 = function(compareFunction = defCallback) {
+    const sort = () => {
+      for (let i = 0; i < this.length; i++) {
+        if (compareFunction(this[i], this[i + 1]) > 0) {
+          const first = this[i];
+          const second = this[i + 1];
+
+          this[i] = second;
+          this[i + 1] = first;
+          sort();
+        }
+      }
+    };
+
+    sort();
+
+    return this;
   };
 }
 
