378216bc5bd196757788ace7d3af3fd6e63f0c9d
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-03-29 15:04:00 +0200

    reduce loops

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,21 +6,16 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     let rememberValue;
-    // [3, 12, 2, 11]
 
-    if (compareFunction === undefined) {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = i + 1; j < this.length; j++) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction === undefined) {
           if (String(this[i]) > String(this[j])) {
             rememberValue = this[i];
             this[i] = this[j];
             this[j] = rememberValue;
           }
-        }
-      }
-    } else {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = i + 1; j < this.length; j++) {
+        } else {
           if (compareFunction(this[i], this[j]) > 0) {
             rememberValue = this[i];
             this[i] = this[j];
