1904c9964b5677a0c383b64bd0c4fc6948fe9693
Igor Galkin <igor_galkin@ukr.net>
2023-03-29 18:20:41 +0300

    1st commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,38 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const defaultFunction = (a, b) => {
+    const prevElement = String(a);
+    const currentElement = String(b);
+
+    if (currentElement > prevElement) {
+      return -1;
+    } else if (currentElement < prevElement) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+
+  [].__proto__.sort2 = function(compareFunction = defaultFunction) {
+    let needIteration = true;
+
+    while (needIteration) {
+      needIteration = false;
+
+      for (let i = 1; i < this.length; i++) {
+        let temp;
+
+        if (compareFunction(this[i - 1], this[i]) > 0) {
+          temp = this[i - 1];
+          this[i - 1] = this[i];
+          this[i] = temp;
+          needIteration = true;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
