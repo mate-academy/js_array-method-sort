9d0b92f4d01486d4763c21ad7ac66cf3a990c61d
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-03-30 14:03:18 +0200

    bubble sort was implemented

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,38 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
+const compareAsStrings = (a, b) => {
+  const stringA = String(a);
+  const stringB = String(b);
+
+  if (stringA > stringB) {
+    return 1;
+  } else if (stringA === stringB) {
+    return 0;
+  } else {
+    return -1;
+  }
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(callback = compareAsStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (callback(prev, current) > 0) {
+          this[i - 1] = current;
+          this[i] = prev;
+          count++;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
