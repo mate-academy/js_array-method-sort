ede3d0f2886097001395d31a2a51c2e0e29f7edb
Nadiia Romanskaya <97957968+NadiiaR@users.noreply.github.com>
2023-04-05 20:29:28 +0300

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,22 +5,22 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compareAsString) {
-    let count;
+    let iteration;
 
     do {
-      count = 0;
+      iteration = false;
 
       for (let i = 1; i < this.length; i++) {
         const prev = this[i - 1];
         const current = this[i];
 
         if (compareFunction(prev, current) > 0) {
-          count++;
+          iteration = true;
           this[i - 1] = current;
           this[i] = prev;
         }
       }
-    } while (count > 0);
+    } while (iteration === true);
 
     return this;
   };
