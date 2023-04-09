62a9692997e3d21b22ba52e3ef7351f3046e7490
Roma Tsvindin <tsvindin1995@gmail.com>
2023-04-09 17:42:29 +0300

    array method sort solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,26 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length - 1; i++) {
+      let minIndex = i;
+
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction
+          ? compareFunction(this[j], this[minIndex]) < 0
+          : `${this[j]}` < `${this[minIndex]}`) {
+          minIndex = j;
+        }
+      }
+
+      if (minIndex !== i) {
+        const temp = this[i];
+
+        this[i] = this[minIndex];
+        this[minIndex] = temp;
+      }
+    }
+
+    return this;
   };
 }
-
 module.exports = applyCustomSort;
