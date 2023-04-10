44a717044cd7e28766c9d69df605b4386b8abb67
Veronika Pastushkova <d9169393507@gmai.com>
2023-04-10 15:42:17 +0300

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,12 +1,45 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const strCompare = (a, b) => {
+    const stringA = String(a);
+    const stringB = String(b);
+
+    if (stringA > stringB) {
+      return 1;
+    } else if (stringA === stringB) {
+      return 0;
+    } else {
+      return -1;
+    }
   };
-}
+
+  [].__proto__.sort2 = function(compareFunction = strCompare) {
+    // write code here
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const current = this[i];
+        const prev = this[i - 1];
+
+        if (compareFunction(prev, current) > 0) {
+          count++;
+          this[i - 1] = current;
+          this[i] = prev;
+        }
+      }
+    } while (count > 0);
+
+    return this;
+  };
+};
 
 module.exports = applyCustomSort;
