07004f5c765edfa742ee6c2f6c26e3d3c26f6b9f
адмін <admin@Mac-Max.local>
2023-04-10 23:15:06 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,25 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const _compareFunction = compareFunction
+      ? (a, b) => compareFunction(a, b) < 0
+      : (a, b) => a.toString() < b.toString();
+
+    for (let i = 0; i < this.length - 1; i++) {
+      let minIndex = i;
+
+      for (let j = i + 1; j < this.length; j++) {
+        if (_compareFunction(this[j], this[minIndex])) {
+          minIndex = j;
+        }
+      }
+
+      if (minIndex !== i) {
+        [this[i], this[minIndex]] = [this[minIndex], this[i]];
+      }
+    }
+
+    return this;
   };
 }
 
