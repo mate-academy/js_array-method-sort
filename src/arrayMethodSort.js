8238c641ba9032cdaf53d820cad627926a520278
Volodymyr Zadorozhnyi <Torsida@gmail.com>
2023-04-10 22:37:55 +0300

    first version

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,25 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (compareFunction) {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          if (compareFunction(this[j], this[i]) < 0) {
+            [this[i], this[j]] = [this[j], this[i]];
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          if (String(this[j]) < String(this[i])) {
+            [this[i], this[j]] = [this[j], this[i]];
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
-
 module.exports = applyCustomSort;
