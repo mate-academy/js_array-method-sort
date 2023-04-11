942b58017f2b8e8d8e20b1d9a515e9e70f358df8
AnastassiaH <nastyagurska@gmail.com>
2023-04-11 09:19:35 +0300

    optimize code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,37 +3,28 @@
 /**
  * Implement method Sort
  */
+const compareFunctionByDefault = (a, b) => {
+  const stringA = a.toString();
+  const stringB = b.toString();
+
+  return stringA > stringB ? 1 : 0;
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction = compareFunctionByDefault) {
     // write code here
-
-    if (compareFunction) {
-      for (let x = 0; x < this.length - 1; x++) {
-        for (let i = 0; i < this.length - 1; i++) {
-          const a = this[i];
-          const b = this[i + 1];
-
-          if (compareFunction.bind(this)(a, b) > 0) {
-            this[i] = b;
-            this[i + 1] = a;
-          };
-        };
-      };
-
-      return this;
-    };
 
     for (let x = 0; x < this.length - 1; x++) {
       for (let i = 0; i < this.length - 1; i++) {
-        const a = this[i].toString();
-        const b = this[i + 1].toString();
+        const a = this[i];
+        const b = this[i + 1];
 
-        if (a > b) {
+        if (compareFunction.bind(this)(a, b) > 0) {
           this[i] = typeof this[i] === 'number' ? +b : b;
-          this[i + 1] = typeof this[i + 1] === 'number' ? +a : a;
-        }
-      }
-    }
+          this[i + 1] = typeof this[i] === 'number' ? +a : a;
+        };
+      };
+    };
 
     return this;
   };
