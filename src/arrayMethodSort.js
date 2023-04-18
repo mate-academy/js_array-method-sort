5b485eff63e6206494c1ae5d6f96ddbbe433db0a
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-04-18 12:27:44 +0200

    after code review corrections

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -23,21 +23,21 @@ function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compareAsString) {
     let count;
 
-    if (compareFunction === undefined) {
-      return this;
+    if (typeof compareFunction !== 'function') {
+      throw new Error('compareFunction is not valid');
     }
 
     do {
       count = 0;
 
       for (let i = 0; i < this.length - 1; i++) {
-        const a = this[i];
-        const b = this[i + 1];
+        const current = this[i];
+        const next = this[i + 1];
 
-        if (compareFunction(a, b) > 0) {
+        if (compareFunction(current, next) > 0) {
           count++;
-          this[i] = b;
-          this[i + 1] = a;
+          this[i] = next;
+          this[i + 1] = current;
         }
       }
     } while (count > 0);
