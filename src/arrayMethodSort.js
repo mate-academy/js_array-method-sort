b47485a64b6d1094d221df861bda6974dbb89d7f
DefaultGitUser <usingforgit@gmail.com>
2023-04-19 16:05:49 +0300

    Sol

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,37 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  function compareAsStrings(a, b) {
+    const StringA = String(a);
+    const StringB = String(b);
+
+    if (StringA > StringB) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          count++;
+          this[i - 1] = current;
+          this[i] = prev;
+        }
+      }
+    }
+    while (count);
+
+    return this;
   };
-}
 
 module.exports = applyCustomSort;
