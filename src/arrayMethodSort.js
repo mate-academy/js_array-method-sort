8d8b4e122eed1a8e9d708735211c63bef59e43e4
Andrii Dudar <andrij.dudar@gmail.com>
2023-04-30 12:54:09 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -7,11 +7,13 @@ function applyCustomSort() {
   const compareAsStrings = (a, b) => {
     if (String(a) > String(b)) {
       return 1;
-    } else if (String(a) === String(b)) {
+    }
+
+    if (String(a) === String(b)) {
       return 0;
-    } else {
-      return -1;
-    };
+    }
+
+    return -1;
   };
 
   [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
