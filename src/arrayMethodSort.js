41ced93b9cb9bfcfb32eef4bf2bdcbdb758d7b99
Ruslan Pashkov <nothingsfading@gmail.com>
2023-05-10 00:31:57 +0300

    Refactor sorting algorithm to improve performance

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,12 +6,12 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compare) {
     for (let i = 0; i < this.length; i++) {
-      for (let j = 0; j < this.length - 1; j++) {
-        if (compareFunction(this[j], this[j + 1]) > 0) {
-          const temp = this[j];
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          const temp = this[i];
 
-          this[j] = this[j + 1];
-          this[j + 1] = temp;
+          this[i] = this[j];
+          this[j] = temp;
         }
       }
     }
