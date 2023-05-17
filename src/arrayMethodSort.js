ebd81044007c67f4b48bee4a69d5e3815d5587f3
Volodymyr Konoplianyi <volodymyr.konoplianyi@gmail.com>
2023-05-17 16:30:40 +0300

    Improved code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,32 +3,31 @@
 /**
  * Implement method Sort
  */
+function defaultCompare(a, b) {
+  if (a.toString() > b.toString()) {
+    return 1;
+  } else if (a.toString() < b.toString()) {
+    return -1;
+  }
+
+  return 0;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    let compare = compareFunction;
+  [].__proto__.sort2 = function(compareFunction = defaultCompare) {
+    let swapped;
 
-    if (compare === undefined) {
-      compare = (a, b) => {
-        if (a.toString() > b.toString()) {
-          return 1;
-        } else if (a.toString() < b.toString()) {
-          return -1;
-        }
+    do {
+      swapped = false;
 
-        return 0;
-      };
-    }
+      for (let i = 0; i < this.length - 1; i++) {
+        if (compareFunction(this[i], this[i + 1]) > 0) {
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
 
-    for (let i = 0; i < this.length; i++) {
-      for (let j = 0; j < this.length - 1 - i; j++) {
-        if (compare(this[j], this[j + 1]) > 0) {
-          const temp = this[j];
-
-          this[j] = this[j + 1];
-          this[j + 1] = temp;
+          swapped = true;
         }
       }
-    }
+    } while (swapped);
 
     return this;
   };
