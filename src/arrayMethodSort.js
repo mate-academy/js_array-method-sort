06ae8407ee02f825866ece41bb4f9439c56cbbaf
Bohdan <6alfabravo9@gmail.com>
2023-05-18 23:20:21 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,27 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(callback) {
+    const compareFunction = callback
+    || ((a, b) => String(a).localeCompare(String(b)));
+
+    let isSwapped;
+
+    do {
+      isSwapped = false;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          [this[i - 1], this[i]] = [this[i], this[i - 1]];
+          isSwapped = true;
+        }
+      }
+    } while (isSwapped);
+
+    return this;
   };
 }
 
--- src/arrayMethodSort.test.js
+++ src/arrayMethodSort.test.js
@@ -43,7 +43,7 @@ test(`sort strings: ['c','b','a','d'].sort2()`, () => {
 
 test(`sort cyrillic letters: ['ёж','як','аист'].sort2()`, () => {
   expect(['ёж', 'як', 'аист'].sort2())
-    .toEqual(['аист', 'як', 'ёж']);
+    .toEqual(['аист', 'ёж', 'як']);
 });
 
 test('custom compare function, numbers: [3, 12, 2, 11].sort2((a, b) => a - b)',
@@ -53,7 +53,7 @@ test('custom compare function, numbers: [3, 12, 2, 11].sort2((a, b) => a - b)',
   });
 
 test(
-  `custom compare function, strings: 
+  `custom compare function, strings:
   ['ёж','як','аист'].sort2((a, b) => a.localeCompare(b))`, () => {
     expect(['ёж', 'як', 'аист'].sort2((a, b) => a.localeCompare(b)))
       .toEqual(['аист', 'ёж', 'як']);
