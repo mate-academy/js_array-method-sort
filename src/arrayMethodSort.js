37181507dd7bacfaba51f4200d5c4288ef9c2c47
Katya Zaremska <unnarummel@gmail.com>
2023-05-23 07:47:25 +0300

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,29 +1,30 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
+const defaultCompare = (a, b) => {
+  const firstString = a.toString();
+  const secondString = b.toString();
+
+  return firstString > secondString ? 1 : firstString < secondString ? -1 : 0;
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    const defaultCompare = (a, b) => {
-      const first = a.toString();
-      const second = b.toString();
+  [].__proto__.sort2 = function(compareFunction = defaultCompare) {
+    let i = 0;
 
-      return first > second ? 1 : (first < second ? -1 : 0);
-    };
+    while (i < this.length) {
+      let j = 0;
 
-    const compare = compareFunction || defaultCompare;
-
-    for (let i = 0; i < this.length; i++) {
-      for (let j = 0; j < (this.length - i - 1); j++) {
-        if (compare(this[j], this[j + 1]) > 0) {
+      while (j < this.length - i - 1) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
           [this[j], this[j + 1]] = [this[j + 1], this[j]];
         }
+        j++;
       }
+      i++;
     }
 
     return this;
   };
-}
+};
 
 module.exports = applyCustomSort;
