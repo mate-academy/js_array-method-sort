96264c292acce587498221281019511ff7249739
Anna Holovko <anutkagolovko1472@gmail.com>
2023-05-23 17:34:43 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,42 @@
 /**
  * Implement method Sort
  */
+
+const CompareAsString = (a, b) => {
+  const stringA = a.toString();
+  const stringB = b.toString();
+
+  if (stringA > stringB) {
+    return 1;
+  }
+
+  if (stringA === stringB) {
+    return 0;
+  }
+
+  return -1;
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = CompareAsString) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prevValue = this[i - 1];
+        const currentValue = this[i];
+
+        if (compareFunction(prevValue, currentValue) > 0) {
+          count++;
+          this[i - 1] = currentValue;
+          this[i] = prevValue;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
