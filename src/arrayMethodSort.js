fa3d4b8523106ce675fef5de6cd17176dbb8165f
Liza Strykharchuk <ol220681vol@gmail.com>
2023-05-24 18:10:15 +0300

    code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,30 @@
 /**
  * Implement method Sort
  */
+const compareAsStrings = function(a, b) {
+  return String(a) > String(b);
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let countObj = 1;
+
+    while (countObj > 0) {
+      countObj = 0;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        const prevObj = this[i];
+        const currentObj = this[i + 1];
+
+        if (compareFunction(prevObj, currentObj) > 0) {
+          this[i] = currentObj;
+          this[i + 1] = prevObj;
+          countObj++;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
