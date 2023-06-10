7940672f6ee639c6a143e785348c7633bea42746
AndriiYelieva <andrii.yelieva@gmail.com>
2023-06-10 10:19:33 +0300

    fix

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,24 +1,36 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
-function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    for (let i = 0; i < this.length - 1; i++) {
-      for (let j = i + 1; j < this.length; j++) {
-        if (
-          compareFunction
-            ? compareFunction(this[i], this[j]) > 0
-            : String(this[i]) > String(this[j])
-        ) {
-          const temp = this[i];
+const compareAsStrings = (a, b) => {
+  const stringA = String(a);
+  const stringB = String(b);
 
-          this[i] = this[j];
-          this[j] = temp;
+  if (stringA > stringB) {
+    return 1;
+  } else if (stringA === stringB) {
+    return 0;
+  }
+
+  return -1;
+};
+
+function applyCustomSort() {
+  [].__proto__.sort2 = function(compareFunction = compareAsStrings) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction(prev, current) > 0) {
+          count++;
+          this[i - 1] = current;
+          this[i] = prev;
         }
       }
-    }
+    } while (count > 0);
 
     return this;
   };
