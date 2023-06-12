8f525940ae674cabbc535e2f77355be535104f04
Ruslan Melnyk <ruslan_melnyk@ukr.net>
2023-06-12 20:02:20 +0300

    Solution MR

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,30 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      let indexMin = i;
+
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction) {
+          if (compareFunction(this[j], this[indexMin]) < 0) {
+            indexMin = j;
+          }
+        } else {
+          if (String(this[j]) < String(this[indexMin])) {
+            indexMin = j;
+          }
+        }
+      }
+
+      if (indexMin !== i) {
+        const lesser = this[indexMin];
+
+        this[indexMin] = this[i];
+        this[i] = lesser;
+      }
+    }
+
+    return this;
   };
 }
 
