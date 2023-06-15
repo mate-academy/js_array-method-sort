f6f9353bcae790598c63446b08021619d50e9384
Olena Bielozorova <lenka3010@gmail.com>
2023-06-15 17:24:39 +0300

    SolutionMethodSort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,17 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction
+          ? compareFunction(this[i], this[j]) > 0
+          : String(this[i]) > String(this[j])) {
+          [this[i], this[j]] = [this[j], this[i]];
+        }
+      }
+    };
+
+    return this;
   };
 }
-
 module.exports = applyCustomSort;
