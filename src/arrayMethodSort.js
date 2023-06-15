6224f7b2efb2740d88f7fd2619eea00e28c4ed4c
Kristina Bekher <bekherkr@gmail.com>
2023-06-15 12:41:10 +0200

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,34 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let count = true;
+
+    do {
+      count = false;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const current = this[i];
+
+        if (compareFunction) {
+          if (compareFunction(prev, current) > 0) {
+            count = true;
+            this[i - 1] = current;
+            this[i] = prev;
+          }
+        }
+
+        if (!compareFunction) {
+          if (`${prev}` > `${current}`) {
+            count = true;
+            this[i - 1] = current;
+            this[i] = prev;
+          }
+        }
+      }
+    } while (count);
+
+    return this;
   };
 }
 
