c1af59c69533c48d505609089b018c569ae71f68
Ruslan Vrazovskyi <dnkons@gmail.com>
2023-06-16 20:29:42 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,11 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (typeof compareFunction === 'function') {
+      return Array.prototype.sort.call(this, compareFunction);
+    } else {
+      return Array.prototype.sort.call(this);
+    }
   };
 }
 
