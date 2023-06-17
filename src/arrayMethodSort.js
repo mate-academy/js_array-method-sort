b1a7b2ad58f51bdd63a14063baf899ea746679a4
Olena Boldysheva <adv55577@gmail.com>
2023-06-17 19:25:25 +0300

    removed redundant if

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,17 +5,16 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
+    const defaultCompare = (a, b) => String(a) > String(b);
+
+    const compare = compareFunction || defaultCompare;
+
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         const current = this[i];
         const next = this[j];
 
-        if (!compareFunction && String(current) > String(next)) {
-          this[i] = next;
-          this[j] = current;
-        }
-
-        if (compareFunction && compareFunction(current, next) > 0) {
+        if (compare(current, next) > 0) {
           this[i] = next;
           this[j] = current;
         }
