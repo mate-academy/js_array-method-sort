44195a991562e423e0c85dcc16b8857f86a6e58f
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-06-19 10:06:00 +0300

    remove useless var

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -8,11 +8,9 @@ function applyCustomSort() {
   [].__proto__.sort2 = function(
     compareFunction = (a, b) => String(a) > String(b)
   ) {
-    const compare = compareFunction;
-
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        if (compare(this[i], this[j]) > 0) {
+        if (compareFunction(this[i], this[j]) > 0) {
           [this[i], this[j]] = [this[j], this[i]];
         }
       }
