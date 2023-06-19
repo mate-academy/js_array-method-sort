ea76da8cb05730b65cf7601bed2c9974b9ce493b
maxyma-sanx <maxymasan.dev@gmail.com>
2023-06-19 17:15:22 +0300

    add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,33 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let str = '';
+
+    const compare = compareFunction || defaultCompare;
+
+    while (str !== this.toString()) {
+      str = this.toString();
+
+      for (let i = 0; i < this.length - 1; i += 1) {
+        const current = this[i];
+        const next = this[i + 1];
+
+        if (compare(current, next) > 0) {
+          const currentItem = this[i];
+          const nextItem = this[i + 1];
+
+          this[i] = nextItem;
+          this[i + 1] = currentItem;
+        }
+      }
+    }
+
+    return this;
   };
+}
+
+function defaultCompare(a, b) {
+  return String(a) > String(b);
 }
 
 module.exports = applyCustomSort;
