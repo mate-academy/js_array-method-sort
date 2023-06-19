11a11b46e4a66d5116f3f0b6833d9bf4a36c8f54
artkas03 <zhonayer2017@gmail.com>
2023-06-19 16:30:35 +0300

    finished task

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,21 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (a, b) => {
+    return a.toString() > b.toString();
+  }) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let y = i + 1; y < this.length; y++) {
+        if (compareFunction(this[i], this[y]) > 0) {
+          const tempValue = this[i];
+
+          this[i] = this[y];
+          this[y] = tempValue;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
