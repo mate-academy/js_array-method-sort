c61971fd1919277508b7e2296e3202be48d940d8
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-06-22 09:43:23 +0300

    Add suggested changes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,21 +1,19 @@
 'use strict';
 
+const defaultCompareFunction = (a, b) => {
+  return String(a) > String(b);
+};
+
 /**
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    const defaultCompareFunction = (a, b) => {
-      return String(a) > String(b);
-    };
-
-    const compareFunctionNormalized = compareFunction || defaultCompareFunction;
-
+  [].__proto__.sort2 = function(compareFunction = defaultCompareFunction) {
     let end = this.length;
 
     while (end > 1) {
       for (let i = 1; i < end; i++) {
-        if (compareFunctionNormalized(this[i - 1], this[i]) > 0) {
+        if (compareFunction(this[i - 1], this[i]) > 0) {
           const temp = this[i - 1];
 
           this[i - 1] = this[i];
