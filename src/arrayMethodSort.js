5d02fc331e859f78d3b244eb4605804aa777dcb5
Valera Viachalo <valeravyachalo@gmail.com>
2023-06-26 21:45:57 +0300

    Sort method clone

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,37 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let sortFunction = compareFunction;
+
+    if (!sortFunction) {
+      sortFunction = function(firstValue, secondValue) {
+        if (String(firstValue) > String(secondValue)) {
+          return 1;
+        } else if (String(firstValue) < String(secondValue)) {
+          return -1;
+        }
+
+        return 0;
+      };
+    }
+
+    for (
+      let index = 0;
+      index < this.length;
+      index++
+    ) {
+      for (
+        let biggerIndex = index + 1;
+        biggerIndex < this.length;
+        biggerIndex++
+      ) {
+        if (sortFunction(this[index], this[biggerIndex]) > 0) {
+          [this[index], this[biggerIndex]] = [this[biggerIndex], this[index]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
