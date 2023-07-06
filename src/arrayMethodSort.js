064425f721289ecc4f28e4bcfb0a04ba85721907
Yuliia Dub <yuliia.kasaraba@gmail.com>
2023-07-06 20:27:58 +0300

    add sort metod

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,43 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const newFunction = function(elA, elB) {
+      if (typeof elA === 'string') {
+        if (elA > elB) {
+          return 1;
+        };
+      };
+
+      if (typeof elA === 'number') {
+        if (String(elA) > String(elB)) {
+          return 1;
+        }
+      }
+
+      return -1;
+    };
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        const a = this[j];
+        const b = this[j + 1];
+        let comparison;
+
+        if (compareFunction) {
+          comparison = compareFunction(a, b);
+        } else {
+          comparison = newFunction(a, b);
+        }
+
+        if (comparison > 0) {
+          this[j] = b;
+          this[j + 1] = a;
+        }
+      }
+    }
+
+    return this;
   };
-}
+};
 
 module.exports = applyCustomSort;
