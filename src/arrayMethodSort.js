6f87550c5ca758e9198a7ed30bbf36376aee7bb1
Oleg Lukyanchuk <oleeluk@gmail.com>
2023-07-12 02:16:41 +0200

    Edited Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -9,7 +9,7 @@ function applyCustomSort() {
 
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        if (compareFunction) {
+        if (typeof compareFunction === 'function') {
           if (compareFunction(this[i], this[j]) > 0) {
             const item = this[i];
 
