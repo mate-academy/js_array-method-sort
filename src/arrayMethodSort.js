f0d3ca069c34640fef1fc0033ea7e9a8eeb4c4fd
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2023-07-13 17:13:50 +0200

    Fixed task

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,18 +4,11 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction = (a, b) =>
+    a.toString() > b.toString()) {
     for (let i = 1; i < this.length; i++) {
       for (let j = 0; j < this.length - 1; j++) {
-        if (compareFunction) {
-          if (compareFunction(this[j], this[j + 1]) > 0) {
-            const temp = this[j];
-
-            this[j] = this[j + 1];
-            this[j + 1] = temp;
-          }
-
-        } else if (this[j].toString() > this[j + 1].toString()) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
           const temp = this[j];
 
           this[j] = this[j + 1];
@@ -27,5 +20,29 @@ function applyCustomSort() {
     return this;
   };
 }
+
+// function applyCustomSort() {
+//   [].__proto__.sort2 = function(compareFunction) {
+//     for (let i = 1; i < this.length; i++) {
+//       for (let j = 0; j < this.length - 1; j++) {
+//         if (compareFunction) {
+//           if (compareFunction(this[j], this[j + 1]) > 0) {
+//             const temp = this[j];
+
+//             this[j] = this[j + 1];
+//             this[j + 1] = temp;
+//           }
+//         } else if (this[j].toString() > this[j + 1].toString()) {
+//           const temp = this[j];
+
+//           this[j] = this[j + 1];
+//           this[j + 1] = temp;
+//         }
+//       }
+//     }
+
+//     return this;
+//   };
+// }
 
 module.exports = applyCustomSort;
