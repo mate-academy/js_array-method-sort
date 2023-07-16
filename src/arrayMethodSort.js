be014de64d78b89927a95a6af83090d08f6e2b94
Ksenia Skrebtsova <121960247+KseniaSkr@users.noreply.github.com>
2023-07-16 03:56:03 +0300

    SortMethod

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,22 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => String(a) > String(b) ? 1 : -1) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        if (compareFunction(this[i], this[i + 1]) > 0) {
+          count = 1;
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
+        }
+      }
+    } while (count === 1);
+
+    return this;
   };
 }
 
