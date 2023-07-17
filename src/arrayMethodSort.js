09dd8213296feea559668f24d521cb18415d8740
AndruhaMan <andreysut12@gmail.com>
2023-07-17 20:30:16 +0300

    create default sorting func and change sorting algorithm

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,18 +3,24 @@
 /**
  * Implement method Sort
  */
-function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = (item1, item2) => {
-    if (String(item1) > String(item2)) {
-      return 1;
-    }
+const defaultComparingFunction = (item1, item2) => {
+  if (String(item1) > String(item2)) {
+    return 1;
+  }
 
+  if (String(item1) < String(item2)) {
     return -1;
-  }) {
-    for (let j = 0; j < this.length - 1; j++) {
-      for (let i = 0; i < this.length - 1; i++) {
-        if (compareFunction(this[i], this[i + 1]) > 0) {
-          [this[i], this[i + 1]] = [this[i + 1], this[i]];
+  }
+
+  return 0;
+};
+
+function applyCustomSort() {
+  [].__proto__.sort2 = function(compareFunction = defaultComparingFunction) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 1 + i; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
