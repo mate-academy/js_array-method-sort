94b01cb9dfa1fa19654f504583592ded1d197032
AndruhaMan <andreysut12@gmail.com>
2023-07-17 19:07:53 +0300

    The task is completed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,22 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (item1, item2) => {
+    if (String(item1) > String(item2)) {
+      return 1;
+    }
+
+    return -1;
+  }) {
+    for (let j = 0; j < this.length - 1; j++) {
+      for (let i = 0; i < this.length - 1; i++) {
+        if (compareFunction(this[i], this[i + 1]) > 0) {
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
