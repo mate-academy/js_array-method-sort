6845604b902e1599b1041aa86f53b86f4feaae95
Ihor Verbivskyi <i.verba2992@gmail.com>
2023-07-23 17:18:56 +0300

    changes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,16 +5,33 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compare) {
-    for (let i = 0; i < this.length; i++) {
-      for (let j = i + 1; j < this.length; j++) {
-        if (compareFunction(this[i], this[j]) > 0) {
-          const curr = this[i];
+    // for (let i = 0; i < this.length; i++) {
+    //   for (let j = i + 1; j < this.length; j++) {
+    //     if (compareFunction(this[i], this[j]) > 0) {
+    //       const curr = this[i];
 
-          this[i] = this[j];
-          this[j] = curr;
+    //       this[i] = this[j];
+    //       this[j] = curr;
+    //     }
+    //   }
+    // }
+
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const prev = this[i - 1];
+        const curr = this[i];
+
+        if (compareFunction(prev, curr) > 0) {
+          count++;
+          this[i] = prev;
+          this[i - 1] = curr;
         }
       }
-    }
+    } while (count > 0);
 
     return this;
   };
@@ -24,15 +41,13 @@ function compare(a, b) {
   const firstValue = a.toString();
   const secondValue = b.toString();
 
-  switch (true) {
-    case firstValue > secondValue:
-      return 1;
-
-    case firstValue < secondValue:
-      return -1;
-    default:
-      return 0;
-  };
+  if (firstValue > secondValue) {
+    return 1;
+  } else if (firstValue < secondValue) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
 module.exports = applyCustomSort;
