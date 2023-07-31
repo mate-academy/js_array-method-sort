12fc29b94608b79063993da92e913ac4bfb6c103
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-07-31 16:40:55 +0300

    add

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,20 +4,29 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = (a, b) =>
-    String(a) > String(b)) {
-    let count;
+  const compareString = (a, b) => {
+    const strA = String(a);
+    const strB = String(b);
 
-    do {
-      count = false;
+    if (strA > strB) {
+      return 1;
+    }
 
-      for (let i = 0; i < this.length - 1; i++) {
-        if (compareFunction(this[i], this[i + 1]) > 0) {
-          count = true;
-          [this[i], this[i + 1]] = [this[i + 1], this[i]];
+    if (strA === strB) {
+      return 0;
+    }
+
+    return -1;
+  };
+
+  [].__proto__.sort2 = function(compareFunction = compareString) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
         }
       }
-    } while (count);
+    }
 
     return this;
   };
