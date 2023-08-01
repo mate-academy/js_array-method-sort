5848640a6d4f48fea669154e74373e614700ef36
Andrzej Pietkiewicz <pietan@me.com>
2023-08-01 15:54:49 +0200

    working solution #1

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,65 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const array = [...this];
+
+    this.length = 0;
+
+    const sortLikeAString = function sort2(arr) {
+      let arrRight = arr.filter(el => `${el}`
+      > `${arr[arr.length - 1]}`);
+
+      let arrLeft = arr.filter(el => `${el}`
+      < `${arr[arr.length - 1]}`);
+
+      const middle = arr.filter(el => `${el}`
+      === `${arr[arr.length - 1]}`);
+
+      if (arrRight.length > 1) {
+        arrRight = sort2(arrRight);
+      }
+
+      if (arrLeft.length > 1) {
+        arrLeft = sort2(arrLeft);
+      }
+
+      return [...arrLeft, ...middle, ...arrRight];
+    };
+
+    const sortNumbers = function sort2(arr) {
+      const last = arr[arr.length - 1];
+
+      let arrRight = arr.filter(el => compareFunction(el, last) > 0);
+
+      let arrLeft = arr.filter(el => compareFunction(el, last) < 0);
+
+      const middle = arr.filter(el => compareFunction(el, last) === 0);
+
+      if (arrRight.length > 1) {
+        arrRight = sort2(arrRight);
+      }
+
+      if (arrLeft.length > 1) {
+        arrLeft = sort2(arrLeft);
+      }
+
+      return [...arrLeft, ...middle, ...arrRight];
+    };
+
+    !compareFunction ? sortLikeAString(array).forEach((el, i) => {
+      this[i] = el;
+    }) : sortNumbers(array).forEach((el, i) => {
+      this[i] = el;
+    });
+
+    return this;
   };
-}
+};
 
 module.exports = applyCustomSort;
+
+const source = [3, 12, 2, 2, 11, 233, 234, 23, 35, 53, 35, 23, 5, 6, 6, 6, 565];
+
+applyCustomSort();
+
+console.log(source.sort2((a, b) => a - b));
