cdd7e8bd0a9091e5b1e2add40b00532ace3329fd
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-08-02 16:09:57 +0200

    made array method .sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,36 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const length = this.length;
+
+    for (let i = 0; i < length - 1; i++) {
+      let minIndex = i;
+
+      for (let j = i + 1; j < length; j++) {
+        if (compareFunction !== undefined) {
+          if (compareFunction(this[j], this[minIndex]) < 0) {
+            minIndex = j;
+          }
+        } else {
+          if (this[j] < this[i]
+          && typeof this[0] === 'string') {
+            minIndex = j;
+          } else if (this[j].toString().localeCompare(this[i].toString()) < 0
+            && typeof this[0] === 'number') {
+            minIndex = j;
+          }
+        }
+      }
+
+      if (minIndex !== i) {
+        const temp = this[i];
+
+        this[i] = this[minIndex];
+        this[minIndex] = temp;
+      }
+    }
+
+    return this;
   };
 }
 
