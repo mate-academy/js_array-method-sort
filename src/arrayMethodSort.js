999791cf20541a4e6f3b06e33f78304165b45503
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-08-04 15:20:38 +0200

    made code simply

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,30 +5,31 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    for (let i = 0; i < this.length - 1; i++) {
-      for (let j = i + 1; j < this.length; j++) {
-        if (compareFunction !== undefined) {
-          compareFunction(this[j], this[i]) < 0
-            ? [this[i], this[j]] = [this[j], this[i]]
-            : [this[i], this[j]];
-        } else if (compareFunction === undefined
-          && typeof this[0] !== 'number') {
-          [this[i], this[j]]
-            = this[j] < this[i]
-              ? [this[i], this[j]] = [this[j], this[i]]
-              : [this[i], this[j]];
-        } else {
-          [this[i], this[j]]
-            = this[j].toString().localeCompare(this[i]) === -1
-              ? [this[j], this[i]]
-              : [this[i], this[j]];
+  [].__proto__.sort2
+    = function(compareFunction = sorting) {
+      for (let i = 0; i < this.length - 1; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+
+          if (compareFunction(this[j], this[i]) < 0) {
+            [this[i], this[j]] = [this[j], this[i]];
+          } else if (compareFunction(this[j], this[i]) === true) {
+            [this[i], this[j]] = [this[j], this[i]];
+          }
         }
       }
+
+      return this;
+    };
+
+  function sorting(a, b) {
+    const areCyrillics = /[\u0400-\u04FF]+/;
+
+    if (areCyrillics.test(a) && areCyrillics.test(b)) {
+      return a < b;
     }
 
-    return this;
-  };
+    return new Intl.Collator().compare(a, b);
+  }
 }
 
 module.exports = applyCustomSort;
