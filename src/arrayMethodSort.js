ef7bdc6a38a9c92d3b67d084c8168d15395fe31c
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-08-08 21:44:53 +0300

    initial commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,21 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => a.toString() > b.toString()
+  ) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          const compareElement = this[i];
+
+          this[i] = this[j];
+          this[j] = compareElement;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
