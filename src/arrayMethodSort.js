8e25966137eb792c3e107490dde1b2381ef415e0
Vitalii <fevitaliis@gmail.com>
2023-08-08 10:35:14 +0300

    reuse

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,12 +4,14 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction =
-  (a, b) => `${a}` > `${b}` ? 1 : -1) {
-    for (let i = 0; i < this.length - 1; i++) {
+  [].__proto__.sort2 = function(compareFunction = compare) {
+    for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
-          [this[i], this[j]] = [this[j], this[i]];
+          const last = this[i];
+
+          this[i] = this[j];
+          this[j] = last;
         }
       }
     }
@@ -17,5 +19,20 @@ function applyCustomSort() {
     return this;
   };
 }
+
+const compare = (a, b) => {
+  const strA = String(a);
+  const strB = String(b);
+
+  if (strA < strB) {
+    return -1;
+  }
+
+  if (strA > strB) {
+    return 1;
+  }
+
+  return 0;
+};
 
 module.exports = applyCustomSort;
