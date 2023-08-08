2b73c2e0dcb2b5c67e0272e93d2101284ca397f3
Denys Poltorak <itwfrontend@gmail.com>
2023-08-08 18:22:25 +0300

    feat: add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,22 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(
+    compareFunction = (el1, el2) =>
+      String(el1) > String(el2) ? 1 : 0
+  ) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          const temporary = this[i];
+
+          this[i] = this[j];
+          this[j] = temporary;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
