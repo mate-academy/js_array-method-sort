3ab3e2b2c2e16678a6aa26aae84572b460f9de74
Anton Tsurkan <wesses1999@gmail.com>
2023-08-08 16:28:45 +0300

    + 1

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -9,7 +9,7 @@ function applyCustomSort() {
     let temporaryValue = null;
 
     for (let i = 0; i < this.length; i++) {
-      for (let j = i; j < this.length; j++) {
+      for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
           temporaryValue = this[i];
           this[i] = this[j];
