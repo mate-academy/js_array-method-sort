3d4b15cadba6d5762613cda7e398120cbb631577
Dmytro Hrynchyi <desevoker@gmail.com>
2023-08-11 01:40:26 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,34 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (this.length < 2) {
+      return this;
+    }
+
+    const comparator = compareFunction || defaultComparator;
+    let swapped;
+
+    do {
+      swapped = false;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        if (comparator(this[i], this[i + 1]) > 0) {
+          swap(this, i, i + 1);
+          swapped = true;
+        }
+      }
+    } while (swapped);
+
+    return this;
+
+    function defaultComparator(a, b) {
+      return a.toString() > b.toString();
+    }
+
+    function swap(arr, i, j) {
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
   };
 }
 
