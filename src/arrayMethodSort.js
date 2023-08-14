ad8663697e7666e26520c236eda65c3ab25a5acd
Kava Bohdan <bogdan.kava.99@gmail.com>
2023-08-14 15:17:45 +0300

    change default function logic

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,15 +5,16 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(
-    compareFunction = ((a, b) => a.toString() > b.toString())
+    compareFunction = (a, b) => {
+      const result = a.toString() > b.toString() ? 1 : -1;
+
+      return result;
+    }
   ) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
-          const temp = this[i];
-
-          this[i] = this[j];
-          this[j] = temp;
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
