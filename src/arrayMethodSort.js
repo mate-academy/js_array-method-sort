cbf3ce19d8f90af5d928c72954e2e792e28990ac
Oleksii Andriushyn <andriushyn.aleks@gmail.com>
2023-08-14 12:57:55 +0300

    fix default function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,7 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2
-    = function(compareFunction = (a, b) => a.toString() > b.toString()) {
+    = function(compareFunction = (a, b) => String(a) > String(b) ? 1 : -1) {
       for (let i = 0; i < this.length; i++) {
         for (let j = i + 1; j < this.length; j++) {
           if (compareFunction(this[i], this[j]) > 0) {
