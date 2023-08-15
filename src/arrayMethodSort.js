3f732d39f21a026febd6a6e21aa28fc5e524a1b9
Kava Bohdan <bogdan.kava.99@gmail.com>
2023-08-15 12:06:04 +0300

    add separate default function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,13 +4,7 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(
-    compareFunction = (a, b) => {
-      const result = a.toString() > b.toString() ? 1 : -1;
-
-      return result;
-    }
-  ) {
+  [].__proto__.sort2 = function(compareFunction = defaultCompare) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
@@ -21,6 +15,12 @@ function applyCustomSort() {
 
     return this;
   };
+
+  function defaultCompare(a, b) {
+    const result = a.toString() > b.toString() ? 1 : -1;
+
+    return result;
+  }
 }
 
 module.exports = applyCustomSort;
