3fa5113a002847ef4b3206812c680d07ac14f364
Vladyslav Prokhasko <vlad56445@gmail.com>
2023-08-19 19:32:32 +0300

    fix solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,12 +1,15 @@
 'use strict';
 
+function defaultCompare(a, b) {
+  return String(a) > String(b) ? 1 : -1;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = (a, b) =>
-    String(a) > String(b)) {
+  [].__proto__.sort2 = function(compareFunction = defaultCompare) {
     for (let i = 0; i < this.length - 1; i++) {
-      for (let j = 0; j < this.length - i - 1; j++) {
-        if (compareFunction(this[j], this[j + 1]) > 0) {
-          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
