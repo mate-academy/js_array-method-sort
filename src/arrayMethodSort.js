253378dea604d9adfeba1ec6f117ba3905ab3a3b
Sergiy Sergiyenko <comith5@ukr.net>
2023-08-22 19:04:12 +0300

    while deleted

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,15 +3,6 @@
 /**
  * Implement method Sort
  */
-const isSorted = function(arr, compareFunction) {
-  for (let i = 1; i < arr.length; i++) {
-    if (compareFunction(arr[i - 1], arr[i]) > 0) {
-      return false;
-    }
-  }
-
-  return true;
-};
 
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = function(a, b) {
@@ -25,14 +16,12 @@ function applyCustomSort() {
   }) {
     let swap = 0;
 
-    while (!isSorted(this, compareFunction)) {
-      for (let j = 0; j < this.length; j++) {
-        for (let i = j + 1; i < this.length; i++) {
-          if (compareFunction(this[j], this[i]) > 0) {
-            swap = this[j];
-            this[j] = this[i];
-            this[i] = swap;
-          }
+    for (let j = 0; j < this.length; j++) {
+      for (let i = j + 1; i < this.length; i++) {
+        if (compareFunction(this[j], this[i]) > 0) {
+          swap = this[j];
+          this[j] = this[i];
+          this[i] = swap;
         }
       }
     }
