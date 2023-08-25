43621b5fc081882cc21edc70f5facd4bc1f4eb9d
Danylo-Chumakov <danil26032003@gmail.com>
2023-08-25 13:32:12 +0300

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,24 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const start = this.slice();
+
+    for (let i = 0; i < start.length - 1; i++) {
+      for (let j = 0; j < start.length - i - 1; j++) {
+        const shouldSwap = compareFunction ? compareFunction(start[j],
+          start[j + 1]) > 0 : String(start[j]) > String(start[j + 1]);
+
+        if (shouldSwap) {
+          const temp = start[j];
+
+          start[j] = start[j + 1];
+          start[j + 1] = temp;
+        }
+      }
+    }
+    this.splice(0, this.length, ...start);
+
+    return this;
   };
 }
 
