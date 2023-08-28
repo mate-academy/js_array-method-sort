bd843f3e9fd3e3437c079682dee1837d3b0d1f45
Vasyl Kurtiak <pocophonef1start@gmail.com>
2023-08-28 21:22:36 +0300

    sorting without comments

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,29 +3,6 @@
 /**
  * Implement method Sort
  */
-
-// function applyCustomSort() {
-//   [].__proto__.sort2 = function(compareFunction) {
-//     for (let i = 0; i < this.length; i++) {
-//       for (let j = 0; j < this.length - i - 1; j++) {
-//         const mappedValue = compareFunction
-//           ? compareFunction(this[j], this[j + 1])
-//           : this[j] - this[j + 1];
-
-//         if (mappedValue > 0) {
-//           const temp = this[j];
-
-//           this[j] = this[j + 1];
-//           this[j + 1] = temp;
-//         }
-//       }
-//     }
-
-//     return this;
-//   };
-// }
-
-// module.exports = applyCustomSort;
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compareDefaultFunction) {
     for (let i = 0; i < this.length - 1; i++) {
