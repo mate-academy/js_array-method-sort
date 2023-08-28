71dcbca07159b9221c0a7899437f3338e5dc06f7
Vasyl Kurtiak <pocophonef1start@gmail.com>
2023-08-28 21:28:28 +0300

    sorting without comment and sorter

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,15 +6,9 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compareDefaultFunction) {
     for (let i = 0; i < this.length - 1; i++) {
-      for (let j = 0; j < this.length - i - 1; j++) {
-        const left = this[j];
-        const right = this[j + 1];
-
-        const compareResult = compareFunction(left, right);
-
-        if (compareResult > 0) {
-          this[j] = right;
-          this[j + 1] = left;
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
