0ddeb05bec8f34c8196cbde7849f94791ee4807a
Misha Koshilka <mkosilka@gmail.com>
2023-09-05 09:52:24 +0300

    add fix to code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,20 +5,18 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    if (compareFunction && typeof compareFunction === 'function') {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = 0; j < this.length - i - 1; j++) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compareFunction) {
+          // Використовуємо функцію порівняння, якщо вона надана
           if (compareFunction(this[j], this[j + 1]) > 0) {
             const temp = this[j];
 
             this[j] = this[j + 1];
             this[j + 1] = temp;
           }
-        }
-      }
-    } else {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = 0; j < this.length - i - 1; j++) {
+        } else {
+          // Використовуємо стандартну порівняльну логіку
           if (String(this[j]) > String(this[j + 1])) {
             const temp = this[j];
 
