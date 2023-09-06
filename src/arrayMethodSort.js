182059cc0050365972d13b0ea6a6e95d01889280
Butovych <dreamextreme29@gmail.com>
2023-09-06 18:01:25 +0300

    fixed function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,82 +4,19 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    let copyCompare = compareFunction;
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => String(a) > String(b)
+  ) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          const temp = this[j + 1];
 
-    if (compareFunction === undefined && typeof this[0] === 'string') {
-      copyCompare = (firstElement, secondElement) =>
-        firstElement > (secondElement);
-
-      do {
-        for (let i = 0; i < this.length - 1; i++) {
-          const first = '' + this[i];
-          const second = '' + this[i + 1];
-
-          if (copyCompare(first, second) === true) {
-            this[i] = second;
-            this[i + 1] = first;
-          }
-        }
-      } while (copyCompare(this[0], this[1]) === true);
-    }
-
-    if (compareFunction !== undefined && typeof this[0] === 'string') {
-      do {
-        for (let i = 0; i < this.length - 1; i++) {
-          const first = '' + this[i];
-          const second = '' + this[i + 1];
-
-          if (copyCompare(first, second) === 1) {
-            this[i] = second;
-            this[i + 1] = first;
-          }
-        }
-      } while (copyCompare(this[0], this[1]) === 1);
-    }
-
-    if (compareFunction === undefined && typeof this[0] === 'number') {
-      copyCompare = (firstElement, secondElement) =>
-        firstElement > secondElement;
-
-      do {
-        for (let i = 0; i < this.length - 1; i++) {
-          const first = '' + this[i];
-          const second = '' + this[i + 1];
-
-          if (copyCompare(first, second) === true) {
-            this[i] = +second;
-            this[i + 1] = +first;
-          }
-        }
-      } while (copyCompare(this[0], this[1]) === true);
-    }
-
-    if (compareFunction !== undefined && typeof this[0] === 'number') {
-      do {
-        for (let i = 0; i < this.length - 1; i++) {
-          const first = this[i];
-          const second = this[i + 1];
-
-          if (copyCompare(first, second) > 0) {
-            this[i] = second;
-            this[i + 1] = first;
-          }
-        }
-      } while (copyCompare(this[0], this[1]) > 0);
-    }
-
-    do {
-      for (let i = 0; i < this.length - 1; i++) {
-        const first = '' + this[i];
-        const second = '' + this[i + 1];
-
-        if (copyCompare(first, second) === 1) {
-          this[i] = second;
-          this[i + 1] = first;
+          this[j + 1] = this[j];
+          this[j] = temp;
         }
       }
-    } while (copyCompare(this[0], this[1]) === 1);
+    }
 
     return this;
   };
