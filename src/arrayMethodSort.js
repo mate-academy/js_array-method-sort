aece16692ecddea86b70b86abd18f65a81e44f5e
Butovych <dreamextreme29@gmail.com>
2023-09-06 17:23:39 +0300

    solution task

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,83 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let copyCompare = compareFunction;
+
+    if (compareFunction === undefined && typeof this[0] === 'string') {
+      copyCompare = (firstElement, secondElement) =>
+        firstElement > (secondElement);
+
+      do {
+        for (let i = 0; i < this.length - 1; i++) {
+          const first = '' + this[i];
+          const second = '' + this[i + 1];
+
+          if (copyCompare(first, second) === true) {
+            this[i] = second;
+            this[i + 1] = first;
+          }
+        }
+      } while (copyCompare(this[0], this[1]) === true);
+    }
+
+    if (compareFunction !== undefined && typeof this[0] === 'string') {
+      do {
+        for (let i = 0; i < this.length - 1; i++) {
+          const first = '' + this[i];
+          const second = '' + this[i + 1];
+
+          if (copyCompare(first, second) === 1) {
+            this[i] = second;
+            this[i + 1] = first;
+          }
+        }
+      } while (copyCompare(this[0], this[1]) === 1);
+    }
+
+    if (compareFunction === undefined && typeof this[0] === 'number') {
+      copyCompare = (firstElement, secondElement) =>
+        firstElement > secondElement;
+
+      do {
+        for (let i = 0; i < this.length - 1; i++) {
+          const first = '' + this[i];
+          const second = '' + this[i + 1];
+
+          if (copyCompare(first, second) === true) {
+            this[i] = +second;
+            this[i + 1] = +first;
+          }
+        }
+      } while (copyCompare(this[0], this[1]) === true);
+    }
+
+    if (compareFunction !== undefined && typeof this[0] === 'number') {
+      do {
+        for (let i = 0; i < this.length - 1; i++) {
+          const first = this[i];
+          const second = this[i + 1];
+
+          if (copyCompare(first, second) > 0) {
+            this[i] = second;
+            this[i + 1] = first;
+          }
+        }
+      } while (copyCompare(this[0], this[1]) > 0);
+    }
+
+    do {
+      for (let i = 0; i < this.length - 1; i++) {
+        const first = '' + this[i];
+        const second = '' + this[i + 1];
+
+        if (copyCompare(first, second) === 1) {
+          this[i] = second;
+          this[i + 1] = first;
+        }
+      }
+    } while (copyCompare(this[0], this[1]) === 1);
+
+    return this;
   };
 }
-
 module.exports = applyCustomSort;
