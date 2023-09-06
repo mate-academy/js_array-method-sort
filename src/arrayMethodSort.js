717166cc6e6e2fa993d2f7079bd6c5d3311f7eda
Ruslan Hrynchuk <r.momos@gmail.com>
2023-09-06 16:43:43 +0300

    Tests done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -2,11 +2,40 @@
 
 /**
  * Implement method Sort
- */
+//  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let value1 = '';
+    let value2 = '';
+
+    if (!compareFunction) {
+      for (let a = this.length; a >= 0; a--) {
+        for (let i = 0; i < this.length - 1; i++) {
+          value1 = this[i];
+          value2 = this[i + 1];
+
+          if ((value1).toString() > (value2).toString()) {
+            this[i] = value2;
+            this[i + 1] = value1;
+          }
+        }
+      }
+    } else {
+      for (let a = this.length; a >= 0; a--) {
+        for (let i = 0; i < this.length - 1; i++) {
+          value1 = this[i];
+          value2 = this[i + 1];
+
+          if (compareFunction(value1, value2) > 0) {
+            this[i] = value2;
+            this[i + 1] = value1;
+          }
+        }
+      }
+    }
+
+    return this;
   };
-}
+};
 
 module.exports = applyCustomSort;
