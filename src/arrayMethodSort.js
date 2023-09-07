1a20663b2a938821a3976edec2ecf16402e9fac7
Ruslan Hrynchuk <r.momos@gmail.com>
2023-09-07 11:06:27 +0300

    With corrections

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,32 +4,16 @@
  * Implement method Sort
 //  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    let value1 = '';
-    let value2 = '';
+  [].__proto__.sort2 = function(compareFunction =
+  (a, b) => String(a) > String(b)) {
+    for (let a = this.length; a >= 0; a--) {
+      for (let i = 0; i < this.length - 1; i++) {
+        const value1 = this[i];
+        const value2 = this[i + 1];
 
-    if (!compareFunction) {
-      for (let a = this.length; a >= 0; a--) {
-        for (let i = 0; i < this.length - 1; i++) {
-          value1 = this[i];
-          value2 = this[i + 1];
-
-          if ((value1).toString() > (value2).toString()) {
-            this[i] = value2;
-            this[i + 1] = value1;
-          }
-        }
-      }
-    } else {
-      for (let a = this.length; a >= 0; a--) {
-        for (let i = 0; i < this.length - 1; i++) {
-          value1 = this[i];
-          value2 = this[i + 1];
-
-          if (compareFunction(value1, value2) > 0) {
-            this[i] = value2;
-            this[i + 1] = value1;
-          }
+        if (compareFunction(value1, value2) > 0) {
+          this[i] = value2;
+          this[i + 1] = value1;
         }
       }
     }
