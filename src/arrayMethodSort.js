381dac8371f9579c0ec82c23ae55e146a5d304e2
Ruslan Hrynchuk <r.momos@gmail.com>
2023-09-07 12:09:33 +0300

    Corrections Part 2

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,15 +5,11 @@
 //  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction =
-  (a, b) => String(a) > String(b)) {
+  (a, b) => String(a) >= String(b) ? 1 : -1) {
     for (let a = this.length; a >= 0; a--) {
       for (let i = 0; i < this.length - 1; i++) {
-        const value1 = this[i];
-        const value2 = this[i + 1];
-
-        if (compareFunction(value1, value2) > 0) {
-          this[i] = value2;
-          this[i + 1] = value1;
+        if (compareFunction(this[i], this[i + 1]) > 0) {
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
         }
       }
     }
