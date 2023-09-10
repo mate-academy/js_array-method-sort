3cc2ffc79676840067808dfe6792c35f3effe0f9
Denys Vasylenkoe <kapodenim@gmail.com>
2023-09-10 12:00:26 +0300

    v1

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,7 +6,43 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     // write code here
+    if (compareFunction === undefined) {
+      bubbleSort(this);
+    } else {
+      bubbleSort(this, compareFunction);
+    }
+
+    return this;
   };
+}
+
+function compareStrings(a, b) {
+  return a.toString() > b.toString();
+}
+
+function bubbleSort(arr, callback = compareStrings) {
+  let i, j;
+  const len = arr.length - 1;
+
+  let isSwapped = false;
+
+  for (i = 0; i < len; i++) {
+    isSwapped = false;
+
+    for (j = 0; j < len; j++) {
+      if (callback(arr[j], arr[j + 1]) > 0) {
+        const temp = arr[j];
+
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        isSwapped = true;
+      }
+    }
+
+    if (!isSwapped) {
+      break;
+    }
+  }
 }
 
 module.exports = applyCustomSort;
