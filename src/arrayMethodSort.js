9870f3a05acaf45ab18b2f74513e9bd975da11ed
BeginerAlex <aleksander9677@gmail.com>
2023-09-11 12:12:41 +0200

    default value for function was added

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,22 +4,13 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction = (
+    a, b) => String(a) > String(b)
+  ) {
     if (compareFunction !== undefined) {
       for (let i = 0; i < this.length - 1; i++) {
         for (let x = 0; x < this.length - i - 1; x++) {
           if (compareFunction(this[x], this[x + 1]) > 0) {
-            const temp = this[x];
-
-            this[x] = this[x + 1];
-            this[x + 1] = temp;
-          }
-        }
-      }
-    } else {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let x = 0; x < this.length - i - 1; x++) {
-          if (String(this[x]) > String(this[x + 1])) {
             const temp = this[x];
 
             this[x] = this[x + 1];
