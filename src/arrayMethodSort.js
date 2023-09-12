7120aaf661e5d6a5c699cd6b3d4e65d61b28ce35
Lubomur Chuchvara <lubomircucvara@gmail.com>
2023-09-12 13:58:28 +0300

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,30 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let callback = compareFunction;
+
+    if (typeof callback !== 'function') {
+      callback = (a, b) => {
+        if (a === b) {
+          return 0;
+        }
+
+        return a + '' < b + '' ? -1 : 1;
+      };
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (callback(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
