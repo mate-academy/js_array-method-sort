e4cc08b0a5382df443d5973064a7e6f96c61213c
Bahtijar <no4kar@gmail.com>
2023-09-12 17:28:54 +0200

    task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,42 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => a.toString().localeCompare(b)
+  ) {
+    return BubbleSort(this, compareFunction);
   };
+}
+
+function BubbleSort(
+  arr,
+  cmpFunc,
+) {
+  let again = false;
+
+  do {
+    again = false;
+
+    for (let i = 1; i < arr.length; i++) {
+      if (cmpFunc(arr[i - 1], arr[i]) > 0) {
+        Swap(arr, i - 1, i);
+        again = true;
+      }
+    }
+  } while (again);
+
+  return arr;
+}
+
+function Swap(
+  arr,
+  index1,
+  index2,
+) {
+  const temp = arr[index1];
+
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
 }
 
 module.exports = applyCustomSort;
