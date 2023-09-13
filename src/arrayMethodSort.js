075662efb1cc1726e78a83f4aaa7c26a963f7527
Kateryna Sydorenko <katerynnnna@gmail.com>
2023-09-13 21:51:07 +0300

    task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,24 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let comparator = (a, b) => a.toString() > b.toString() ? 1 : -1;
+
+    if (compareFunction && typeof compareFunction === 'function') {
+      comparator = compareFunction;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (comparator(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
--- src/arrayMethodSort.test.js
+++ src/arrayMethodSort.test.js
@@ -53,7 +53,7 @@ test('custom compare function, numbers: [3, 12, 2, 11].sort2((a, b) => a - b)',
   });
 
 test(
-  `custom compare function, strings: 
+  `custom compare function, strings:
   ['їжачок','яблуко','апельсин'].sort2((a, b) => a.localeCompare(b))`, () => {
     expect(['їжачок', 'яблуко', 'апельсин'].sort2((a, b) => a.localeCompare(b)))
       .toEqual(['апельсин', 'їжачок', 'яблуко']);
