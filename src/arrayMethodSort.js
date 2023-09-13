dfcd4066a4484bef268235d1dcd259a7ca1cf133
Stanislav Kohut <stako211@gmail.com>
2023-09-14 01:12:46 +0300

    added task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,47 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    function compareFn(a, b) {
+      if (a < b) {
+        return -1;
+      }
+
+      if (a > b) {
+        return 1;
+      }
+
+      if (a === b) {
+        return 0;
+      }
+    };
+
+    if (!compareFunction) {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          if (compareFn(`${this[i]}`, `${this[j]}`) === 1) {
+            const temp = this[i];
+
+            this[i] = this[j];
+            this[j] = temp;
+          }
+        }
+      };
+
+      return this;
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          if (compareFunction(this[i], this[j]) > 0) {
+            const temp = this[i];
+
+            this[i] = this[j];
+            this[j] = temp;
+          }
+        }
+      };
+
+      return this;
+    }
   };
 }
 
