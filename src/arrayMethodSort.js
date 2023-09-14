d22c5bd3b5307654912cd4c2e5e0308b39b6eeba
Oleksii Bielovolov <whiteoxofficial@gmail.com>
2023-09-14 14:16:19 +0300

    methodSort0.1

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,15 +4,10 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    const compare = compareFunction;
-    const compareFn = compareFunction ? compare : withoutCallback;
-
+  [].__proto__.sort2 = function(compareFunction = sortAsString) {
     for (let i = 0; i < this.length; i++) {
       for (let j = 1; j < this.length; j++) {
-        const result = compareFn(this[j - 1], this[j]);
-
-        if (checkEquality(result, !!compareFn)) {
+        if (compareFunction(this[j - 1], this[j]) > 0) {
           [this[j - 1], this[j]] = [this[j], this[j - 1]];
         }
       }
@@ -22,12 +17,8 @@ function applyCustomSort() {
   };
 }
 
-function withoutCallback(a, b) {
+function sortAsString(a, b) {
   return a.toString() > b.toString() ? 1 : -1;
-}
-
-function checkEquality(result, biggerEqual = false) {
-  return biggerEqual ? result >= 1 : result === 1;
 }
 
 module.exports = applyCustomSort;
