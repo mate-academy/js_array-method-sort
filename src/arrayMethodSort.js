fe5d4eeb5f5598c044c195635a8495726180b2f4
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-09-21 17:32:49 +0200

    created a default comparing function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,16 +3,12 @@
 /**
  * Implement method Sort
  */
-function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = (a, b) => {
-    if (String(a) < String(b)) {
-      return -1;
-    } else if (String(a) > String(b)) {
-      return 1;
-    }
+const defaultComparing = (a, b) => {
+  return String(a) < String(b) ? -1 : 1;
+};
 
-    return 0;
-  }) {
+function applyCustomSort() {
+  [].__proto__.sort2 = function(compareFunction = defaultComparing) {
     for (let i = 1; i < this.length; i++) {
       for (let k = i; k && compareFunction(this[k - 1], this[k]) > 0; k--) {
         [this[k], this[k - 1]] = [this[k - 1], this[k]];
