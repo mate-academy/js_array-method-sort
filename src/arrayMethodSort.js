8087d8f48c08f38e79f9918afad380ed09c92646
koros <korosov.j@gmail.com>
2023-09-21 10:18:03 +0300

    review fixes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,19 +1,24 @@
 'use strict';
 
 /**
+ * @param a
+ * @param b
+ *
+ * @returns {number}
+ */
+const defaultCompare = (a, b) => {
+  return a.toString() > b.toString() ? 1 : -1;
+};
+
+/**
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = (a, b) => {
-    return a.toString() > b.toString() ? 1 : -1;
-  }) {
+  [].__proto__.sort2 = function(compareFunction = defaultCompare) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
-          const tmp = this[i];
-
-          this[i] = this[j];
-          this[j] = tmp;
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
