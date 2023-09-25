76da87886edd8287e02e24b0e0fa26bbd09d2f63
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-09-25 19:26:46 +0300

    add some changes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,10 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFn = (a, b) => String(a) > String(b)) {
-    for (let i = 0; i < this.length - 1; i++) {
-      for (let j = 0; j < this.length - 1 - i; j++) {
+    const PENULTIMATE_INDEX = this.length - 1;
+
+    for (let i = 0; i < PENULTIMATE_INDEX; i++) {
+      for (let j = 0; j < PENULTIMATE_INDEX - i; j++) {
         if (compareFn(this[j], this[j + 1]) > 0) {
           const temp = this[j];
 
