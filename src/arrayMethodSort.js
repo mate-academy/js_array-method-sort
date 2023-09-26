6d51aa6d90977cec9f004f461f7544af85b6ab59
Viktoriia Melnyk <melnychka1993@gmail.com>
2023-09-26 12:17:39 +0100

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,9 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction =
-  (a, b) => String(a) > String(b)) {
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => String(a) > String(b)
+  ) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
