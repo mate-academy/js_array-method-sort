6e0366be54acd066dc178929d1898ce809fef76c
Viktoriia Melnyk <melnychka1993@gmail.com>
2023-09-26 11:41:14 +0100

    added default

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,29 +4,15 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    if (compareFunction === undefined) {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = i + 1; j < this.length; j++) {
-          if (this[i].toString() > this[j].toString()) {
-            const temp = this[i];
+  [].__proto__.sort2 = function(compareFunction =
+  (a, b) => String(a) > String(b)) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          const temp = this[i];
 
-            this[i] = this[j];
-            this[j] = temp;
-          }
-        }
-      }
-    }
-
-    if (compareFunction) {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = i + 1; j < this.length; j++) {
-          if (compareFunction(this[i], this[j]) > 0) {
-            const temp = this[i];
-
-            this[i] = this[j];
-            this[j] = temp;
-          }
+          this[i] = this[j];
+          this[j] = temp;
         }
       }
     }
