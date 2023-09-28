b89241d624f9d0ca4d00887be95634821a435baf
Робенко Олександр Олександрович <Robenko-O@yugcontract.ua>
2023-09-28 23:30:55 +0300

    fixed version

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,20 +5,18 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = compareAsString) {
-    if (compareFunction && typeof compareFunction === 'function') {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = 0; j < this.length - i - 1; j++) {
-          if (compareFunction(this[j], this[j + 1]) > 0) {
-            const temp = this[j];
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
 
-            this[j] = this[j + 1];
-            this[j + 1] = temp;
-          }
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
         }
       }
-
-      return this;
     }
+
+    return this;
   };
 }
 
