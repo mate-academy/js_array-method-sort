d092d38d44ac0adf625fed04f5defed91c788151
Робенко Олександр Олександрович <Robenko-O@yugcontract.ua>
2023-09-28 23:24:31 +0300

    add fixes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,7 +4,7 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction = compareAsString) {
     if (compareFunction && typeof compareFunction === 'function') {
       for (let i = 0; i < this.length - 1; i++) {
         for (let j = 0; j < this.length - i - 1; j++) {
@@ -16,21 +16,17 @@ function applyCustomSort() {
           }
         }
       }
-    } else {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = 0; j < this.length - i - 1; j++) {
-          if (String(this[j]) > (String(this[j + 1]))) {
-            const temp = this[j];
 
-            this[j] = this[j + 1];
-            this[j + 1] = temp;
-          }
-        }
-      }
+      return this;
     }
-
-    return this;
   };
 }
+
+const compareAsString = (a, b) => {
+  const stringA = String(a);
+  const stringB = String(b);
+
+  return stringA > stringB ? 1 : -1;
+};
 
 module.exports = applyCustomSort;
