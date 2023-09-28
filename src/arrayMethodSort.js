b29fcf9ae52d77935b5647b06fec706aa42016ec
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2023-09-28 16:43:30 +0300

    Improvements after mentor review

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,16 +1,22 @@
 'use strict';
 
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = (a, b) => {
-    return (a.toString() >= b.toString()) ? 1 : -1;
-  }) {
+  [].__proto__.sort2 = function(compareFunction) {
+    let actualCompareFunc = compareFunction;
+
+    if (!compareFunction) {
+      actualCompareFunc = (a, b) => {
+        return (a.toString() >= b.toString()) ? 1 : -1;
+      };
+    }
+
     if (!this) {
       return this;
     }
 
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
-        if (compareFunction(this[j], this[j + 1]) > 0) {
+        if (actualCompareFunc(this[j], this[j + 1]) > 0) {
           [this[j + 1], this[j]] = [this[j], this[j + 1]];
         }
       }
