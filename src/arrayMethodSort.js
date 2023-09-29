40842c2756de8dea84e46a81af2a4e1b8c4432d7
Hlib Vakhrameev@icloud.com <g.vakhrameev@icloud.com>
2023-09-29 14:49:38 +0300

    set the default value to make this code shorter

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,30 +4,19 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    if (typeof compareFunction === 'function') {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = 0; j < this.length - 1 - i; j++) {
-          if (compareFunction(this[j], this[j + 1]) > 0) {
-            const temp = this[j];
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => `${a}` > `${b}` ? 1 : -1
+  ) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
 
-            this[j] = this[j + 1];
-            this[j + 1] = temp;
-          }
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
         }
       }
-    } else {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = 0; j < this.length - 1 - i; j++) {
-          if (`${this[j]}` > `${this[j + 1]}`) {
-            const temp = this[j];
-
-            this[j] = this[j + 1];
-            this[j + 1] = temp;
-          }
-        }
-      }
-    };
+    }
 
     return this;
   };
