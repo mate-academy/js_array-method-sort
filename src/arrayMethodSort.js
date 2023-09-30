d95df868bcf4d66bcace15254a266b535e023f00
Tetyana Shatrova <shatrova.tetyana@gmail.com>
2023-09-30 18:20:32 +0300

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let cf = compareFunction;
+
+    if (cf === undefined) {
+      cf = (a, b) => a.toString() === b.toString()
+        ? 0 : a.toString() > b.toString() ? 1 : -1;
+    }
+
+    let f = true; let t;
+
+    while (f) {
+      f = false;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        if (cf(this[i], this[i + 1]) > 0) {
+          t = this[i];
+          this[i] = this[i + 1];
+          this[i + 1] = t;
+          f = true;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
