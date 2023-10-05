f2f6ada004d5c34b10295d02bf1b201584812eed
Leo <you@example.com>
2023-10-05 14:50:20 +0300

    create

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,21 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (a, b) =>
+    String(a) > String(b)) {
+    for (let i = 0; i < this.length; i++) {
+      for (let k = i + 1; k < this.length; k++) {
+        const curentItem = this[i];
+        const nextItem = this[k];
+
+        if (compareFunction(curentItem, nextItem) > 0) {
+          this[i] = nextItem;
+          this[k] = curentItem;
+        };
+      };
+    };
+
+    return this;
   };
 }
 
