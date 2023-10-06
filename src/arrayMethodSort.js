882acaceb654418e6d9630972aad704c9f517f8b
newqou <137334905+newqou@users.noreply.github.com>
2023-10-06 18:24:34 +0300

    updated

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,14 +5,13 @@
  */
 
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction
+  = (a, b) => String(a) > String(b) ? 1 : -1) {
     const arr = this;
 
     for (let i = 0; i < arr.length - 1; i++) {
       for (let j = 0; j < arr.length - i - 1; j++) {
-        if (compareFunction
-          ? compareFunction(arr[j], arr[j + 1]) > 0
-          : String(arr[j]) > String(arr[j + 1])) {
+        if (compareFunction(arr[j], arr[j + 1]) > 0) {
           [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         }
       }
