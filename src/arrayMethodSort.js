04687d3971fbb99a3571a58273c7a19ca2597fed
Mari <kap.mebel.a@gmail.com>
2023-10-06 17:12:24 +0200

    removed comment, use default value

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,11 +6,10 @@
 function applyCustomSort() {
   const defaultCompare = (a, b) => `${a}` < `${b}` ? -1 : 1;
 
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction = defaultCompare) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        // eslint-disable-next-line max-len
-        const res = compareFunction ? compareFunction(this[i], this[j]) : defaultCompare(this[i], this[j]);
+        const res = compareFunction(this[i], this[j]);
 
         if (res > 0) {
           const val = this[j];
@@ -26,13 +25,3 @@ function applyCustomSort() {
 }
 
 module.exports = applyCustomSort;
-
-// const source = ['їжачок','яблуко','апельсин'];
-// const source = [2, 3, 1];
-
-// applyCustomSort();
-
-// const res = source.sort2();
-
-// eslint-disable-next-line no-console
-// console.log(res);
