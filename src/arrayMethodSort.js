3cdbe565cd20b6a2a9caa510b73f8f606161bfcf
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-10-07 14:15:04 +0300

    add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,22 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        const currentEl = this[j];
+
+        const comparisonResult = compareFunction
+          ? compareFunction(this[j], this[j + 1])
+          : `${this[j]}` > `${this[j + 1]}`;
+
+        if (comparisonResult > 0) {
+          this[j] = this[j + 1];
+          this[j + 1] = currentEl;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
