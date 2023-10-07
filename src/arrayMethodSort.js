123287e19b07aa4d63dae11bd25adb782d63ebe4
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-10-07 14:59:44 +0300

    add default value for compareFunction

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,14 +4,12 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => String(a) > String(b)) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
         const currentEl = this[j];
-
-        const comparisonResult = compareFunction
-          ? compareFunction(this[j], this[j + 1])
-          : `${this[j]}` > `${this[j + 1]}`;
+        const comparisonResult = compareFunction(this[j], this[j + 1]);
 
         if (comparisonResult > 0) {
           this[j] = this[j + 1];
