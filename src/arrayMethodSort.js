5cf08f3ceb610c374bd9e8266445bec04d7d8153
Damian <damian.adamczewski92@gmail.com>
2023-10-12 16:30:37 +0200

    write a solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,34 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (typeof compareFunction === 'function') {
+      for (let i = 0; i < this.length - 1; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          if (compareFunction(this[i], this[j]) > 0) {
+            // Swap elements if compareFunction returns a positive value
+            const temp = this[i];
+
+            this[i] = this[j];
+            this[j] = temp;
+          }
+        }
+      }
+    } else {
+      // Default sorting behavior (as strings)
+      for (let i = 0; i < this.length - 1; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          if (String(this[i]) > String(this[j])) {
+            // Swap elements if default comparison returns true
+            const temp = this[i];
+
+            this[i] = this[j];
+            this[j] = temp;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
