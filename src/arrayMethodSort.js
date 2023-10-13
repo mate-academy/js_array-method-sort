8182e0be3b7dea3603e4ea038c93cc3644d4c46a
Viktor Bykh <bykhviktor21@gmail.com>
2023-10-13 14:13:10 +0300

    first version of hw js_array-method-sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,25 @@
 /**
  * Implement method Sort
  */
+
+const compareStr = (a, b) => `${a}` > `${b}` ? 1 : -1;
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareStr) {
+    let condition = true;
+
+    do {
+      condition = false;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        if (compareFunction(this[i], this[i + 1]) > 0) {
+          condition = true;
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
+        }
+      }
+    } while (condition);
+
+    return this;
   };
 }
 
