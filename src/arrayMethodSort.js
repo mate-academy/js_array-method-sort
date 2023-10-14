a378e60cba32b9e7bfa40ccfd051a0233d251e46
Tatiana <tanya_potapenko@bigmir.net>
2023-10-14 11:41:46 +0200

    --no-verify

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,34 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const сompareAsStrings = (a, b) => {
+      const stringA = String(a);
+      const stringB = String(b);
+
+      if (stringA > stringB) {
+        return 1;
+      } else
+      if (stringA === stringB) {
+        return 0;
+      } else {
+        return -1;
+      }
+    };
+
+    const compare = compareFunction || сompareAsStrings;
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length; j++) {
+        if (compare(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
