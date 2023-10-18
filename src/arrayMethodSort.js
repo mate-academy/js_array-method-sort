f1cf118ae77d5ed564f414e16529bd2b674e7cb8
Oleksandr Podvoiskyi <podvoyskiy01@gmail.com>
2023-10-18 15:19:27 +0300

    Add better task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,27 +4,20 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    if (!compareFunction) {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = 0; j < this.length - i - 1; j++) {
-          if (`${this[j]}` > `${this[j + 1]}`) {
-            const temp = this[j];
-
-            this[j] = this[j + 1];
-            this[j + 1] = temp;
-          }
-        }
-      }
+  [].__proto__.sort2 = function(compareFunction = (a, b) => {
+    if (`${a}` > `${b}`) {
+      return 1;
     } else {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = 0; j < this.length - i - 1; j++) {
-          if (compareFunction(this[j], this[j + 1]) >= 1) {
-            const temp = this[j];
+      return 0;
+    }
+  }) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compareFunction(this[j], this[j + 1]) >= 1) {
+          const temp = this[j];
 
-            this[j] = this[j + 1];
-            this[j + 1] = temp;
-          }
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
         }
       }
     }
