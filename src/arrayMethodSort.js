d64df747a154d8503f7eb86295b6dd0fb315b6e4
Taras Shevchenko <shevchenkotv91@gmail.com>
2023-10-18 17:05:50 +0300

    fix after review

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,7 +6,7 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     const callback = compareFunction
-      || ((prevElem, elem) => elem.toString() < prevElem.toString());
+      || ((prevElem, elem) => String(elem) < String(prevElem));
 
     for (let i = 1; i < this.length; i++) {
       for (let j = 0; j < i; j++) {
