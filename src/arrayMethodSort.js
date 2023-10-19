95574e1c0ee4b01ea1fb858c01f86ae0d905bb4a
Daria Sydorenko <dashasidorenko123@gmail.com>
2023-10-19 16:53:03 +0300

    Task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,23 @@
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (a, b) => {
+    return a.toString() > b.toString() ? 1 : -1;
+  }) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let y = i + 1; y < this.length; y++) {
+        if (compareFunction(this[i], this[y]) > 0) {
+          const temp = this[i];
+
+          this[i] = this[y];
+          this[y] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
