e249c45b0fd707d8156c8a00b2edbdcf0b00d53b
Svitlana Ramanauskas <ramanauskassvs@gmail.com>
2023-10-20 14:31:06 +0300

    task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,38 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (compareFunction === undefined) {
+      for (let index = 0; index < this.length; index++) {
+        for (let nextIndex = index; nextIndex < this.length; nextIndex++) {
+          const element = String(this[index]);
+          const nextElement = String(this[nextIndex]);
+
+          if (element > nextElement) {
+            const temp = this[index];
+
+            this[index] = this[nextIndex];
+            this[nextIndex] = temp;
+          }
+        }
+      }
+
+      return this;
+    }
+
+    if (compareFunction !== undefined) {
+      for (let index = 0; index < this.length; index++) {
+        for (let nextIndex = index; nextIndex < this.length; nextIndex++) {
+          if (compareFunction(this[index], this[nextIndex]) > 0) {
+            const temp = this[index];
+
+            this[index] = this[nextIndex];
+            this[nextIndex] = temp;
+          }
+        }
+      }
+
+      return this;
+    }
   };
 }
 
