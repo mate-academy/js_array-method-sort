4f64e7051737c7a9c208a3b15107575707afaef6
Kateryna <kate042798@gmail.com>
2023-10-20 20:32:33 -0600

    updated Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,13 +4,11 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    const defaultCompareFunction = (a, b) => String(a) < String(b) ? -1 : 1;
-    const LoopCompareFunction = compareFunction || defaultCompareFunction;
-
+  [].__proto__.sort2 = function(compareFunction = (a, b) =>
+    String(a) < String(b) ? -1 : 1) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
-        if (LoopCompareFunction(this[j], this[j + 1]) > 0) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
           const temp = this[j];
 
           this[j] = this[j + 1];
