97116b1b8f6acca08e8586f6cfdbb58ebceac76f
Evhen Teneta <tenetaevgen@gmail.com>
2023-10-24 13:34:48 +0300

    task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,30 @@
 /**
  * Implement method Sort
  */
+/*
+realise sort method for array
+which repeats the original sort method
+the beginning is already there
+*/
+
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const compFn = compareFunction || ((a, b) => String(a) > String(b));
+
+    // (a, b) => (''+ a).localeCompare(b, 'uk')
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compFn(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
