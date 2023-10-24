e825c866c497e188511e26807153d6c4562781b7
Julia <julia@ocuhealthcare.com>
2023-10-24 15:51:30 -0500

    Sort function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,22 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => String(a) > String(b)
+  ) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        const currentEl = this[i];
+        const prevEl = this[j];
+
+        if (compareFunction(currentEl, prevEl) > 0) {
+          this[i] = prevEl;
+          this[j] = currentEl;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
