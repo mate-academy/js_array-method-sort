23dcaf3a4eb11242793f4c6c7bfa7f10435f523c
Evhen Teneta <tenetaevgen@gmail.com>
2023-10-24 16:26:28 +0300

    edit comments from VitaliyBodnar

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -11,12 +11,11 @@ the beginning is already there
 
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = (a, b) =>
-    String(a) > String(b)) {
-    const compFn = compareFunction;
-
+    String(a) > String(b)
+    ) {
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
-        if (compFn(this[j], this[j + 1]) > 0) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
           const temp = this[j];
 
           this[j] = this[j + 1];
