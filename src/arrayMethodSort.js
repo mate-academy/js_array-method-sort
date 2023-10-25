cd788566f1994fb6ec02c1a56d53d47a8b761297
Julia <julia@ocuhealthcare.com>
2023-10-25 14:13:54 -0500

    Fix

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -10,10 +10,9 @@ function applyCustomSort() {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         const currentEl = this[i];
-        const prevEl = this[j];
 
-        if (compareFunction(currentEl, prevEl) > 0) {
-          this[i] = prevEl;
+        if (compareFunction(currentEl, this[j]) > 0) {
+          this[i] = this[j];
           this[j] = currentEl;
         }
       }
