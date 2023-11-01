ae274889611773a777c48a33bfa26bc08e257a4e
Vladislav Balaban <vlad.balaban2020@gmail.com>
2023-11-01 10:36:13 +0200

    Added method Sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,12 +1,24 @@
-'use strict';
+"use strict";
 
 /**
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (
+    compareFunction = (a, b) => String(a) > String(b)
+  ) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let k = 0; k < this.length - i - 1; k++) {
+        if (compareFunction(this[k], this[k + 1]) > 0) {
+          const temp = this[k];
+
+          this[k] = this[k + 1];
+          this[k + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
-
 module.exports = applyCustomSort;
