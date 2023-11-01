e5b295e1e5014f8d532726a097a51e1fe89da337
Vladislav Balaban <vlad.balaban2020@gmail.com>
2023-11-01 11:30:54 +0200

    Added separate compare method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,12 +4,22 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (
-    compareFunction = (a, b) => String(a) > String(b)
-  ) {
+  [].__proto__.sort2 = function (compareFunction) {
+    const defaultCompareFunction = (a, b) => {
+      const aStr = a.toString();
+      const bStr = b.toString();
+
+      if (aStr < bStr) return -1;
+      if (aStr > bStr) return 1;
+
+      return 0;
+    };
+
+    const compareMethod = compareFunction || defaultCompareFunction;
+
     for (let i = 0; i < this.length - 1; i++) {
       for (let k = 0; k < this.length - i - 1; k++) {
-        if (compareFunction(this[k], this[k + 1]) > 0) {
+        if (compareMethod(this[k], this[k + 1]) > 0) {
           const temp = this[k];
 
           this[k] = this[k + 1];
