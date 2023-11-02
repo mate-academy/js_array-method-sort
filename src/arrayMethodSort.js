081d84b15458151b0ca3586753d9ceb7c33962a0
Maksym Svoboda <maksym.svoboda.kn.2021@lpnu.ua>
2023-11-02 17:18:23 +0200

    made solution for sorting method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,29 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
-  };
+  const SORTED = 1;
+  const NOT_SORTED = -1;
+  const MEDIUM = 0;
+
+  [].__proto__.sort2
+  = function(compareFunction = (
+      (el1, el2) => el1.toString() >= el2.toString() ? SORTED : NOT_SORTED)
+    ) {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length; j++) {
+          if (compareFunction(this[i], this[j]) >= MEDIUM) {
+            continue;
+          }
+
+          const tempElement = this[i];
+
+          this[i] = this[j];
+          this[j] = tempElement;
+        }
+      }
+
+      return this;
+    };
 }
 
 module.exports = applyCustomSort;
