0ebdc3a49dc89aca42e609055ed0a51a43e55e7d
Maksym Svoboda <maksym.svoboda.kn.2021@lpnu.ua>
2023-11-04 18:21:29 +0200

    changed parameter function assignment

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -8,25 +8,25 @@ function applyCustomSort() {
   const NOT_SORTED = -1;
   const MEDIUM = 0;
 
-  [].__proto__.sort2
-  = function(compareFunction = (
-      (el1, el2) => el1.toString() >= el2.toString() ? SORTED : NOT_SORTED)
-    ) {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = 0; j < this.length; j++) {
-          if (compareFunction(this[i], this[j]) >= MEDIUM) {
-            continue;
-          }
+  const baseSorting = (el1, el2) =>
+    (el1.toString() >= el2.toString() ? SORTED : NOT_SORTED);
 
-          const tempElement = this[i];
-
-          this[i] = this[j];
-          this[j] = tempElement;
+  [].__proto__.sort2 = function(compareFunction = baseSorting) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) >= MEDIUM) {
+          continue;
         }
-      }
 
-      return this;
-    };
+        const tempElement = this[i];
+
+        this[i] = this[j];
+        this[j] = tempElement;
+      }
+    }
+
+    return this;
+  };
 }
 
 module.exports = applyCustomSort;
