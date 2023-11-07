201d552153b36e463fa28a7b473bf20cc96b8f44
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2023-11-07 20:25:10 +0100

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,43 @@
 /**
  * Implement method Sort
  */
+
+function compareAsString(a, b) {
+  // linter didn't allow me to use the variables a and b here
+  const aNum = String(a);
+  const bNum = String(b);
+
+  if (aNum > bNum) {
+    return 1;
+  }
+
+  if (aNum < bNum) {
+    return -1;
+  }
+
+  return 0;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = compareAsString) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        const a = this[i];
+        const b = this[i + 1];
+
+        if (compareFunction(a, b) > 0) {
+          count++;
+          this[i] = b;
+          this[i + 1] = a;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
