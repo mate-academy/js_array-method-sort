de440172b69cc1ec3555d4e0c4d69568b3eb580b
kate-prakofyeva <catherine.smiff@gmail.com>
2023-11-08 12:47:53 +0100

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,24 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const length = this.length - 1;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i; j++) {
+        const isCallback = compareFunction
+          ? compareFunction(this[j], this[j + 1]) > 0
+          : this[j].toString() > this[j + 1].toString();
+
+        if (isCallback) {
+          const biggest = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = biggest;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
