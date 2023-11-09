c3e433f1ee22e0ebe27a16283a9eb664269e86c6
kate-prakofyeva <catherine.smiff@gmail.com>
2023-11-09 10:01:52 +0100

    add sugestion fixes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,15 +5,18 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
+    const isCompareFunction = compareFunction !== undefined
+      && compareFunction !== null
+      && typeof compareFunction === 'function';
     const length = this.length - 1;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i; j++) {
-        const isCallback = compareFunction
+        const isSwap = isCompareFunction
           ? compareFunction(this[j], this[j + 1]) > 0
           : this[j].toString() > this[j + 1].toString();
 
-        if (isCallback) {
+        if (isSwap) {
           const biggest = this[j];
 
           this[j] = this[j + 1];
