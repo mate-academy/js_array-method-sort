2e2be3667b277f42f3adda87eeeb1513190c664c
Damian <damian.adamczewski92@gmail.com>
2023-11-10 15:19:31 +0100

    refactored code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,30 +4,20 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    if (typeof compareFunction === 'function') {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = i + 1; j < this.length; j++) {
-          if (compareFunction(this[i], this[j]) > 0) {
-            // Swap elements if compareFunction returns a positive value
-            const temp = this[i];
+  // Default comparison function
+  function sortByDefault(a, b) {
+    return String(a) > String(b) ? 1 : -1;
+  }
 
-            this[i] = this[j];
-            this[j] = temp;
-          }
-        }
-      }
-    } else {
-      // Default sorting behavior (as strings)
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = i + 1; j < this.length; j++) {
-          if (String(this[i]) > String(this[j])) {
-            // Swap elements if default comparison returns true
-            const temp = this[i];
+  [].__proto__.sort2 = function(compareFunction = sortByDefault) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          // Swap elements if compareFunction returns a positive value
+          const temp = this[i];
 
-            this[i] = this[j];
-            this[j] = temp;
-          }
+          this[i] = this[j];
+          this[j] = temp;
         }
       }
     }
