78c05f9ecfc843191ef66c3aacf0ab9af1c74883
Mykhailo Hubko <mixelios@gmail.com>
2023-11-10 21:23:41 +0100

    implemented method sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,33 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (compareFunction === undefined) {
+      for (let i = 0; i < this.length - 1; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          const a = '' + this[i];
+          const b = '' + this[j];
+          const change = this[i];
+
+          if (a > b) {
+            this[i] = this[j];
+            this[j] = change;
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i < this.length - 1; i++) {
+        for (let j = i; j < this.length; j++) {
+          if (compareFunction(this[i], this[j]) > 0) {
+            const temp = this[i];
+
+            this[i] = this[j];
+            this[j] = temp;
+          }
+        }
+      };
+    }
+
+    return this;
   };
 }
 
