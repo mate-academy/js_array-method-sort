51df9fc9f979f8fa1de2cbfbfd53c2479df0dc14
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2023-11-12 18:37:40 +0200

    Fix default value for compareFunction

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,25 +4,16 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => String(a) > String(b)) {
     if (this.length < 2) {
       return this;
     }
 
-    if (compareFunction === undefined) {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = 0; j < this.length - i - 1; j++) {
-          if (String(this[j]) > String(this[j + 1])) {
-            [this[j], this[j + 1]] = [this[j + 1], this[j]];
-          }
-        }
-      }
-    } else {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = 0; j < this.length - i - 1; j++) {
-          if (compareFunction(this[j], this[j + 1]) > 0) {
-            [this[j], this[j + 1]] = [this[j + 1], this[j]];
-          }
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
         }
       }
     }
