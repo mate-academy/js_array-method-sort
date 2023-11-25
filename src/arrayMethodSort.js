05792edb049b96cef2128db11a48481953fc2ded
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-25 07:38:48 +0200

    arrayMethodSort.js

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,18 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (a, b) => `${a}` > `${b}`) {
+    let len = this.length;
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - i - 1; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          let temp = this[j];
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+    return this;
   };
 }
 
