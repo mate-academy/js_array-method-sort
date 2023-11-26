83cde2527c2f20cf20b91a6a8411c4cc10908070
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-11-26 23:40:56 +0200

    sort method impl

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,27 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(callback) {
+    let swapper;
+
+    do {
+      swapper = false;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        if (callback
+          ? callback(this[i], this[i + 1]) > 0
+          : `${this[i]}` > `${this[i + 1]}`
+        ) {
+          const currValue = this[i];
+
+          this[i] = this[i + 1];
+          this[i + 1] = currValue;
+          swapper = true;
+        }
+      }
+    } while (swapper);
+
+    return this;
   };
 }
 
