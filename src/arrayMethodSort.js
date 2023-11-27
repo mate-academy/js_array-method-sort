0e7ba12f02397394101dcab410052946895bdee8
boytrend0108 <boytrend@gmail.com>
2023-11-27 11:01:04 +0200

    fix err

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,23 +3,10 @@
 /**
  * Implement method Sort
  */
+const compareStr = (a, b) => `${a}` > `${b}` ? 1 : -1;
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    if (compareFunction === undefined) {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = 0; j < this.length - 1 - i; j++) {
-          if (this[j].toString() > this[j + 1].toString()) {
-            const tmp = this[j];
-
-            this[j] = this[j + 1];
-            this[j + 1] = tmp;
-          }
-        }
-      }
-
-      return this;
-    }
-
+  [].__proto__.sort2 = function(compareFunction = compareStr) {
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length - 1 - i; j++) {
         if (compareFunction(this[j], this[j + 1]) > 0) {
