dac685d8d2a1a946e30dee7bd61822b7cdf97629
Denis Reznik <reznik.d.o12@gmail.com>
2023-11-29 22:40:29 +0200

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,28 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length; j++) {
+        const prev = this[j];
+
+        if (typeof (compareFunction) === 'function') {
+          if ((compareFunction(this[j], this[j + 1])) > 0) {
+            this[j] = this[j + 1];
+            this[j + 1] = prev;
+          }
+        } else {
+          const a = `${this[j]}`;
+          const b = `${this[j + 1]}`;
+
+          if (a > b) {
+            this[j] = this[j + 1];
+            this[j + 1] = prev;
+          }
+        }
+      }
+    };
+
+    return this;
   };
 }
 
