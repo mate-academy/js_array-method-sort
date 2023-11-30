6e9876a6cf1ccbebbd6884112f6de42d554e4989
Denis Reznik <reznik.d.o12@gmail.com>
2023-11-30 21:46:00 +0200

    add (

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -22,11 +22,11 @@ function applyCustomSort() {
 
 function defaultCompareFunction(a, b) {
   if (typeof (a) === 'number') {
-    return String(a) > String(b) ? 1 : -1;
+    return (String(a) > String(b) ? 1 : -1);
   }
 
   if (typeof (a) === 'string') {
-    return a > b ? 1 : -1;
+    return (a > b ? 1 : -1);
   }
 }
 
