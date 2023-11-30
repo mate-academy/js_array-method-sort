ac9c09f1bf72c8bff81e0e49ddddb493b938cbe9
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2023-11-30 14:17:23 +0100

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const copiedArray = [...this];
+
+    for (let i = 0; i < copiedArray.length - 1; i++) {
+      for (let j = 0; j < copiedArray.length - 1 - i; j++) {
+        const swaped
+        = compareFunction
+          ? compareFunction(copiedArray[j], copiedArray[j + 1]) > 0
+          : `${copiedArray[j]}` > `${copiedArray[j + 1]}`;
+
+        if (swaped) {
+          const temp = copiedArray[j];
+
+          copiedArray[j] = copiedArray[j + 1];
+          copiedArray[j + 1] = temp;
+        }
+      }
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      this[i] = copiedArray[i];
+    }
+
+    return this;
   };
 }
 
