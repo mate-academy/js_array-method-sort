fbb061f63f2ea6d17cfc33ca65f5e609a18f4e0a
Denis Reznik <reznik.d.o12@gmail.com>
2023-11-30 21:04:06 +0200

    add fix solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,7 +4,7 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction = defaultCompareFunction) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length; j++) {
         const prev = this[j];
@@ -14,22 +14,22 @@ function applyCustomSort() {
             this[j] = this[j + 1];
             this[j + 1] = prev;
           }
-        } else {
-          if (typeof (this[j]) === 'string') {
-            if (this[j] > this[j + 1]) {
-              this[j] = this[j + 1];
-              this[j + 1] = prev;
-            }
-          } else if (String(this[j]) > String(this[j + 1])) {
-            this[j] = this[j + 1];
-            this[j + 1] = prev;
-          }
         }
       }
     }
 
     return this;
   };
+}
+
+function defaultCompareFunction(a, b) {
+  if (typeof (a) === 'number') {
+    return String(a) > String(b) ? 1 : -1;
+  }
+
+  if (typeof (a) === 'string') {
+    return a > b ? 1 : -1;
+  }
 }
 
 module.exports = applyCustomSort;
