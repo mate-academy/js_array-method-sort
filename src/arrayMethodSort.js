8a76c2ce9e4858bf75b0d3cfe35d7517a2f69d00
Vladyslav Starodub <starodubvlad08@gmail.com>
2023-11-30 16:56:41 +0200

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,28 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      for (let o = i + 1; o < this.length; o++) {
+        const mainItem = this[i];
+        const checkItem = this[o];
+        let doChange;
+
+        if (typeof compareFunction === 'function') {
+          doChange = compareFunction(mainItem, checkItem);
+        } else {
+          doChange = `${mainItem}` > `${checkItem}`;
+        }
+
+        if (doChange > 0) {
+          const temp = mainItem;
+
+          this[i] = checkItem;
+          this[o] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
