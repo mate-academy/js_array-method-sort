22d242907f8c74e3c0ae543e07b728251461a0ab
Vladyslav Starodub <starodubvlad08@gmail.com>
2023-12-01 09:39:10 +0200

    set default value for `compareFunction`

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,18 +4,16 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => {
+      return `${a}` > `${b}` ? 1 : -1;
+    }
+  ) {
     for (let i = 0; i < this.length; i++) {
       for (let o = i + 1; o < this.length; o++) {
         const mainItem = this[i];
         const checkItem = this[o];
-        let doChange;
-
-        if (typeof compareFunction === 'function') {
-          doChange = compareFunction(mainItem, checkItem);
-        } else {
-          doChange = `${mainItem}` > `${checkItem}`;
-        }
+        const doChange = compareFunction(mainItem, checkItem);
 
         if (doChange > 0) {
           const temp = mainItem;
