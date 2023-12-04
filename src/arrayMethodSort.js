f03264896eb55ca12f05c65dda36a0499d7b0a14
Iryna Lysko <goiralysko@gmail.com>
2023-12-04 19:03:22 +0200

    add sort method implementation

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,35 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (!compareFunction) {
+      for (let i = 0; i < this.length; i++) {
+        let temp = this[i];
+
+        for (let j = 0; j < this.length; j++) {
+          if (this[i].toString() <= this[j].toString()) {
+            temp = this[j];
+            this[j] = this[i];
+            this[i] = temp;
+          }
+        }
+      }
+    }
+
+    if (compareFunction) {
+      for (let i = 0; i < this.length; i++) {
+        let temp = this[i];
+
+        for (let j = 0; j < this.length; j++) {
+          if (compareFunction(this[i], this[j]) <= 0) {
+            temp = this[j];
+            this[j] = this[i];
+            this[i] = temp;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
