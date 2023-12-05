7f35cb6b8e50e04e1f0a379b2289cc86198bb89f
Oleksii Karpov <alneon1@gmail.com>
2023-12-05 12:53:58 +0200

    added task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,29 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let stepsCount = this.length - 1;
+    let swapped;
+
+    function compareElements(a, b, compareFunc) {
+      return compareFunc
+        ? compareFunc(a, b) > 0
+        : a.toString() > b.toString();
+    }
+
+    do {
+      swapped = false;
+
+      for (let i = 0; i < stepsCount; i++) {
+        if (compareElements(this[i], this[i + 1], compareFunction)) {
+          [this[i + 1], this[i]] = [this[i], this[i + 1]];
+          swapped = true;
+        }
+      }
+      stepsCount--;
+    } while (swapped);
+
+    return this;
   };
 }
 
