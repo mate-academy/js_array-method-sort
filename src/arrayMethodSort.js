b8eb5113ee2ea627c83d50872326b09758fd080b
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2023-12-07 19:56:02 +0200

    add sort method solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,21 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (a, b) => {
+    return a.toString() > b.toString();
+  }) {
+    for (let i = 0; i < this.length; i++) {
+      for (let k = i + 1; k < this.length; k++) {
+        if (compareFunction(this[i], this[k]) > 0) {
+          const valueI = this[i];
+
+          this[i] = this[k];
+          this[k] = valueI;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
