f57c284d19fc6e9ab1b9cd916773be1e25ee6313
Maksym Nazarenko <71183975+maks-n@users.noreply.github.com>
2023-12-10 17:22:10 -0500

    add solution with bubble sort as default

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,33 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = (a, b) => bubbleSort(a, b)) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < (this.length - i - 1); j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
+}
+
+function bubbleSort(a, b) {
+  const aStr = a.toString();
+  const bStr = b.toString();
+
+  for (let i = 0; i < aStr.length; i++) {
+    if (aStr.charAt(i) < bStr.charAt(i)) {
+      return -1;
+    } else if (aStr.charAt(i) > bStr.charAt(i)) {
+      return 1;
+    }
+  }
 }
 
 module.exports = applyCustomSort;
