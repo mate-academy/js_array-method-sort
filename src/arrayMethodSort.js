0e636916e6ac701e93813997d3fea225e8079daf
Stephen Forde <sforde08@gmail.com>
2023-12-10 21:35:34 +0000

    array method sort soultion 1

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,16 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compareFunction ? compareFunction(this[j],
+          this[j + 1]) > 0 : `${this[j]}` > `${this[j + 1]}`) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
