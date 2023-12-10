05fd1d0422683c26d8720cdf7af39b27561f6695
Serhii Gramma <grammas56@gmail.com>
2023-12-10 13:01:42 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,22 @@
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(cb) {
+    for (let i = 0; i < this.length - 1; i++) {
+      const currElem = this[i];
+      const nextElem = this[i + 1];
+
+      if (cb ? cb(nextElem, currElem, i, this) < 0
+        : nextElem.toString()[0] < currElem.toString()[0]) {
+        this.splice(i + 1, 1);
+        this.unshift(nextElem);
+        i = -1;
+      }
+    }
+
+    return this;
   };
 }
 
