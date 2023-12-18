e07141995e99d6695dddb0f5244c0a16814fe639
Vasil Liakhivskyi <vazilx@gmail.com>
2023-12-18 17:27:56 +0100

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,22 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const callBek = compareFunction === undefined
+      ? (item1, item2) => `${item1}` > `${item2}` ? 1 : 0
+      : compareFunction;
+
+    for (let ii = this.length - 1; ii > 0; ii--) {
+      for (let i = 0; i < ii; i++) {
+        if (callBek(this[i], this[i + 1]) > 0) {
+          const _ = this[i];
+
+          this[i] = this[i + 1];
+          this[i + 1] = _;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
