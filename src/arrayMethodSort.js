9c563c784713634f3e6fe7d148a848e0d5859482
Alyona Nabok <alyonana42@gmail.com>
2023-12-19 14:08:26 +0200

    add const for default value

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,9 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction =
-  (a, b) => (a + '') > (b + '')) {
+  const defaultCompareFunc = (a, b) => (a + '') > (b + '');
+
+  [].__proto__.sort2 = function(compareFunction = defaultCompareFunc) {
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.slice(0, i).length; j++) {
         if (compareFunction(this[j], this[i]) === true
