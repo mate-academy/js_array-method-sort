e077ff4f5a6fcadcf377159a995f18dd340dfcda
Oleksandr Boiko <iccc97@gmail.com>
2023-12-22 12:09:08 +0200

    add fixes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -15,11 +15,10 @@ function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction = defaultCompare) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
-        if (compareFunction(this[j], this[j + 1]) > 0) {
-          const temp = this[j];
+        const compare = compareFunction(this[j], this[j + 1]);
 
-          this[j] = this[j + 1];
-          this[j + 1] = temp;
+        if (compare > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
         }
       }
     }
