db5cf7178ca42df25783877c074f1cb642cfba1e
Bohdan Ivankov <bodanich600@gmail.com>
2023-12-26 15:36:44 +0000

    remove comments

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -19,36 +19,3 @@ function applyCustomSort() {
 }
 
 module.exports = applyCustomSort;
-
-// function applyCustomSort() {
-//   [].__proto__.sort2 = function(compareFunction = (a, b) => {
-//     a.toString();
-//     b.toString();
-
-//     if (a < b) {
-//       return -1;
-//     } else if (a === b) {
-//       return 0;
-//     } else if (a > b) {
-//       return 1;
-//     }
-//   }) {
-//     let sortedArray = [];
-
-//     for (let i = 0; i < this.length; i++) {
-//       if (this[i + 1] !== undefined) {
-//         if (compareFunction(this[i], this[i + 1]) === -1) {
-//           sortedArray.push(this[i]);
-//         } else if (compareFunction(this[i], this[i + 1]) === 0) {
-//           sortedArray.push(this[i]);
-//         } else if (compareFunction(this[i], this[i + 1]) === 1) {
-//           sortedArray.push(this[i]);
-//         }
-//       } else {
-//         sortedArray.push(this[i]);
-//       }
-//     }
-
-//     return sortedArray;
-//   };
-// }
