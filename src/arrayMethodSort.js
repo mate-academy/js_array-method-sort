023fef1abff906ec54bc9dd179e48f55d1fab9bc
Rostyslav Tkachuk <pro100kor.moon@gmail.com>
2023-12-27 00:01:19 +0200

    implement method Sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,10 +3,34 @@
 /**
  * Implement method Sort
  */
-function applyCustomSort() {
+function applyCustomSort(callback) {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let step = 0; step < this.length; step++) {
+      for (let i = 0; i < this.length; i++) {
+        const currValue = this[i];
+        const nextValue = this[i + 1];
+
+        if (nextValue) {
+          if (compareFunction) {
+            if (compareFunction(currValue, nextValue) > 0) {
+              this[i] = nextValue;
+              this[i + 1] = currValue;
+            }
+          } else if (typeof currValue === 'string' && nextValue < currValue) {
+            this[i] = nextValue;
+            this[i + 1] = currValue;
+          } else if (nextValue.toString() < currValue.toString()) {
+            this[i] = nextValue;
+            this[i + 1] = currValue;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
+
+applyCustomSort();
 
 module.exports = applyCustomSort;
