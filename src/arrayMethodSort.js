9765f48bb3dac54e4e034255c727cac90a787931
Rostyslav Tkachuk <pro100kor.moon@gmail.com>
2023-12-28 18:36:40 +0200

    small fixes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,22 +6,17 @@
 function applyCustomSort(callback) {
   [].__proto__.sort2 = function(compareFunction) {
     for (let step = 0; step < this.length; step++) {
-      for (let i = 0; i < this.length; i++) {
+      for (let i = 0; i < this.length - step - 1; i++) {
         const currValue = this[i];
         const nextValue = this[i + 1];
 
         if (nextValue) {
           if (compareFunction) {
             if (compareFunction(currValue, nextValue) > 0) {
-              this[i] = nextValue;
-              this[i + 1] = currValue;
+              [this[i], this[i + 1]] = [nextValue, currValue];
             }
-          } else if (typeof currValue === 'string' && nextValue < currValue) {
-            this[i] = nextValue;
-            this[i + 1] = currValue;
           } else if (nextValue.toString() < currValue.toString()) {
-            this[i] = nextValue;
-            this[i + 1] = currValue;
+            [this[i], this[i + 1]] = [nextValue, currValue];
           }
         }
       }
