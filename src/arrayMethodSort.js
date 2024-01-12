6aca59fb73919ad27a79df05a2f16f280ec0171f
marichkamt <emilycry1999@gmail.com>
2024-01-12 21:34:43 +0100

    array method sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,31 +4,12 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    let actualCompareFunction = compareFunction;
-
-    if (typeof actualCompareFunction !== 'function') {
-      actualCompareFunction = (a, b) => {
-        if (a < b) {
-          return -1;
-        }
-
-        if (a > b) {
-          return 1;
-        }
-
-        return 0;
-      };
-    }
-
-    for (let i = 0; i < this.length - 1; i++) {
-      for (let k = 0; k < this.length - 1; k++) {
-        if (actualCompareFunction(this[k].toString(),
-          this[k + 1].toString()) > 0) {
-          const temp = this[k];
-
-          this[k] = this[k + 1];
-          this[k + 1] = temp;
+  [].__proto__.sort2 = function(compareFunction = (a, b) =>
+    String(a) > String(b)) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[j], this[i]] = [this[i], this[j]];
         }
       }
     }
