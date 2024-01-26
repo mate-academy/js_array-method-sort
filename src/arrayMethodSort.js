f3bb6aa6d3b68a41b313443599e01d63d486fe99
beliyandr <beliyandr@gmail.com>
2024-01-26 17:16:27 +0200

    First try sort_v1

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,62 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    function swap(i, j, array) {
+      const temp = array[i];
+
+      array[i] = array[j];
+      array[j] = temp;
+    }
+
+    if (compareFunction === undefined) {
+      let flag = false;
+
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          const a = this[i].toString().charCodeAt();
+          const b = this[j].toString().charCodeAt();
+
+          if (a === b) {
+            if (this[i] > this[j]) {
+              swap(i, j, this);
+              i = j;
+              flag = true;
+            }
+          }
+
+          if (a > b) {
+            swap(i, j, this);
+            i = j;
+            flag = true;
+          }
+        }
+
+        if (flag) {
+          flag = false;
+          i = -1;
+        }
+      }
+
+      return this;
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        let flag = false;
+
+        for (let j = i + 1; j < this.length; j++) {
+          if (compareFunction(this[i], this[j]) > 0) {
+            swap(i, j, this);
+            flag = true;
+          }
+        }
+
+        if (flag) {
+          i = -1;
+        }
+      }
+
+      return this;
+    }
   };
-}
+};
 
 module.exports = applyCustomSort;
