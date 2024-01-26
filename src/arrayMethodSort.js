2da87bf02bf1ef89e22fe7220f97956cf8eb3993
ArsenSkandalenko <arsenskandal@gmail.com>
2024-01-26 23:30:51 +0300

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,10 +3,25 @@
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compare) {
     // write code here
+    [].__proto__.sort2 = function(
+      compareFunction = (a, b) => String(a) > String(b)) {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          const currentValue = this[j];
+
+          if (compareFunction(this[i], currentValue) > 0) {
+            this[j] = this[i];
+            this[i] = currentValue;
+          }
+        }
+      }
+
+      return this;
+    };
   };
 }
-
 module.exports = applyCustomSort;
