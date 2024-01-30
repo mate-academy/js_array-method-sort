d122c04994245e033a68dbefe8b0a1a6b9e21a23
Illia Vasulenko <vasulenko.illia@gmail.com>
2024-01-30 21:08:16 +0200

    Solution fix

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,20 +3,17 @@
 /**
  * Implement method Sort
  */
+function compareFunctionDefault(item1, item2) {
+  return item1.toString() > item2.toString();
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction = compareFunctionDefault) {
     const result = this;
-
-    function compareFunctionDefault(item1, item2) {
-      return item1.toString() > item2.toString();
-    }
-
-    const currentSortFunction = compareFunction
-    === undefined ? compareFunctionDefault : compareFunction;
 
     for (let i = 0; i < result.length; i++) {
       for (let j = 0; j < (result.length - i - 1); j++) {
-        if (currentSortFunction(result[j], result[j + 1]) > 0) {
+        if (compareFunction(result[j], result[j + 1]) > 0) {
           const temp = result[j];
 
           result[j] = result[j + 1];
