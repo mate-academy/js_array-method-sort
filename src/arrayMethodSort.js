131a0ab27f695e55555bb3fcd001437bd2c0b308
Taras Nechyporuck <tarasnechyporuck@gmail.com>
2024-02-01 18:31:31 +0200

    task done;

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,28 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const arrayLength = this.length;
+
+    for (let i = 0; i < arrayLength - 1; i++) {
+      for (let j = 0; j < arrayLength - i - 1; j++) {
+        let shouldSwap = false;
+
+        if (compareFunction) {
+          shouldSwap = compareFunction(this[j], this[j + 1]) > 0;
+        } else {
+          shouldSwap = String(this[j]) > String(this[j + 1]);
+        }
+
+        if (shouldSwap) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
