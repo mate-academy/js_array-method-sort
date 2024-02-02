80b1a86254fb6744ceb888a6d5f9f3986dc51c83
Katya <evil.666.buny@gmail.com>
2024-02-02 11:11:31 +0200

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,39 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const effectiveCompareFunction = typeof compareFunction === 'function'
+      ? compareFunction
+      : (a, b) => {
+        if (String(a) < String(b)) {
+          return -1;
+        }
+
+        if (String(a) > String(b)) {
+          return 1;
+        }
+
+        return 0;
+      };
+
+    let n = this.length;
+    let swapped;
+
+    do {
+      swapped = false;
+
+      for (let i = 1; i < n; i++) {
+        if (effectiveCompareFunction(this[i - 1], this[i]) > 0) {
+          const temp = this[i - 1];
+
+          this[i - 1] = this[i];
+          this[i] = temp;
+          swapped = true;
+        }
+      }
+      n--;
+    } while (swapped);
+
+    return this;
   };
 }
 
