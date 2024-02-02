dbf86e35f173602a1b0ede90a938ba35497e5556
bla2001 <blasmartlab@gmail.com>
2024-02-02 17:56:24 +0200

    Solution2

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,21 +5,17 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    const tempArray = this.slice();
-
-    for (let i = 0; i < tempArray.length - 1; i++) {
-      for (let j = i + 1; j < tempArray.length; j++) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = i + 1; j < this.length; j++) {
         const comparisonFunc = compareFunction || compareAsFunction;
-        const functionA = String(tempArray[i]);
-        const functionB = String(tempArray[j]);
+        const functionA = String(this[i]);
+        const functionB = String(this[j]);
 
         if (comparisonFunc(functionA, functionB) > 0) {
-          [tempArray[i], tempArray[j]] = [tempArray[j], tempArray[i]];
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
-
-    this.splice(0, this.length, ...tempArray);
 
     return this;
   };
