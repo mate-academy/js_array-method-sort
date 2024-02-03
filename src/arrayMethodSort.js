9c7a1e0e29078d4ef4d66b7b1b2ad63135ae4de6
Marina Semipyatnaya <epicentr50@gmail.com>
2024-02-03 21:37:18 +0200

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,19 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compareFunction ? compareFunction(this[j], this[j + 1])
+          > 0 : String(this[j]) > String(this[j + 1])) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
