de9c7e1083601c2c8fcadb5186a9557af68369d1
Olha Shvets <lv160888soi@gmail.com>
2024-02-05 19:55:47 +0200

    fixed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,19 +4,14 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    let comparator = compareFunction;
-    const defaultCompare = (a, b) => {
-      return String(a) > String(b);
-    };
+  const defaultCompare = (a, b) => {
+    return String(a) > String(b);
+  };
 
-    if (!compareFunction) {
-      comparator = defaultCompare;
-    }
-
+  [].__proto__.sort2 = function(compareFunction = defaultCompare) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - 1 - i; j++) {
-        if (comparator(this[j], this[j + 1]) > 0) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
           [this[j], this[j + 1]] = [this[j + 1], this[j]];
         }
       }
