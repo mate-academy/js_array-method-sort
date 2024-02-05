3c8bc3bd08214fa56e09708f871f9e1df7050a8d
Nazar Kovalenko <xeilen1112@gmail.com>
2024-02-05 22:18:29 +0200

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    function swapItems(index) {
+      const temp = this[index];
+
+      this[index] = this[index - 1];
+      this[index - 1] = temp;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (compareFunction) {
+        if (compareFunction(this[i - 1], this[i]) > 0) {
+          swapItems.call(this, i);
+          i = 0;
+        }
+        continue;
+      }
+
+      if (this[i - 1].toString() > this[i].toString()) {
+        swapItems.call(this, i);
+        i = 0;
+      }
+    }
+
+    return this;
   };
 }
 
