652ada26313a10049962dfc709e662362846052b
beliyandr <beliyandr@gmail.com>
2024-02-05 20:47:39 +0200

    fix, use method bubble

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,78 +3,23 @@
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    function sortString(data) {
-      for (let i = 0; i < data.length; i++) {
-        let flag = false;
+  [].__proto__.sort2 = function(compareFunction =
+  (a, b) => String(a) > String(b) ? 1 : -1) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
 
-        for (let j = i + 1; j < data.length; j++) {
-          const a = data[i].toString();
-          const b = data[j].toString();
-
-          if (a === b) {
-            if (data[i] > data[j]) {
-              const temp = data[i];
-
-              data[i] = data[j];
-              data[j] = temp;
-              i = j;
-              flag = true;
-            }
-          }
-
-          if (a > b) {
-            const temp = data[i];
-
-            data[i] = data[j];
-            data[j] = temp;
-            i = j;
-            flag = true;
-          }
-        }
-
-        if (flag) {
-          flag = false;
-          i = -1;
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
         }
       }
-    }
-
-    if (compareFunction === undefined) {
-      sortString(this);
-
-      return this;
-    }
-
-    if (compareFunction) {
-      for (let i = 0; i < this.length; i++) {
-        let flag = false;
-
-        for (let j = i + 1; j < this.length; j++) {
-          if (compareFunction(this[i], this[j]) > 0) {
-            const temp = this[i];
-
-            this[i] = this[j];
-            this[j] = temp;
-            flag = true;
-            i--;
-          } else if (compareFunction(this[i], this[i + 1]) < 0
-            || (this[i], this[i + 1]) === 0) {
-            continue;
-          }
-        }
-
-        if ((i === this.length - 1) && flag) {
-          i = -1;
-        }
-      }
-
-      return this;
     }
 
     return this;
   };
-};
+}
 
 module.exports = applyCustomSort;
