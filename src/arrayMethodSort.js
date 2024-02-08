cb6faf02cb58083050b46a28a0258f68046687b4
Denys Lysiak <lisyak99.dl@gmail.com>
2024-02-08 11:43:11 +0200

    add suggested changes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,36 +4,18 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    let callCompare = compareFunction;
-
-    if (compareFunction === undefined) {
-      callCompare = function(a, b) {
-        const strA = a.toString();
-        const strB = b.toString();
-
-        if (strA < strB) {
-          return -1;
-        } else if (strA > strB) {
-          return 1;
-        }
-
-        return 0;
-      };
-    }
-
-    let arrLength = this.length - 1;
-
+  [].__proto__.sort2 = function(compareFunction = (a, b) => {
+    return `${a}` > `${b}` ? 1 : 0;
+  }) {
     for (let i = 0; i < this.length; i++) {
-      for (let n = 0; n < arrLength; n++) {
-        if (callCompare(this[n], this[n + 1]) > 0) {
+      for (let n = 0; n < this.length - 1 - i; n++) {
+        if (compareFunction(this[n], this[n + 1]) > 0) {
           const temp = this[n];
 
           this[n] = this[n + 1];
           this[n + 1] = temp;
         }
       }
-      arrLength--;
     }
 
     return this;
