5cf6b4e5f24adbb238409d4d0169087e1aa158b9
Lesia Bihun <lbr15@ukr.net>
2024-02-10 08:57:01 +0200

    add array method sort solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,26 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (this[j].toString() < this[i].toString()) {
+          const temp = this[i];
+
+          this[i] = this[j];
+          this[j] = temp;
+        }
+
+        if (compareFunction !== undefined
+          && compareFunction(this[j], this[i]) < 0) {
+          const temp = this[i];
+
+          this[i] = this[j];
+          this[j] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
