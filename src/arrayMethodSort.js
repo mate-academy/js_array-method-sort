31a25c0ec744edb58ce85cdd18d52effa9093083
katya havriuk <0303gavkat@gmail.com>
2024-02-09 18:30:00 -0700

    solved task

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,33 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let n = this.length;
+    let swapped;
+
+    do {
+      swapped = false;
+
+      for (let i = 1; i < n; i++) {
+        const isCompareFunction = compareFunction !== undefined
+          && compareFunction !== null
+          && typeof compareFunction === 'function';
+        let shouldSwap;
+
+        if (isCompareFunction) {
+          shouldSwap = compareFunction(this[i - 1], this[i]) > 0;
+        } else {
+          shouldSwap = this[i - 1].toString() > this[i].toString();
+        }
+
+        if (shouldSwap) {
+          [this[i - 1], this[i]] = [this[i], this[i - 1]];
+          swapped = true;
+        }
+      }
+      n--;
+    } while (swapped);
+
+    return this;
   };
 }
 
