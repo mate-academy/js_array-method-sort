5648b2b7731bf3480b39567c1c35e4bcd02371e6
denys <denis.bielykh38@gmail.com>
2024-02-13 14:26:58 +0200

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,27 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(
+    compareFunction = (a, b) => {
+      if (`${a}` > `${b}`) {
+        return 1;
+      };
+
+      return -1;
+    }
+  ) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let n = i + 1; n < this.length; n++) {
+        if (compareFunction(this[i], this[n]) > 0) {
+          const ELEM = this[i];
+
+          this[i] = this[n];
+          this[n] = ELEM;
+        };
+      }
+    };
+
+    return this;
   };
 }
 
