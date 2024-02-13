b1f393be6eaced661536084a13f6c93611f4b7d4
Vasyl Chukhrai <wasik1995@gmail.com>
2024-02-13 15:27:35 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,27 +1,27 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
+function compare(elementFirst, elementSecond) {
+  if (String(elementFirst) < String(elementSecond)) {
+    return -1;
+  };
+
+  if (String(elementFirst) > String(elementSecond)) {
+    return 1;
+  };
+
+  return 0;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function(compareFunction = compare) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
         const elementFirst = this[j];
         const elementSecond = this[j + 1];
 
-        if (compareFunction) {
-          if (compareFunction(elementFirst, elementSecond) >= 1) {
-            this[j] = elementSecond;
-            this[j + 1] = elementFirst;
-          }
-        } else {
-          if (`${elementFirst}` < `${elementSecond}`) {
-            continue;
-          } else {
-            this[j] = elementSecond;
-            this[j + 1] = elementFirst;
-          }
+        if (compareFunction(elementFirst, elementSecond) > 0) {
+          this[j] = elementSecond;
+          this[j + 1] = elementFirst;
         }
       }
     }
