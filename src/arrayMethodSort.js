cc6507aa715050169e9a5d9533b57772280e35f6
mfcastilho <fredericocastilho@hotmail.com>
2024-02-14 16:28:57 -0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,21 +4,17 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    if (typeof compareFunction !== 'function') {
-      compareFunction = (a, b) => {
-        if (String(a) < String(b)) {
-          return -1;
-        }
-        if (String(a) > String(b)) {
-          return 1;
-        }
-        return 0;
-      }
+  [].__proto__.sort2 = function(compareFunction = (a, b) => {
+    if (String(a) < String(b)) {
+      return -1;
     }
-
+    if (String(a) > String(b)) {
+      return 1;
+    }
+    return 0;
+  }) {
     for (let i = 0; i < this.length - 1; i++) {
-      for (let j = 0; j < this.length - 1 - i; j++) { 
+      for (let j = 0; j < this.length - 1 - i; j++) {
         if (compareFunction(this[j], this[j + 1]) > 0) {
           const temp = this[j];
           this[j] = this[j + 1];
@@ -26,7 +22,7 @@ function applyCustomSort() {
         }
       }
     }
-
+  
     return this;
   };
 }
