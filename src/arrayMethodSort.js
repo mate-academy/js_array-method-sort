b94ce31a81f3931ed5d2d4358a03cd619292f2d4
Nikita Ganzha <geekneek28@gmail.com>
2024-02-15 11:18:53 +0200

    created default compare function, code refactor

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,28 +3,23 @@
 /**
  * Implement method Sort
  */
+
+const defaultCompareFunction = (a, b) => {
+  if (a.toString() > b.toString()) {
+    return 1;
+  } else if (a.toString() < b.toString()) {
+    return -1;
+  } else {
+    return 0;
+  }
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    if (compareFunction) {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = i + 1; j < this.length; j++) {
-          if (compareFunction(this[i], this[j]) > 0) {
-            const temp = this[i];
-
-            this[i] = this[j];
-            this[j] = temp;
-          }
-        }
-      }
-    } else {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = i + 1; j < this.length; j++) {
-          if (this[i].toString() > this[j].toString()) {
-            const temp = this[i];
-
-            this[i] = this[j];
-            this[j] = temp;
-          }
+  [].__proto__.sort2 = function(compareFunction = defaultCompareFunction) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
