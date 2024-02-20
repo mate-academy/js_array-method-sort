1e04659ce5a0dfd2f23aaa94a7908c4287b799d4
Oleksandra Kuziura <radioday2014@gmail.com>
2024-02-20 17:55:03 +0200

    imlement sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < (this.length - i - 1); j++) {
+        if (compareFunction === undefined) {
+          if (this[j] + '' > this[j + 1] + '') {
+            const temp = this[j];
+
+            this[j] = this[j + 1];
+            this[j + 1] = temp;
+          }
+          continue;
+        }
+
+        if (compareFunction(this[j], this[j + 1]) === 1
+        || compareFunction(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
