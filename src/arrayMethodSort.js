9da6c7afb9246d92e87f426f692fd7a669f0d9b9
Justin <thedarkvibe1@gmail.com.com>
2024-02-20 17:19:49 +0100

    taskComplete

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,32 @@
  * Implement method Sort
  */
 function applyCustomSort() {
+  // prettier-ignore
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let hadToSwitch = false;
+
+    const originalArr = this;
+
+    do {
+      hadToSwitch = false;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        const compareResult
+          = compareFunction === undefined
+            ? this[i].toString() > this[i + 1].toString()
+            : compareFunction(this[i], this[i + 1]) > 0;
+
+        if (compareResult) {
+          const temporaryEl = this[i];
+
+          this[i] = this[i + 1];
+          this[i + 1] = temporaryEl;
+          hadToSwitch = true;
+        }
+      }
+    } while (hadToSwitch);
+
+    return originalArr;
   };
 }
 
