c7be88061d78710c7de157e75792a9abc65d7c34
Daruna Kolinska <daruna.kolinska@gmail.com>
2024-02-20 15:13:50 +0200

    Removal of redundant logic

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,21 +6,7 @@
 function applyCustomSort() {
   [].__proto__.sort2
   = function(customCompareFunction = (a, b) => `${a}` > `${b}`) {
-      let compareFunction = customCompareFunction;
-
-      if (typeof compareFunction !== 'function') {
-        compareFunction = (a, b) => {
-          if (a < b) {
-            return -1;
-          }
-
-          if (a > b) {
-            return 1;
-          }
-
-          return 0;
-        };
-      }
+      const compareFunction = customCompareFunction;
 
       for (let i = 0; i < this.length; i++) {
         for (let j = 0; j < this.length - i - 1; j++) {
