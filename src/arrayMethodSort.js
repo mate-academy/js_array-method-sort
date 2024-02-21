24bca0b73895da346f63c9d471f006a1ec549bc7
Ruslan Ogerenko <ruslanogerenko@gmail.com>
2024-02-21 02:05:15 +0200

    solve the task

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,36 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let compare = compareFunction;
+
+    if (!compareFunction) {
+      compare = function(a, b) {
+        if (String(a) < String(b)) {
+          return -1;
+        }
+
+        if (String(a) > String(b)) {
+          return 1;
+        }
+
+        return 0;
+      };
+    }
+
+    const len = this.length;
+
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - i - 1; j++) {
+        if (compare(this[j], this[j + 1]) > 0) {
+          const current = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = current;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
