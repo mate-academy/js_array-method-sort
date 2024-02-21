47cbfcb8140f373c5e44da53f4886668e48b006b
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-02-21 14:06:01 +0100

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,38 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length - 1; i++) {
+      let swapped = false;
+
+      for (let j = 0; j < this.length - i - 1; j++) {
+        const a = this[j].toString();
+        const b = this[j + 1].toString();
+
+        if (compareFunction) {
+          if (compareFunction(a, b) > 0) {
+            const temp = this[j];
+
+            this[j] = this[j + 1];
+            this[j + 1] = temp;
+            swapped = true;
+          }
+        } else {
+          if (a > b) {
+            const temp = this[j];
+
+            this[j] = this[j + 1];
+            this[j + 1] = temp;
+            swapped = true;
+          }
+        }
+      }
+
+      if (!swapped) {
+        break;
+      };
+    }
+
+    return this;
   };
 }
 
