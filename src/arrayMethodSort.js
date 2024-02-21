4f9bc6f879a6e7c3b924702a8fddca780e78463c
Oleksandra Kuziura <radioday2014@gmail.com>
2024-02-21 18:53:42 +0200

    imlement worked solition, cod doesn passed test if applied mentor`s previous suggestion

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -8,13 +8,6 @@ function applyCustomSort() {
   (a, b) => String(a) > String(b)) {
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < (this.length - i - 1); j++) {
-        if (this[j] + '' > this[j + 1] + '') {
-          const temp = this[j];
-
-          this[j] = this[j + 1];
-          this[j + 1] = temp;
-        }
-
         if (compareFunction(this[j], this[j + 1]) === 1
         || compareFunction(this[j], this[j + 1]) > 0) {
           const temp = this[j];
