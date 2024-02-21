60be7b3d2f819a02ba542074629eb101d8a8bb20
Oleksandra Kuziura <radioday2014@gmail.com>
2024-02-21 18:19:22 +0200

    add function default value remove redutant check

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,17 +4,15 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction = 0) {
+  [].__proto__.sort2 = function(compareFunction =
+  (a, b) => String(a) > String(b)) {
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < (this.length - i - 1); j++) {
-        if (compareFunction === 0) {
-          if (this[j] + '' > this[j + 1] + '') {
-            const temp = this[j];
+        if (this[j] + '' > this[j + 1] + '') {
+          const temp = this[j];
 
-            this[j] = this[j + 1];
-            this[j + 1] = temp;
-          }
-          continue;
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
         }
 
         if (compareFunction(this[j], this[j + 1]) === 1
