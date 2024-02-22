110826afc66744b26147d9052a447f53d963bc41
Denis Kotov <dionisiy2904@gmail.com>
2024-02-22 14:37:13 +0200

    Method sorted

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,38 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    let compareFunctionMain = compareFunction;
+
+    if (typeof compareFunction === 'function') {
+      compareFunctionMain = (a, b) => {
+        const valueA = '' + a;
+        const valueB = '' + b;
+
+        if (valueA > valueB) {
+          return 1;
+        }
+
+        if (valueA < valueB) {
+          return -1;
+        }
+
+        return 0;
+      };
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunctionMain(this[i], this[j]) > 0) {
+          const temp = this[i];
+
+          this[i] = this[j];
+          this[j] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
