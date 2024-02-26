ef2fdda665e922facff1d50a19c97e77c4c5d947
Medvid Oleksandr <samaster010604@gmail.com>
2024-02-26 17:00:54 +0200

    add change

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,23 +4,11 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    for (let i = 0; i < this.length - 1; i++) {
-      for (let j = 0; j < this.length - 1 - i; j++) {
-        if (compareFunction) {
+  [].__proto__
+    .sort2 = function(compareFunction = (a, b) => String(a) > String(b)) {
+      for (let i = 0; i < this.length - 1; i++) {
+        for (let j = 0; j < this.length - 1 - i; j++) {
           if (compareFunction(this[j], this[j + 1]) > 0) {
-            const buf = this[j];
-
-            this[j] = this[j + 1];
-            this[j + 1] = buf;
-          }
-        } else {
-          if (this[j].toString()[0] > this[j + 1].toString()[0]) {
-            const buf = this[j];
-
-            this[j] = this[j + 1];
-            this[j + 1] = buf;
-          } else if (this[j].toString()[1] > this[j + 1].toString()[1]) {
             const buf = this[j];
 
             this[j] = this[j + 1];
@@ -28,10 +16,9 @@ function applyCustomSort() {
           }
         }
       }
-    }
 
-    return this;
-  };
+      return this;
+    };
 }
 
 module.exports = applyCustomSort;
