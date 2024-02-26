985b52971dccb3ebea7411db8343c37440fc42fe
Medvid Oleksandr <samaster010604@gmail.com>
2024-02-26 15:39:55 +0200

    methodSort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,32 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compareFunction) {
+          if (compareFunction(this[j], this[j + 1]) > 0) {
+            const buf = this[j];
+
+            this[j] = this[j + 1];
+            this[j + 1] = buf;
+          }
+        } else {
+          if (this[j].toString()[0] > this[j + 1].toString()[0]) {
+            const buf = this[j];
+
+            this[j] = this[j + 1];
+            this[j + 1] = buf;
+          } else if (this[j].toString()[1] > this[j + 1].toString()[1]) {
+            const buf = this[j];
+
+            this[j] = this[j + 1];
+            this[j + 1] = buf;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
