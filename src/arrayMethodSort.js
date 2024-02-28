757434e1ad92a5712705d7dcd6696f90a37db05c
Denis Kotov <dionisiy2904@gmail.com>
2024-02-28 10:09:00 +0200

    change function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,29 +4,25 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    let compareFunctionMain = compareFunction;
+  const compareFunctionMain = (a, b) => {
+    const valueA = `${a}`;
+    const valueB = `${b}`;
 
-    if (typeof compareFunction !== 'function') {
-      compareFunctionMain = (a, b) => {
-        const valueA = '' + a;
-        const valueB = '' + b;
-
-        if (valueA > valueB) {
-          return 1;
-        }
-
-        if (valueA < valueB) {
-          return -1;
-        }
-
-        return 0;
-      };
+    if (valueA > valueB) {
+      return 1;
     }
 
+    if (valueA < valueB) {
+      return -1;
+    }
+
+    return 0;
+  };
+
+  [].__proto__.sort2 = function(compareFunction = compareFunctionMain) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        if (compareFunctionMain(this[i], this[j]) > 0) {
+        if (compareFunction(this[i], this[j]) > 0) {
           const temp = this[i];
 
           this[i] = this[j];
