d17810d4e511e432bd43064e4103fba29c654fa8
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-03-01 01:15:26 +0200

    sorted an array

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,30 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const compare = compareFunction || toSort;
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compare(this[i], this[j]) > 0) {
+          const tmp = this[i];
+          this[i] = this[j];
+          this[j] = tmp;
+        }
+      }
+    }
+
+    return this;
   };
 }
+
+function toSort(a, b) {
+  if (String(a) > String(b)) {
+    return 1;
+  } else if (String(b) > String(a)) {
+    return -1;
+  } else {
+    return 0;
+  }
+};
 
 module.exports = applyCustomSort;
