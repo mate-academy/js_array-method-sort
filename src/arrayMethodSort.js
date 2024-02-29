e4530795e2318c43e493c19f3a3a7c496c4892ac
Maksym-Atanasov <157310647+Maksym-Atanasov@users.noreply.github.com>
2024-02-29 21:43:44 +0200

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,27 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let element = 0; element < this.length - i - 1; element++) {
+        if (compareFunction && typeof compareFunction === 'function') {
+          if (compareFunction(this[element], this[element + 1]) > 0) {
+            const temp = this[element];
+
+            this[element] = this[element + 1];
+            this[element + 1] = temp;
+          }
+        } else {
+          if (String(this[element]) > String(this[element + 1])) {
+            const temp = this[element];
+
+            this[element] = this[element + 1];
+            this[element + 1] = temp;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
