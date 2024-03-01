b62b2ffc9442abfe40aea7576b86ff3b31b8722a
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-03-01 14:47:51 +0200

    correct toSort function call

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,12 +4,10 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    const compare = compareFunction || toSort;
-
+  [].__proto__.sort2 = function(compareFunction = toSort) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        if (compare(this[i], this[j]) > 0) {
+        if (compareFunction(this[i], this[j]) > 0) {
           const tmp = this[i];
           this[i] = this[j];
           this[j] = tmp;
