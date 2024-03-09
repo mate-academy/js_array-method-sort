b90ff797c085fdbcf72b5d0d62d3d6dbdde46c64
OleksChernikov <pjatochkin2013@gmail.com>
2024-03-09 20:38:43 +0200

    commit1

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,28 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (!compareFunction) {
+          if (this[i].toString() > this[j].toString()) {
+            const temp = this[i];
+            this[i] = this[j];
+            this[j] = temp;
+          }
+        }
+
+        if (compareFunction) {
+          if (compareFunction(this[i].toString(), this[j].toString()) > 0) {
+            const temp = this[i];
+            this[i] = this[j];
+            this[j] = temp;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
