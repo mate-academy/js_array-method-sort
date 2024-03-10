d1b7314e9cd35160764f454d6f4a055e05d88efa
Oleksii Koliada <apbshnik75@gmail.com>
2024-03-10 12:56:06 +0200

    commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,20 +4,20 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  const defaultCompareSortFunction = (a, b) => String(a) > String(b) ? 1 : -1;
+  const defaultCompareSortFunction = (a, b) => (String(a) > String(b) ? 1 : -1);
 
-  [].__proto__.sort2 = function(compareFunction = defaultCompareSortFunction) {
+  [].__proto__.sort2 = function (compareFunction = defaultCompareSortFunction) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
           const temp = this[i];
+
           this[i] = this[j];
           this[j] = temp;
         }
       }
     }
-    
-    // console.log(compareFunction)
+
     return this;
   };
 }
