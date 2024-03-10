6af425e9207b3157a94cf4daf577c9400a5f62cf
OleksChernikov <pjatochkin2013@gmail.com>
2024-03-10 12:38:51 +0200

    commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,24 +3,16 @@
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
+  const defaultCompareSortFunction = (a, b) => String(a) > String(b) ? 1 : -1;
+  [].__proto__.sort2 = function (compareFunction = defaultCompareSortFunction) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        if (!compareFunction) {
-          if (this[i].toString() > this[j].toString()) {
-            const temp = this[i];
-            this[i] = this[j];
-            this[j] = temp;
-          }
-        }
-
-        if (compareFunction) {
-          if (compareFunction(this[i].toString(), this[j].toString()) > 0) {
-            const temp = this[i];
-            this[i] = this[j];
-            this[j] = temp;
-          }
+        if (compareFunction(this[i], this[j]) > 0) {
+          const temp = this[i];
+          this[i] = this[j];
+          this[j] = temp;
         }
       }
     }
