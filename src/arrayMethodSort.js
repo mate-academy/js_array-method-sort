0bf91a76bf00f7f92f05fedacf5570766ee864c3
Tetiana Lisne <tplovic@gmail.com>
2024-03-11 17:23:49 +0200

    buble sort implementation

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,18 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
+const defaultCompare = (a, b) => String(a) > (String(b));
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(compareFunction = defaultCompare) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[j], this[i]] = [this[i], this[j]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
