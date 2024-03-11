7895540deb0da579c21d443493aa0e05cc2b1794
hyriix <shyrii11bohdan@gmail.com>
2024-03-11 17:48:11 +0100

    liquidated

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,23 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (
+    callback = (a, b) => {
+      return String(a) > String(b) ? 1 : String(a) < String(b) ? -1 : 0;
+    },
+  ) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (callback(this[j], this[j + 1]) > 0) {
+          const tmp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = tmp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
