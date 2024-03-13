69e104a6d19291d09086116dea0c38ac67fc0fbf
Yevhenii Kushnir <aroblack17@gmail.com>
2024-03-13 19:51:07 +0100

    made modifications

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,17 +4,15 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
+  [].__proto__.sort2 = function (
+    compareFunction = (a, b) => {
+      return a.toString() > b.toString() ? 1 : 0;
+    },
+  ) {
     // write code here
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length - 1 - i; j++) {
-        let result;
-
-        if (compareFunction === undefined) {
-          result = this[j].toString() > this[j + 1].toString() ? 1 : 0;
-        } else {
-          result = compareFunction(this[j], this[j + 1]);
-        }
+        const result = compareFunction(this[j], this[j + 1]);
 
         if (result > 0) {
           const temp = this[j];
