f50c63cb26e35fd049ccd6cf1549e2c859a33cbd
Gena Korytov <korytov.gena22@gmail.com>
2024-03-20 00:10:41 +0200

    JS sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,18 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (
+    compareFunction = (a, b) => (String(a) > String(b) ? 1 : -1),
+  ) {
+    for (let j = 1; j < this.length; j++) {
+      for (let i = 0; i < this.length; i++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
-
 module.exports = applyCustomSort;
