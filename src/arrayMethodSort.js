23e01d81b7c660b91ad8b58abb56123e2c65792d
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-03-22 11:03:49 +0200

    improved the implementation

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,18 +6,22 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function (
     compareFunction = (first, second) => {
-      return String(first) >= String(second) ? 1 : -1;
+      if (String(first) > String(second)) {
+        return 1;
+      } else if (String(first) < String(second)) {
+        return -1;
+      } else {
+        return 0;
+      }
     },
   ) {
     for (let i = 0; i < this.length; i++) {
-      for (let j = i - 1; j >= 0; j--) {
+      for (let j = i - 1; j >= 0; j--, i--) {
         if (compareFunction(this[i], this[j]) < 0) {
-          const lesserValue = this[i];
-          const biggerValue = this[j];
+          const copiedArray = [...this];
 
-          this[i] = biggerValue;
-          this[j] = lesserValue;
-          i--;
+          this[i] = copiedArray[j];
+          this[j] = copiedArray[i];
         } else {
           break;
         }
