6034bd54bb18a78a74085b9a28e592bb74450dde
mfcastilho <fredericocastilho@hotmail.com>
2024-03-24 10:30:01 -0300

    feat: add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -8,15 +8,18 @@ function applyCustomSort() {
     if (String(a) < String(b)) {
       return -1;
     }
+
     if (String(a) > String(b)) {
       return 1;
     }
+
     return 0;
   }) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - 1 - i; j++) {
         if (compareFunction(this[j], this[j + 1]) > 0) {
           const temp = this[j];
+
           this[j] = this[j + 1];
           this[j + 1] = temp;
         }
