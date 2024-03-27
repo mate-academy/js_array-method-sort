6e483c39ef7e79d28995dfac3e19bd2ff549ba3d
Bohdan Liutyi <liutyibohdan13@gmail.com>
2024-03-27 11:07:37 +0200

    implement original sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,27 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    let callback = compareFunction;
+
+    if (!compareFunction) {
+      callback = (arg1, arg2) => {
+        return '' + arg1 > '' + arg2 ? 1 : -1;
+      };
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (callback(this[i], this[j]) > 0) {
+          const swap = this[i];
+
+          this[i] = this[j];
+          this[j] = swap;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
