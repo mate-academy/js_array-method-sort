fdf5ac4cc564baee52dce1c4fa03fef470e9b59d
mfcastilho <fredericocastilho@hotmail.com>
2024-03-28 16:00:10 -0300

    feat: add solution

--- src/arrayMethodSort.test.js
+++ src/arrayMethodSort.test.js
@@ -4,50 +4,58 @@ const applyCustomSort = require('./arrayMethodSort');
 
 applyCustomSort();
 
-describe('sort2', () => {
-  it('is a function', () => {
-    expect([].sort2)
-      .toBeInstanceOf(Function);
-  });
+test('sort2 is added to [].__proto__', () => {
+  expect([].sort2)
+    .toBeInstanceOf(Function);
+});
 
-  it(`doesn't call default sort`, () => {
-    expect([].sort2.toString().includes('.sort('))
-      .toBe(false);
-  });
+test(`sort2 doesn't call default sort`, () => {
+  expect([].sort2.toString().includes('.sort('))
+    .toBe(false);
+});
 
-  it('returns original array', () => {
-    const source = [3, 12, 2, 11];
-    const result = source.sort2();
+test('sort returns original array', () => {
+  const source = [3, 12, 2, 11];
+  const result = source.sort2();
 
-    expect(source)
-      .toBe(result);
-  });
+  expect(source)
+    .toBe(result);
+});
 
-  it('sorts lower case strings', () => {
-    expect(['c', 'ab', 'a', 'ad', 'b'].sort2())
-      .toEqual(['a', 'ab', 'ad', 'b', 'c']);
-  });
+test('the array is sorted', () => {
+  const source = [3, 12, 2, 11];
 
-  it('sorts mixed case strings', () => {
-    expect(['c', 'Ab', 'a', 'ad', 'B'].sort2())
-      .toEqual(['Ab', 'B', 'a', 'ad', 'c']);
-  });
+  source.sort2();
 
-  it('by default sorts number as strings', () => {
-    expect([3, 12, 2, 11].sort2())
-      .toEqual([11, 12, 2, 3]);
-  });
+  expect(source)
+    .toEqual([11, 12, 2, 3]);
+});
 
-  it('supports custom compare function for numbers', () => {
+test('numbers are sorted as strings by default: [3, 12, 2, 11].sort2()', () => {
+  expect([3, 12, 2, 11].sort2())
+    .toEqual([11, 12, 2, 3]);
+});
+
+test(`sort strings: ['c','b','a','d'].sort2()`, () => {
+  expect(['c', 'b', 'a', 'd'].sort2())
+    .toEqual(['a', 'b', 'c', 'd']);
+});
+
+test(`sort cyrillic letters: ['їжачок','яблуко','апельсин'].sort2()`, () => {
+  expect(['їжачок', 'яблуко', 'апельсин'].sort2())
+    .toEqual(['апельсин', 'яблуко', 'їжачок']);
+});
+
+test('custom compare function, numbers: [3, 12, 2, 11].sort2((a, b) => a - b)',
+  () => {
     expect([3, 12, 2, 11].sort2((a, b) => a - b))
       .toEqual([2, 3, 11, 12]);
   });
 
-  it('supports custom compare function for strings', () => {
-    const compare = (a, b) => a.localeCompare(b);
-
-    expect(['c', 'Ab', 'a', 'ad', 'B'].sort2(compare))
-      .toEqual(['a', 'Ab', 'ad', 'B', 'c']);
-  });
-});
-
+test(
+  `custom compare function, strings: 
+  ['їжачок','яблуко','апельсин'].sort2((a, b) => a.localeCompare(b))`, () => {
+    expect(['їжачок', 'яблуко', 'апельсин'].sort2((a, b) => a.localeCompare(b)))
+      .toEqual(['апельсин', 'їжачок', 'яблуко']);
+  },
+);
