93cfb15f478f29d6b603c79bad7abc3f1bd9c4ee
Vlad Buldenko <vladbuldenko@gmail.com>
2024-03-29 19:43:18 +0200

    VB solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,27 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction = compare) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
+}
+
+function compare(first, second) {
+  const a = String(first);
+  const b = String(second);
+
+  return a < b ? -1 : a > b ? 1 : 0;
 }
 
 module.exports = applyCustomSort;
