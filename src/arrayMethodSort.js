4f0dd8f99f24b5210fe926c129aad61e4103864d
eduvinagre <eduvinagre@gmail.com>
2024-03-30 10:13:52 -0300

    add quick sort solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -21,19 +21,35 @@ function applyCustomSort() {
       throw new TypeError('compareFunction must be a function');
     }
 
-    for (let i = 0; i < this.length; i++) {
-      for (let j = i + 1; j < this.length; j++) {
-        if (compareFunction(this[i], this[j]) > 0) {
-          const temp = this[i];
-
-          this[i] = this[j];
-          this[j] = temp;
-        }
-      }
-    }
+    quickSort(this, compareFunction, 0, this.length - 1);
 
     return this;
   };
+}
+
+function quickSort(array, compareFunction, left, right) {
+  if (left < right) {
+    const pivotIndex = partition(array, compareFunction, left, right);
+
+    quickSort(array, compareFunction, left, pivotIndex - 1);
+    quickSort(array, compareFunction, pivotIndex + 1, right);
+  }
+}
+
+function partition(array, compareFunction, left, right) {
+  const pivot = array[right];
+  let i = left - 1;
+
+  for (let j = left; j < right; j++) {
+    if (compareFunction(array[j], pivot) <= 0) {
+      i++;
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  [array[i + 1], array[right]] = [array[right], array[i + 1]];
+
+  return i + 1;
 }
 
 module.exports = applyCustomSort;
