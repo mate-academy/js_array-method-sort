13db83ccf4fbee0727605c3e44292099152f6eb6
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-04-03 13:17:55 +0300

    changed the code according to Sergiy advices

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -7,11 +7,12 @@ function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction = basicComparing) {
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length - 1 - i; j++) {
-        if (compareFunction(this[j], this[j + 1]) > 0) {
-          const temp = this[j];
+        const value = this[j];
+        const nextValue = this[j + 1];
 
-          this[j] = this[j + 1];
-          this[j + 1] = temp;
+        if (compareFunction(value, nextValue) > 0) {
+          this[j] = nextValue;
+          this[j + 1] = value;
         }
       }
     }
@@ -23,5 +24,16 @@ function applyCustomSort() {
 module.exports = applyCustomSort;
 
 function basicComparing(a, b) {
-  return String(a) >= String(b);
+  const strA = String(a);
+  const strB = String(b);
+
+  if (strA > strB) {
+    return 1;
+  }
+
+  if (strA < strB) {
+    return -1;
+  }
+
+  return 0;
 }
