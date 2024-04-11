10c2667a8e9d0aeb1e71fa9e728d648c6fcce99d
dmytrochernega <dimadneprfdb@gmail.coma>
2024-04-11 20:54:07 +0300

    changed the variable name

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const Compare = (a, b) => (a.toString() > b.toString() ? 1 : -1);
+const compare = (a, b) => (a.toString() > b.toString() ? 1 : -1);
 
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction = Compare) {
+  [].__proto__.sort2 = function (compareFunction = compare) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
