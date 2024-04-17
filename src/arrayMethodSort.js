36d82ce74fa22f87f5b96caa18cbb091352b66e1
Volodymyr Kryvytskyi <v.kryvytskyy@mate.academy>
2024-04-17 17:18:53 +0300

    solution 2

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,17 +5,28 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    for (let i = 0; i < this.length; i++) {
-      for (let j = 0; j < this.length - 1; j++) {
-        if (
-          compareFunction
-            ? compareFunction(this[j], this[j + 1]) > 0
-            : String(this[j]) > String(this[j + 1])
-        ) {
-          const cash = this[j];
+    if (typeof compareFunction === 'function') {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+          if (compareFunction(this[i], this[j]) > 0) {
+            const temp = this[j];
 
-          this[j] = this[j + 1];
-          this[j + 1] = cash;
+            this[j] = this[i];
+            this[i] = temp;
+          }
+        }
+      }
+
+      return this;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (this[i].toString() > this[j].toString()) {
+          const temp = this[j];
+
+          this[j] = this[i];
+          this[i] = temp;
         }
       }
     }
