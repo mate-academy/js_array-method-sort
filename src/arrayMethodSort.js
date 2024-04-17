995e7315ae3ad79af8073645063db1bbdcfc24e8
Volodymyr Kryvytskyi <v.kryvytskyy@mate.academy>
2024-04-17 17:25:09 +0300

    solution 3

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,35 +4,20 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
-    if (typeof compareFunction === 'function') {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = i + 1; j < this.length; j++) {
-          if (compareFunction(this[i], this[j]) > 0) {
-            const temp = this[j];
-
-            this[j] = this[i];
-            this[i] = temp;
-          }
-        }
-      }
-
-      return this;
-    }
-
+  [].__proto__.sort2 = function (callback = (a, b) => String(a) > String(b)) {
+    // write code here
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        if (this[i].toString() > this[j].toString()) {
-          const temp = this[j];
+        if (callback(this[i], this[j]) > 0) {
+          const firstValue = this[i];
 
-          this[j] = this[i];
-          this[i] = temp;
+          this[i] = this[j];
+          this[j] = firstValue;
         }
       }
     }
 
     return this;
-  };
 }
 
 module.exports = applyCustomSort;
