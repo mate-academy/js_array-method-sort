e107864a3b57e42b68c726147591a7a8363b3b6e
MarkGodnyi <libertyinprogresslife@gmail.com>
2024-04-17 19:50:58 +0200

    simplified sort function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,49 +5,27 @@
  */
 function applyCustomSort() {
   function compareDefault(a, b) {
-    const aString = String(a);
-    const bString = String(b);
-    const areInSameCase =
-      (isUpperCase(aString) && isUpperCase(bString)) ||
-      (!isUpperCase(aString) && !isUpperCase(bString));
-
-    if (areInSameCase) {
-      return aString.localeCompare(b);
-    }
-
-    if (isUpperCase(a)) {
-      return -1;
-    } else {
+    if (String(a) > String(b)) {
       return 1;
     }
-  }
 
-  function isUpperCase(item) {
-    return item.toLowerCase() !== item;
+    if (String(a) < String(b)) {
+      return -1;
+    }
+
+    return 0;
   }
 
   [].__proto__.sort2 = function (compareFunction = compareDefault) {
-    let itemToBePlacedAtTheEnd = this[0];
-    let currentEnd = this.length;
-    let currentSearchIndex = 0;
-    let itemIndex = 0;
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          const temp = this[i];
 
-    for (let repeatCount = 1; repeatCount < this.length; repeatCount++) {
-      while (currentSearchIndex < currentEnd) {
-        if (
-          compareFunction(this[currentSearchIndex], itemToBePlacedAtTheEnd) > 0
-        ) {
-          itemToBePlacedAtTheEnd = this[currentSearchIndex];
-          itemIndex = currentSearchIndex;
+          this[i] = this[j];
+          this[j] = temp;
         }
-        currentSearchIndex++;
       }
-      this[itemIndex] = this[currentEnd - 1];
-      this[currentEnd - 1] = itemToBePlacedAtTheEnd;
-      currentEnd--;
-      currentSearchIndex = 0;
-      itemToBePlacedAtTheEnd = this[0];
-      itemIndex = 0;
     }
 
     return this;
