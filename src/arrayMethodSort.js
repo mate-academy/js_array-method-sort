b540d771f99ec71715fb43e4cb2e3386bf8ecf7d
Igor <luiz23@ukr.net>
2024-04-19 10:06:15 +0300

    result

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,19 @@
 /**
  * Implement method Sort
  */
+const compare = (a, b) => (a.toString() > b.toString() ? 1 : -1);
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction = compare) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
