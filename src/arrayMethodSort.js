dfc82b2aa6beb83e75736106f72e4f0abfb1cb78
Yurii <urabelivr@gmail.com>
2024-04-19 15:52:11 +0200

    fixed standart value of compareFunction

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,22 +5,8 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (
-    compareFunction = (item1, item2) => {
-      if (
-        String(item1).length > String(item2).length &&
-        String(item1).startsWith(String(item2))
-      ) {
-        return 1;
-      }
-
-      for (let i = 0; i < String(item2).length; i++) {
-        if (String(item1).charCodeAt(i) - String(item2).charCodeAt(i)) {
-          return String(item1).charCodeAt(i) - String(item2).charCodeAt(i);
-        }
-      }
-
-      return 0;
-    },
+    compareFunction = (item1, item2) =>
+      String(item1) > String(item2) ? 1 : -1,
   ) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = i + 1; j < this.length; j++) {
