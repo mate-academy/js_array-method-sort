4b610b17fc1fb3070f54d6d628af5039b9ace6ac
KromchenkovOleksandr <khrom31@gmail.com>
2024-04-20 12:45:39 +0300

    Solution_sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,36 @@
 /**
  * Implement method Sort
  */
+function compareByDefolt(a, b) {
+  const srtingA = String(a);
+  const srtingB = String(b);
+
+  if (srtingA < srtingB) {
+    return -1;
+  }
+
+  if (srtingA > srtingB) {
+    return 1;
+  }
+
+  return 0;
+}
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction = compareByDefolt) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        const a = this[i];
+        const b = this[j];
+
+        if (compareFunction(a, b) > 0) {
+          this[i] = b;
+          this[j] = a;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
