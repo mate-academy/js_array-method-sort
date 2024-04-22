326d1a126f5bcac82bef2227b659c13405f18e7a
Volodymyr Kryvytskyi <v.kryvytskyy@mate.academy>
2024-04-22 21:50:07 +0300

    fixed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,15 +4,12 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (callback = (a, b) => String(a) > String(b)) {
+  [].__proto__.sort2 = function (callback = (a, b) => String(a).localeCompare(String(b))) {
     // write code here
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (callback(this[i], this[j]) > 0) {
-          const firstValue = this[i];
-
-          this[i] = this[j];
-          this[j] = firstValue;
+          [this[i], this[j]] = [this[j], this[i]]
         }
       }
     }
