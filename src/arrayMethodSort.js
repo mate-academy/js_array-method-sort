e5d707d8e9a9f472a9deb85ab06a5f52777b89cd
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-04-29 09:41:07 +0300

    add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,27 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      if (!compareFunction) {
+        for (let k = i; k < this.length; k++) {
+          if (String(this[k]) < String(this[i])) {
+            this.splice(i, 0, this[k]);
+            this.splice(k + 1, 1);
+          }
+        }
+      }
+
+      if (compareFunction) {
+        for (let j = i; j < this.length; j++) {
+          if (compareFunction(this[i], this[j]) === 1) {
+            this.splice(i, 0, this[j]);
+            this.splice(j + 1, 1);
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
