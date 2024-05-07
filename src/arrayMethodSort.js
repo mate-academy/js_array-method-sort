ce7b40f46700ecb3d3edf027a2930e8b38fe791e
mate-student <166617557+mate-student@users.noreply.github.com>
2024-05-07 14:00:28 +0300

    Update arrayMethodSort.js

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,25 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction = (a, b) => `${a}` > `${b}`) {
+    for (let i = 0; i < this.length - 1; i++) {
+      if (
+        compareFunction(this[i], this[i + 1]) === true ||
+        compareFunction(this[i], this[i + 1]) > 0
+      ) {
+        const temporary = this[i];
+
+        this[i] = this[i + 1];
+
+        this[i + 1] = temporary;
+
+        i = -1;
+      }
+    }
+
+    return this;
   };
 }
+applyCustomSort();
 
 module.exports = applyCustomSort;
