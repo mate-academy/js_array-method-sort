7838f493d1dd668fd6d71b20e72727167ad80002
NataliaTopornitska <natalia.topornitska3@gmail.com>
2024-05-18 18:35:47 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,19 +5,43 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    const defaultCompareFunction = (a, b) => (String(a) > String(b) ? 1 : -1);
+    const defaultCompareFunction = (a, b) => {
+      const strA = String(a);
+      const strB = String(b);
+
+      return strA < strB ? -1 : strA > strB ? 1 : 0;
+    };
+
     const actualCompareFunction =
       typeof compareFunction === 'function'
         ? compareFunction
         : defaultCompareFunction;
 
-    for (let i = 0; i < this.length; i++) {
-      for (let j = 0; j < this.length - i - 1; j++) {
-        if (actualCompareFunction(this[j], this[j + 1]) > 0) {
-          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+    const quicksort = (arr, left, right) => {
+      if (left < right) {
+        const pivotIndex = partition(arr, left, right);
+
+        quicksort(arr, left, pivotIndex - 1);
+        quicksort(arr, pivotIndex + 1, right);
+      }
+    };
+
+    const partition = (arr, left, right) => {
+      const pivot = arr[right];
+      let i = left - 1;
+
+      for (let j = left; j < right; j++) {
+        if (actualCompareFunction(arr[j], pivot) < 0) {
+          i++;
+          [arr[i], arr[j]] = [arr[j], arr[i]];
         }
       }
-    }
+      [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
+
+      return i + 1;
+    };
+
+    quicksort(this, 0, this.length - 1);
 
     return this;
   };
