4627e1a9de3e39946d209f4585eca348790b5e27
Mikhailo Koshilka <mkosilka@gmail.com>
2024-06-01 00:16:39 +0100

    Task Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (compareFunction === undefined) {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length; j++) {
+          if (this[j] + '' > this[j + 1] + '') {
+            const current = this[j];
+            this[j] = this[j + 1];
+            this[j + 1] = current;
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length; j++) {
+          if (compareFunction(this[j], this[j + 1]) > 0) {
+            const current = this[j];
+            this[j] = this[j + 1];
+            this[j + 1] = current;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
