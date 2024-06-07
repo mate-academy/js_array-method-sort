b25db36bd70ab23eac93b5219b8d621be841fca5
alina myronenko <alinamyronenko93@gmail.com>
2024-06-07 14:52:58 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -10,10 +10,7 @@ function applyCustomSort() {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
-          const result = this[i];
-
-          this[i] = this[j];
-          this[j] = result;
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
