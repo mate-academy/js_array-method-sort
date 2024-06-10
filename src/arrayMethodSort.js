6cf2a0b8f487b410c9f67d922a13e37733504366
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-10 14:46:54 +0300

    [Feature] created sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,38 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const initialCompareFunction = (a, b) => {
+      if (String(a) < String(b)) {
+        return -1;
+      }
+
+      return 1;
+    };
+
+    const compare = compareFunction ?? initialCompareFunction;
+
+    let isSwapped = false;
+
+    do {
+      isSwapped = false;
+
+      for (let idx = 0; idx < this.length; idx++) {
+        const result = compare(this[idx], this[idx + 1]);
+
+        if (result > 0) {
+          const temp = this[idx];
+
+          this[idx] = this[idx + 1];
+
+          this[idx + 1] = temp;
+
+          isSwapped = true;
+        }
+      }
+    } while (isSwapped);
+
+    return this;
   };
 }
 
