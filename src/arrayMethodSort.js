ab2512ed5c44b3577fde962331de75657fd9b7d7
Mikhailo Koshilka <mkosilka@gmail.com>
2024-06-13 21:37:23 +0100

    add fix

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,25 +4,17 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    if (compareFunction === undefined) {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = 0; j < this.length; j++) {
-          if (this[j] + '' > this[j + 1] + '') {
-            const current = this[j];
-            this[j] = this[j + 1];
-            this[j + 1] = current;
-          }
-        }
-      }
-    } else {
-      for (let i = 0; i < this.length; i++) {
-        for (let j = 0; j < this.length; j++) {
-          if (compareFunction(this[j], this[j + 1]) > 0) {
-            const current = this[j];
-            this[j] = this[j + 1];
-            this[j + 1] = current;
-          }
+  [].__proto__.sort2 = function (compareFn) {
+    const compareFunction = compareFn || function(a, b) {
+      return (a + '' > b + '') ? 1 : -1;
+    };
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - 1; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
         }
       }
     }
