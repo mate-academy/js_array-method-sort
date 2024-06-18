c5449d8a80c75e2e9f9a25b599422948fc5bfd13
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-06-18 16:49:58 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,22 +5,37 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    let isSwapped;
+    if (this.length < 2) {
+      return this;
+    }
 
-    do {
-      isSwapped = false;
+    const pivot = this[this.length - 1];
+    const left = [];
+    const right = [];
 
-      for (let i = 0; i < this.length; i++) {
-        const condition = compareFunction
-          ? compareFunction(this[i], this[i + 1]) > 0
-          : String(this[i]) > String(this[i + 1]);
-
-        if (condition) {
-          [this[i], this[i + 1]] = [this[i + 1], this[i]];
-          isSwapped = true;
-        }
+    for (let i = 0; i < this.length - 1; i++) {
+      if (
+        typeof compareFunction === 'function'
+          ? compareFunction(this[i], pivot) > 0
+          : String(this[i]) > String(pivot)
+      ) {
+        right.push(this[i]);
+      } else {
+        left.push(this[i]);
       }
-    } while (isSwapped);
+    }
+
+    const sorted = [
+      ...left.sort2(compareFunction),
+      pivot,
+      ...right.sort2(compareFunction),
+    ];
+
+    this.length = 0;
+
+    for (let i = 0; i < sorted.length; i++) {
+      this.push(sorted[i]);
+    }
 
     return this;
   };
