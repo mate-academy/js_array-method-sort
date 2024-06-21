a13cf8ecc8a78a76c40f972503849014994bfcc8
Kate Hubko <gubkoekaterina9592@gmail.com>
2024-06-21 09:56:44 +0300

    Sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,21 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (
+    compareFunction = (item1, item2) => item1.toString() > item2.toString(),
+  ) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          const tmp = this[i];
+
+          this[i] = this[j];
+          this[j] = tmp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
