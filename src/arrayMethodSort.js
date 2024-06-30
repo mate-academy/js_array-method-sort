0f39eebf57577a99d8d1e6a3b844ad36474f8d30
Oleksii Symonets <alex.simonets@gmail.com>
2024-06-30 15:33:39 +0300

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,31 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (
+    compareFunction = (a, b) => {
+      if (String(a) < String(b)) {
+        return -1;
+      } else if (String(a) > String(b)) {
+        return 1;
+      }
+
+      return 0;
+    },
+  ) {
+    for (let i = 0; i < this.length - 1; ) {
+      const a = this[i];
+      const b = this[i + 1];
+
+      if (compareFunction(a, b) > 0) {
+        this[i] = b;
+        this[i + 1] = a;
+        i = 0;
+      } else {
+        i++;
+      }
+    }
+
+    return this;
   };
 }
 
