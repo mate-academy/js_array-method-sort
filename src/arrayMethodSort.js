ccfa2c44eba39816997c77f7c02ce8d5215eb7b4
Olexander Bielov <alehandro.bel96@gmail.com>
2024-07-01 19:43:02 +0300

    Add callback variable for better readabillity

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,13 +5,15 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (
-    compareFunction = (a, b) =>
-      String(a) > String(b)
+    compareFunction = (a, b) => String(a) > String(b),
   ) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        if (compareFunction(this[i], this[j]) > 0) {
+        const callback = compareFunction(this[i], this[j]);
+
+        if (callback > 0) {
           const temp = this[i];
+
           this[i] = this[j];
           this[j] = temp;
         }
@@ -20,6 +22,6 @@ function applyCustomSort() {
 
     return this;
   };
-};
+}
 
 module.exports = { applyCustomSort };
