e7cfeadbbd28215a636749e09b4b0336ae8bcd61
Korolko Anastasiia <korolkonasta@gmail.com>
2024-07-04 16:35:14 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,26 @@
 /**
  * Implement method Sort
  */
+const defaultCompare = (a, b) => a.toString() > b.toString();
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction = defaultCompare) {
+    for (let i = 0, endI = this.length - 1; i < endI; i++) {
+      let wasSwap = false;
+
+      for (let j = i + 1, endJ = endI; j < endJ; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+          wasSwap = true;
+        }
+      }
+
+      if (!wasSwap) {
+        break;
+      }
+    }
+
+    return this;
   };
 }
 
