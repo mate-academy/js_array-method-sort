4635b34884bbc64e077b551da999a5dd23335322
SerhiiSemennikov <semennik@hotmail.com>
2024-07-05 11:14:15 +0200

    cahange compare to compareFunction

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,14 +6,14 @@
 function applyCustomSort() {
   const cF = (a, b) => String(a) > String(b);
 
-  [].__proto__.sort2 = function (compare = cF) {
+  [].__proto__.sort2 = function (compareFunction = cF) {
     for (let currentIndex = 0; currentIndex < this.length; currentIndex++) {
       for (
         let nextIndex = currentIndex + 1;
         nextIndex < this.length;
         nextIndex++
       ) {
-        if (compare(this[currentIndex], this[nextIndex]) > 0) {
+        if (compareFunction(this[currentIndex], this[nextIndex]) > 0) {
           const value = this[currentIndex];
 
           this[currentIndex] = this[nextIndex];
