41f489eca2cf65107415bbe06e927ed6cdaddf48
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-07-11 20:24:03 +0200

    Solution2

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,34 +4,32 @@
  * Implement method Sort
  */
 
-function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction = undefined) {
-    for (let i = 0; i < this.length - 1; i++) {
-      if (String(this[i]) > String(this[i + 1])) {
-        const currentIndex = this[i];
-
-        this[i] = this[i + 1];
-        this[i + 1] = currentIndex;
-
-        i = -1;
+const applyCustomSort = () => {
+  [].__proto__.sort2 = function (compareFunction) {
+    const defaultCompareFunction = (a, b) => {
+      if (String(a) < String(b)) {
+        return -1;
       }
-    }
 
-    if (compareFunction !== undefined) {
-      for (let i = 0; i < this.length - 1; i++) {
-        if (compareFunction(this[i], this[i + 1]) > 0) {
-          const currentIndex = this[i];
+      if (String(a) > String(b)) {
+        return 1;
+      }
 
-          this[i] = this[i + 1];
-          this[i + 1] = currentIndex;
+      return 0;
+    };
 
-          i = -1;
+    const compare = compareFunction || defaultCompareFunction;
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compare(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
 
     return this;
   };
-}
+};
 
 module.exports = applyCustomSort;
