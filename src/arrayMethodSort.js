6e12fc78b6adbc19bef61ffc16612c616d4a1f2d
Iryna Yehorova <yehorova.dev@gmail.com>
2024-07-11 19:19:53 +0300

    Edit solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,11 +6,18 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction = defaultCase) {
     for (let i = 0; i < this.length; i++) {
+      let min = this[i];
+      let minInd = i;
+
       for (let y = i + 1; y < this.length; y++) {
-        if (compareFunction(this[i], this[y]) > 0) {
-          [this[i], this[y]] = [this[y], this[i]];
+        const current = this[y];
+
+        if (compareFunction(min, current) > 0) {
+          min = current;
+          minInd = y;
         }
       }
+      [this[i], this[minInd]] = [this[minInd], this[i]];
     }
 
     return this;
