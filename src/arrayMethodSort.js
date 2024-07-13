046bf6337a8be586dea277587baf83d9cc2a351f
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-07-13 19:07:23 +0300

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,39 @@
 /**
  * Implement method Sort
  */
+
+const defaultCompare = function (a, b) {
+  const aNum = String(a);
+  const bNum = String(b);
+
+  if (aNum < bNum) {
+    return -1;
+  }
+
+  if (aNum > bNum) {
+    return 1;
+  }
+
+  if (aNum === bNum) {
+    return 0;
+  }
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction = defaultCompare) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          const item = this[i];
+
+          this[i] = this[j];
+
+          this[j] = item;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
