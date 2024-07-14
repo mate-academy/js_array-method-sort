20529cf9a4af53171214f23c85310a8f13c94c66
Diana Darienko <diana.darienko21@gmail.com>
2024-07-14 14:04:14 +0300

    fixed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,14 +5,16 @@
  */
 
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction = (a, b) => `${a}` > `${b}`) {
+  [].__proto__.sort2 = function (compareFunction) {
+    const compare =
+      typeof compareFunction === 'function'
+        ? compareFunction
+        : (a, b) => (`${a}` > `${b}` ? 1 : -1);
+
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        if (compareFunction(this[i], this[j]) > 0) {
-          const value = this[i];
-
-          this[i] = this[j];
-          this[j] = value;
+        if (compare(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
