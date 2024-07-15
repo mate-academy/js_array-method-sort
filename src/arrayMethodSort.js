6c95b81dccb8a0529e18078f537d2bca27401dd9
Valentyn Radobenko <valikradobenko@gmail.com>
2024-07-15 21:54:58 +0300

    add the case when a and b are equal, simplify the swap logic

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -10,16 +10,17 @@ function applyCustomSort() {
         return -1;
       }
 
+      if (a + '' === b + '') {
+        return 0;
+      }
+
       return 1;
     },
   ) {
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length; j++) {
         if (i !== j && compareFunction(this[i], this[j]) < 0) {
-          const temp = this[j];
-
-          this[j] = this[i];
-          this[i] = temp;
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
