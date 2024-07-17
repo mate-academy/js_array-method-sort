afd5ed992b35ecf270e4f7ffbd97be767e9b7ea9
Olia Shymanska <oliaa.shymanska@gmail.com>
2024-07-17 12:40:57 +0300

    implement method sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,30 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (
+    compareFunction = (elem1, elem2) => {
+      if (String(elem1) < String(elem2)) {
+        return -1;
+      }
+
+      if (String(elem1) === String(elem2)) {
+        return 0;
+      }
+
+      if (String(elem1) > String(elem2)) {
+        return 1;
+      }
+    },
+  ) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
