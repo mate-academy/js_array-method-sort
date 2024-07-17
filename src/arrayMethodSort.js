9b1f493b163a7a2ce8ac706805632928df02201d
Olia Shymanska <oliaa.shymanska@gmail.com>
2024-07-17 13:10:09 +0300

    fixed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,24 +4,25 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (
-    compareFunction = (elem1, elem2) => {
-      if (String(elem1) < String(elem2)) {
+  [].__proto__.sort2 = function (compareFunction) {
+    function compareFnDefault(a, b) {
+      if (String(a) < String(b)) {
         return -1;
       }
 
-      if (String(elem1) === String(elem2)) {
-        return 0;
-      }
-
-      if (String(elem1) > String(elem2)) {
+      if (String(a) > String(b)) {
         return 1;
       }
-    },
-  ) {
+      // a must be equal to b
+
+      return 0;
+    }
+
+    const compareFn = compareFunction || compareFnDefault;
+
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        if (compareFunction(this[i], this[j]) > 0) {
+        if (compareFn(this[i], this[j]) > 0) {
           [this[i], this[j]] = [this[j], this[i]];
         }
       }
