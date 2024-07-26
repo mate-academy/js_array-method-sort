ca6cb57368f009b0c4a7852de0a01f68665a476d
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-07-26 12:40:54 +0200

    some changes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,23 +4,18 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
-    for (let i = 0; i < this.length; i++) {
-      for (let j = i; j < this.length; j++) {
-        if (compareFunction) {
-          if (compareFunction(this[i], this[j]) > 0) {
-            const temp = this[i];
+  [].__proto__.sort2 = function (
+    compareFunction = (val1, val2) => {
+      return String(val1) > String(val2) ? 1 : -1;
+    },
+  ) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          const temp = this[i];
 
-            this[i] = this[j];
-            this[j] = temp;
-          }
-        } else {
-          if (String(this[j]) < String(this[i])) {
-            const temp = this[i];
-
-            this[i] = this[j];
-            this[j] = temp;
-          }
+          this[i] = this[j];
+          this[j] = temp;
         }
       }
     }
