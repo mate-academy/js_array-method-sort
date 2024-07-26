74bb972aa961e0ae73281f81c8b7ed7dad18abec
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-07-26 16:49:16 +0300

    js_array-method-sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,36 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const compFunc =
+      compareFunction ||
+      function (a, b) {
+        const aStr = String(a);
+        const bStr = String(b);
+
+        if (aStr < bStr) {
+          return -1;
+        }
+
+        if (aStr > bStr) {
+          return 1;
+        }
+
+        return 0;
+      };
+
+    for (let i = 1; i < this.length; i++) {
+      const current = this[i];
+      let j = i - 1;
+
+      while (j >= 0 && compFunc(this[j], current) > 0) {
+        this[j + 1] = this[j];
+        j--;
+      }
+      this[j + 1] = current;
+    }
+
+    return this;
   };
 }
 
