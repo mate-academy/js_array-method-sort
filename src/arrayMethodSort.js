be2a0ce8800dd08eafe87b7a472badb596f33332
Wojciech Goliasz <wojtekgoliasz98@gmail.com>
2024-08-03 13:03:02 +0200

    adding a check if compareFunction is a function and changing the do loop to a for loop

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,6 +5,10 @@
  */
 function applyCustomSort() {
   function compareAsString(a, b) {
+    if (a == null || b == null) {
+      return -1;
+    }
+
     let x = a;
     let y = b;
 
@@ -23,24 +27,30 @@ function applyCustomSort() {
   }
 
   [].__proto__.sort2 = function (compareFunction = compareAsString) {
-    let count;
+    if (typeof compareFunction === 'function') {
+      let count = 0;
 
-    do {
-      count = 0;
+      for (let j = 0; j < this.length - 1; j++) {
+        count = 0;
 
-      for (let i = 0; i < this.length - 1; i++) {
-        const a = this[i];
-        const b = this[i + 1];
+        for (let i = 0; i < this.length - 1 - j; i++) {
+          const a = this[i];
+          const b = this[i + 1];
 
-        if (compareFunction(a, b) > 0) {
-          count++;
-          this[i] = b;
-          this[i + 1] = a;
+          if (compareFunction(a, b) > 0) {
+            count++;
+            this[i] = b;
+            this[i + 1] = a;
+          }
+        }
+
+        if (count === 0) {
+          break;
         }
       }
-    } while (count > 0);
 
-    return this;
+      return this;
+    }
   };
 }
 
