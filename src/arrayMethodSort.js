c31fbf8aa18a64d32e57cd50ce102047fa34d328
Igor <igor.olshanskyi@gmail.com>
2024-08-04 13:08:02 +0300

    solve

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,30 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function (compareFunction) {
     // write code here
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i; j < this.length; j++) {
+        if (compareFunction) {
+          if (compareFunction(this[i], this[j]) > 0) {
+            const x = this[i];
+
+            this[i] = this[j];
+            this[j] = x;
+          }
+        } else {
+          if (String(this[j]) < String(this[i])) {
+            const x = this[i];
+
+            this[i] = this[j];
+            this[j] = x;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
