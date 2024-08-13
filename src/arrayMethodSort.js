a0eccdd1b28066b0261e6bfaad833b7d5c343282
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-08-13 13:46:24 +0300

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,29 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    for (let i = 0; i < this.length; i++) {
+      for (let k = 0; k < this.length; k++) {
+        const comparedEl = this[i];
+        const currentEl = this[k];
+
+        if (compareFunction === undefined) {
+          if (this[i].toString() < this[k].toString()) {
+            this[i] = currentEl;
+            this[k] = comparedEl;
+          }
+
+          continue;
+        }
+
+        if (compareFunction(this[i], this[k]) < 0) {
+          this[i] = currentEl;
+          this[k] = comparedEl;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
