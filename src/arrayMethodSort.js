eeffe8d491ecd051ceba75f50eea070d231d96b1
Vladyslav Humeniak <vladyslavhumenyak@gmail.com>
2024-08-14 12:38:02 +0300

    task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,27 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const ARRAY = [...this];
+    let cmpFunc = compareFunction;
+
+    if (!compareFunction) {
+      cmpFunc = (a, b) => String(a) > String(b);
+    }
+
+    for (let i = 0; i < ARRAY.length - 1; i++) {
+      for (let j = 0; j < ARRAY.length - 1 - i; j++) {
+        if (cmpFunc(ARRAY[j], ARRAY[j + 1]) > 0) {
+          [ARRAY[j], ARRAY[j + 1]] = [ARRAY[j + 1], ARRAY[j]];
+        }
+      }
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      this[i] = ARRAY[i];
+    }
+
+    return this;
   };
 }
 
