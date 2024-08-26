6a4c2d6bed88b9e1aaa2550af5dfb84628cdf68f
Yevhen Stepanenko <yevhen.s@mate.academy>
2024-08-26 15:03:13 +0300

    first commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,42 @@
 /**
  * Implement method Sort
  */
+
+const compareAsFunction = (a, b) => {
+  const functionA = String(a);
+  const functionB = String(b);
+
+  if (functionA > functionB) {
+    return 1;
+  }
+
+  if (functionA < functionB) {
+    return -1;
+  }
+
+  return 0;
+};
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction = compareAsFunction) {
+    let count;
+
+    do {
+      count = 0;
+
+      for (let i = 1; i < this.length; i++) {
+        const previous = this[i - 1];
+        const curent = this[i];
+
+        if (compareFunction(previous, curent) > 0) {
+          count++;
+          this[i - 1] = curent;
+          this[i] = previous;
+        }
+      }
+    } while (count > 0);
+
+    return this;
   };
 }
 
