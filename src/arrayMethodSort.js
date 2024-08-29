5a42f33f50a45b5c8b02a4aa96c78a4cc496093d
Andrey Pikalov <andrtan10mail.com@gmail.com>
2024-08-29 19:29:25 +0300

    solved task

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,32 @@
+/* eslint-disable no-param-reassign */
 'use strict';
 
 /**
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    if (typeof compareFunction !== 'function') {
+      compareFunction = function (a, b) {
+        const strA = String(a);
+        const strB = String(b);
+
+        return strA < strB ? -1 : strA > strB ? 1 : 0;
+      };
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
