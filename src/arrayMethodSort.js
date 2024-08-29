a3152af3837d97984005b41704458b0df391a8cc
Viktoriia Dereza <viktoriiadereza7@gmail.com>
2024-08-29 15:47:20 +0300

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,19 +4,13 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
-    const useCompareFunction = typeof compareFunction === 'function';
-
+  [].__proto__.sort2 = function (
+    compareFunction = (a, b) => (String(a) > String(b) ? 1 : -1),
+  ) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        if (useCompareFunction) {
-          if (compareFunction(this[i], this[j]) > 0) {
-            [this[i], this[j]] = [this[j], this[i]];
-          }
-        } else {
-          if (String(this[i]) > String(this[j])) {
-            [this[i], this[j]] = [this[j], this[i]];
-          }
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
