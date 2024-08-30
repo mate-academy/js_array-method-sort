5773b3b1ac09d79c2d3f168ac90cb2e6c7e974b7
Korshun <dmitriy.kor91@gmail.com>
2024-08-30 20:05:08 +0300

    use array destructuring

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -19,14 +19,11 @@ const compareAsStrings = (a, b) => {
 };
 
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareAs = compareAsStrings) {
+  [].__proto__.sort2 = function (compareFn = compareAsStrings) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
-        if (compareAs(this[j], this[j + 1]) > 0) {
-          const temp = this[j];
-
-          this[j] = this[j + 1];
-          this[j + 1] = temp;
+        if (compareFn(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
         }
       }
     }
