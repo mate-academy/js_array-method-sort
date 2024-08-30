04a308f7f4754fe7ddad2e9b02bd8ef4c5a255b6
Andrey Pikalov <andrtan10mail.com@gmail.com>
2024-08-30 22:51:18 +0300

    improve the code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,23 +1,32 @@
-/* eslint-disable no-param-reassign */
 'use strict';
 
 /**
  * Implement method Sort
  */
+
+function defaultCompareFunction(a, b) {
+  const strA = String(a);
+  const strB = String(b);
+
+  if (strA < strB) {
+    return -1;
+  } else if (strA > strB) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    if (typeof compareFunction !== 'function') {
-      compareFunction = function (a, b) {
-        const strA = String(a);
-        const strB = String(b);
-
-        return strA < strB ? -1 : strA > strB ? 1 : 0;
-      };
-    }
+    const betterCompareFunction =
+      typeof compareFunction === 'function'
+        ? compareFunction
+        : defaultCompareFunction;
 
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
-        if (compareFunction(this[j], this[j + 1]) > 0) {
+        if (betterCompareFunction(this[j], this[j + 1]) > 0) {
           const temp = this[j];
 
           this[j] = this[j + 1];
