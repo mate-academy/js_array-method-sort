43c792f55853e17281daaddeda9f11bb67d54713
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-01 16:36:53 +0300

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,43 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    let compareFunctionFinished;
+
+    if (!compareFunction) {
+      compareFunctionFinished = (a, b) => {
+        const stringA = a.toString();
+        const stringB = b.toString();
+
+        if (stringA.match(/\p{Lu}/u) && !stringB.match(/\p{Lu}/u)) {
+          return -1;
+        } else if (stringB.match(/\p{Lu}/u) && !stringA.match(/\p{Lu}/u)) {
+          return 1;
+        } else {
+          return stringA.localeCompare(stringB, undefined, {
+            sensitivity: 'base',
+          });
+        }
+      };
+    } else {
+      compareFunctionFinished = compareFunction;
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      let minIndex = i;
+
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunctionFinished(this[j], this[minIndex]) < 0) {
+          minIndex = j;
+        }
+      }
+
+      if (minIndex !== i) {
+        [this[i], this[minIndex]] = [this[minIndex], this[i]];
+      }
+    }
+
+    return this;
   };
 }
 
