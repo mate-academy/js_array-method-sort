ddc4ff1daacbc3a1745caf3345ea0b1e127e5bdd
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-01 22:50:48 +0300

    default function

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,26 +4,20 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
-    let compareFunctionFinished;
+  [].__proto__.sort2 = function (
+    compareFunction = (a, b) => {
+      const stringA = a.toString();
+      const stringB = b.toString();
 
-    if (!compareFunction) {
-      compareFunctionFinished = (a, b) => {
-        const stringA = a.toString();
-        const stringB = b.toString();
-
-        if (stringA.match(/\p{Lu}/u) && !stringB.match(/\p{Lu}/u)) {
-          return -1;
-        } else if (stringB.match(/\p{Lu}/u) && !stringA.match(/\p{Lu}/u)) {
-          return 1;
-        } else {
-          return stringA.localeCompare(stringB);
-        }
-      };
-    } else {
-      compareFunctionFinished = compareFunction;
-    }
-
+      if (stringA.match(/\p{Lu}/u) && !stringB.match(/\p{Lu}/u)) {
+        return -1;
+      } else if (stringB.match(/\p{Lu}/u) && !stringA.match(/\p{Lu}/u)) {
+        return 1;
+      } else {
+        return stringA.localeCompare(stringB);
+      }
+    },
+  ) {
     const length = this.length;
     let swapped;
 
@@ -31,7 +25,7 @@ function applyCustomSort() {
       swapped = false;
 
       for (let i = 0; i < length - 1; i++) {
-        if (compareFunctionFinished(this[i], this[i + 1]) > 0) {
+        if (compareFunction(this[i], this[i + 1]) > 0) {
           [this[i], this[i + 1]] = [this[i + 1], this[i]];
           swapped = true;
         }
