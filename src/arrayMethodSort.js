f2bdf4835c2b92afbc9f764714c58ff0a791bf20
Alina Luchytska <aluchitska@gmail.com>
2024-09-02 10:56:56 +0300

    update code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,25 +4,16 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
+  [].__proto__.sort2 = function (
+    compareFunction = (a, b) => String(a) > String(b),
+  ) {
     // write code here
-    const copyArr = [...this];
-    let defaultFn = compareFunction;
-
-    if (!compareFunction) {
-      defaultFn = (a, b) => String(a) > String(b);
-    }
-
-    for (let i = 0; i < copyArr.length - 1; i++) {
-      for (let j = 0; j < copyArr.length - 1 - i; j++) {
-        if (defaultFn(copyArr[j], copyArr[j + 1]) > 0) {
-          [copyArr[j], copyArr[j + 1]] = [copyArr[j + 1], copyArr[j]];
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
         }
       }
-    }
-
-    for (let i = 0; i < this.length; i++) {
-      this[i] = copyArr[i];
     }
 
     return this;
