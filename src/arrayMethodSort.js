f9d87422bc11fb9937961ca09cde6dbc49e27228
alena <bonda5229@gmail.com>
2024-09-05 21:59:33 +0300

    fix Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,19 +5,46 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    for (let i = 0; i < this.length - 1; i++) {
-      for (let j = 0; j < this.length - 1 - i; j++) {
-        if (compareFunction) {
-          if (compareFunction(this[j], this[j + 1]) > 0) {
-            [this[j], this[j + 1]] = [this[j + 1], this[j]];
-          }
-        } else {
-          if (String(this[j]) > String(this[j + 1])) {
-            [this[j], this[j + 1]] = [this[j + 1], this[j]];
-          }
+    const defaultCompare = (a, b) => {
+      if (String(a) < String(b)) {
+        return -1;
+      }
+
+      if (String(a) > String(b)) {
+        return 1;
+      }
+
+      return 0;
+    };
+
+    const comparator = compareFunction || defaultCompare;
+
+    const partition = (arr, low, high) => {
+      const pivot = arr[high];
+      let i = low - 1;
+
+      for (let j = low; j < high; j++) {
+        if (comparator(arr[j], pivot) < 0) {
+          i++;
+          [arr[i], arr[j]] = [arr[j], arr[i]];
         }
       }
-    }
+
+      [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+
+      return i + 1;
+    };
+
+    const quicksort = (arr, low, high) => {
+      if (low < high) {
+        const pi = partition(arr, low, high);
+
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+      }
+    };
+
+    quicksort(this, 0, this.length - 1);
 
     return this;
   };
