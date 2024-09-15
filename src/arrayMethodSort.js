2137f851bb0d2731e12b0da3435b219be9a99a09
Pasha Shchukin <pasha4320692@gmail.com>
2024-09-15 21:59:59 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,34 +5,34 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
-    // eslint-disable-next-line no-param-reassign
-    compareFunction =
-      compareFunction ||
-      function (a, b) {
-        a = String(a);
-        b = String(b);
+  [].__proto__.sort2 = function (
+    compareFunction = (a, b) => {
+      a = String(a);
+      b = String(b);
 
-        if (a < b) {
-          return -1;
-        }
+      if (a < b) {
+        return -1;
+      }
 
-        if (a > b) {
-          return 1;
-        }
+      if (a > b) {
+        return 1;
+      }
 
-        return 0;
-      };
-
-    for (let i = 0; i < this.length - 1; i++) {
-      for (let j = 0; j < this.length - 1 - i; j++) {
-        if (compareFunction(this[j], this[j + 1]) > 0) {
-          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+      return 0;
+    },
+  ) {
+    // eslint-disable-next-line no-lone-blocks
+    {
+      for (let i = 0; i < this.length - 1; i++) {
+        for (let j = 0; j < this.length - 1 - i; j++) {
+          if (compareFunction(this[j], this[j + 1]) > 0) {
+            [this[j], this[j + 1]] = [this[j + 1], this[j]];
+          }
         }
       }
-    }
 
-    return this;
+      return this;
+    }
   };
 }
 
