0315451951c22f5f063551fc7f32d9363c954c2b
tsiupaknazar <sjprime280@gmail.com>
2024-09-19 19:08:14 +0100

    made fixes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,13 +5,32 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (
-    compareFunction = (a, b) => String(a) > String(b),
+    compareFunction = (a, b) => {
+      if (String(a) < String(b)) {
+        return -1;
+      }
+
+      if (String(a) > String(b)) {
+        return 1;
+      }
+
+      return 0;
+    },
   ) {
+    let swapped;
+
     for (let i = 0; i < this.length; i++) {
+      swapped = false;
+
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
           [this[i], this[j]] = [this[j], this[i]];
+          swapped = true;
         }
+      }
+
+      if (!swapped) {
+        break;
       }
     }
 
