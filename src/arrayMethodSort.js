da387bc334952c8abff34d2ab6f68d4c94b3c441
Serhii Mamchur <kovaniysvit@gmail.com>
2024-10-04 23:37:05 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -10,10 +10,7 @@ function applyCustomSort() {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         if (compareFunction(this[i], this[j]) > 0) {
-          const word = this[i];
-
-          this[i] = this[j];
-          this[j] = word;
+          [this[i], this[j]] = [this[j], this[i]];
         }
       }
     }
@@ -21,5 +18,4 @@ function applyCustomSort() {
     return this;
   };
 }
-
 module.exports = applyCustomSort;
