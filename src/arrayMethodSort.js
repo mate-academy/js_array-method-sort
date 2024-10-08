3c4de5530210ee3ce33e123c4261154848d1a9dd
Maxym Modrytskyi <maxmodrr@gmail.com>
2024-10-08 21:35:40 +0300

    result

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,19 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const defaultCompare = (a, b) => (String(a) < String(b) ? -1 : 1);
+    const compare = compareFunction || defaultCompare;
+
+    for (let i = 1; i < this.length; i++) {
+      for (let j = 0; j < i; j++) {
+        if (compare(this[i], this[j]) < 0) {
+          [this[i], this[j]] = [this[j], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
