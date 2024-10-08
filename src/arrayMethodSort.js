266134924a971315430ce4ef54d9994706a8876a
Mykola Zavalnitskyi <zavalnitskiyk@gmaail.com>
2024-10-08 21:12:37 +0300

    Add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,27 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    let callBack = compareFunction;
+
+    if (!callBack) {
+      callBack = function (a, b) {
+        return String(a) > String(b) ? 1 : -1;
+      };
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let a = 0; a < this.length - 1 - i; a++) {
+        if (callBack(this[a], this[a + 1]) > 0) {
+          const temp = this[a];
+
+          this[a] = this[a + 1];
+          this[a + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
