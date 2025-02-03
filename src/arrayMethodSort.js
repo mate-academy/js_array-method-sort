d555a0d7b3c0b1a842421fdd06a73393008aa281
Danylo Kachaniuk <logistik2004@ukr.net>
2025-02-03 14:03:51 +0200

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,20 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (
+          compareFunction
+            ? compareFunction(this[j], this[j + 1]) > 0
+            : String(this[j]) > String(this[j + 1])
+        ) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
