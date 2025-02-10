35e6891000455f3b46fc571bc851900dceeaadd7
inikiforov <ievgen.nikiforov@tedata.de>
2025-02-10 17:16:30 +0100

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,34 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    // Default compare function: compares elements as strings in ascending order
+    const compare =
+      compareFunction ||
+      ((a, b) => {
+        if (String(a) < String(b)) {
+          return -1;
+        }
+
+        if (String(a) > String(b)) {
+          return 1;
+        }
+
+        return 0;
+      });
+
+    for (let i = 1; i < this.length; i++) {
+      const current = this[i];
+      let j = i - 1;
+
+      while (j >= 0 && compare(this[j], current) > 0) {
+        this[j + 1] = this[j];
+        j--;
+      }
+      this[j + 1] = current;
+    }
+
+    return this;
   };
 }
 
