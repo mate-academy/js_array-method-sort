dfa7ccc4dbab25d46281a914833a9266b577d28f
Yaroslav <yarmoshyk96@gmail.com>
2025-02-15 17:59:40 +0200

    Add array method sort solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,26 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const comparator =
+      typeof compareFunction === 'function'
+        ? compareFunction
+        : getDefaultComparator();
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - 1; j++) {
+        if (comparator(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+        }
+      }
+    }
+
+    return this;
   };
+}
+
+function getDefaultComparator() {
+  return (a, b) => (String(a) > String(b) ? 1 : String(a) < String(b) ? -1 : 0);
 }
 
 module.exports = applyCustomSort;
