8f9d27ba91354798406ff5dc32ad1e64de2109c7
Anton Polishchuk <hardixxxxx@gmail.com>
2025-02-22 22:32:46 +0100

    complete task

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,31 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const compareFn =
+      compareFunction ||
+      ((a, b) => {
+        return a.toString() > b.toString();
+      });
+
+    for (let i = 0; i < this.length; i++) {
+      for (let k = i + 1; k < this.length; k++) {
+        const firstValue = this[i];
+        const secondValue = this[k];
+        const result = compareFn(firstValue, secondValue);
+
+        if (result === 0) {
+          continue;
+        }
+
+        if (result > 0) {
+          this[i] = secondValue;
+          this[k] = firstValue;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
