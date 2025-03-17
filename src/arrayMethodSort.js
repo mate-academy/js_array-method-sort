ad8a6323bc20a2998f78947de3fbe7eedc51d53b
BoGoBooM <myPikcha@gmail.com>
2025-03-17 18:11:43 +0200

    bubble created

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,27 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - 1; j++) {
+        const a = this[j];
+        const b = this[j + 1];
+        const compareResult = compareFunction
+          ? compareFunction(a, b)
+          : a.toString() > b.toString()
+            ? 1
+            : a.toString() < b.toString()
+              ? -1
+              : 0;
+
+        if (compareResult > 0) {
+          this[j] = b;
+          this[j + 1] = a;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
