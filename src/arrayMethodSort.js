136bd9895a5a5943f57b527eeda1d43af8e7c629
Oksana Sidanych <sasha3sashkova@gmail.com>
2025-03-19 22:45:52 +0200

    add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,19 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1; j++) {
+        if (this[j] > this[j + 1]) {
+          const swap = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = swap;
+        }
+      }
+    }
+
+    return this;
   };
 }
-
 module.exports = applyCustomSort;
