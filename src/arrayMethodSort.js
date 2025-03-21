56b30e000e1a64f65486a6738feb4a60a421c5f5
Oksana Sidanych <sasha3sashkova@gmail.com>
2025-03-21 19:22:33 +0200

    add new solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,18 +5,30 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
+    let swapped;
+
+    const compare = compareFunction || ((a, b) => a - b);
+
     for (let i = 0; i < this.length - 1; i++) {
-      for (let j = 0; j < this.length - 1; j++) {
-        if (this[j] > this[j + 1]) {
+      swapped = false;
+
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compare(this[j], this[j + 1]) > 0) {
           const swap = this[j];
 
           this[j] = this[j + 1];
           this[j + 1] = swap;
+          swapped = true;
         }
+      }
+
+      if (!swapped) {
+        break;
       }
     }
 
     return this;
   };
 }
+
 module.exports = applyCustomSort;
