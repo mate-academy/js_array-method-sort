ced2daf81e5d2eb23dca6e669dae12f5c0848979
Jeny Berezhna <JenySmails24@gmail.com>
2025-04-01 20:46:51 +0100

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,47 +3,31 @@
 /**
  * Implement method Sort
  */
-function applyCustomSort(array, compareFunction) {
-  if (!Array.isArray(array)) {
-    return [];
-  }
+/**
+ * Custom sorting function for arrays.
+ *
+ * @param {Array} array - The array to sort.
+ * @param {Function} [compareFunction]
+ * @returns {Array}
+ */
+function applyCustomSort() {
+  [].__proto__.sort2 = function (
+    compareFunction = (a, b) => a.toString() > b.toString(),
+  ) {
+    chooseMethod(this, compareFunction);
 
-  if (typeof compareFunction !== 'function' && compareFunction !== undefined) {
-    throw new TypeError('The comparison must be a function or undefined.');
-  }
+    return this;
 
-  const arr = [...array];
-  const length = arr.length;
-
-  for (let i = 0; i < length - 1; i++) {
-    let swapped = false;
-
-    for (let j = 0; j < length - 1 - i; j++) {
-      const a = arr[j];
-      const b = arr[j + 1];
-
-      let shouldSwap;
-
-      if (compareFunction) {
-        shouldSwap = compareFunction(a, b) > 0;
-      } else if (typeof a === 'number' && typeof b === 'number') {
-        shouldSwap = a > b;
-      } else {
-        shouldSwap = String(a).toLowerCase() > String(b).toLowerCase();
-      }
-
-      if (shouldSwap) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        swapped = true;
+    function chooseMethod(array, method) {
+      for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+          if (method(array[i], array[j]) > 0) {
+            [array[i], array[j]] = [array[j], array[i]];
+          }
+        }
       }
     }
-
-    if (!swapped) {
-      break;
-    }
-  }
-
-  return arr;
+  };
 }
 
 module.exports = applyCustomSort;
