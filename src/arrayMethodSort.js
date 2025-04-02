e1476c1ff7dc4d85d25f6c43540e94941f15427c
Maksum Kovshun <129434499+mkovshun@users.noreply.github.com>
2025-04-02 15:56:43 +0000

    Implement QuickSort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,37 +1,40 @@
 'use strict';
 
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
+  [].__proto__.sort2 = function (compareFunction = ((a, b) => (String(a) > String(b) ? 1 : -1))) {
     if (this.length < 2) {
       return this;
     }
 
-    const compare = compareFunction || ((a, b) => String(a).localeCompare(String(b)));
+    const quickSort = (arr, left = 0, right = arr.length - 1) => {
+      if (left >= right) {
+        return;
+      }
 
-    const partition = (arr, low, high) => {
-      const pivot = arr[high];
-      let i = low - 1;
+      const pivotIndex = partition(arr, left, right);
 
-      for (let j = low; j < high; j++) {
-        if (compare(arr[j], pivot) < 0) {
+      quickSort(arr, left, pivotIndex - 1);
+      quickSort(arr, pivotIndex + 1, right);
+    };
+
+    const partition = (arr, left, right) => {
+      const pivot = arr[right];
+      let i = left - 1;
+
+      for (let j = left; j < right; j++) {
+        if (compareFunction(arr[j], pivot) < 0) {
           i++;
           [arr[i], arr[j]] = [arr[j], arr[i]];
         }
       }
 
-      [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+      [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
+
       return i + 1;
     };
 
-    const quickSort = (arr, low, high) => {
-      if (low < high) {
-        const pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-      }
-    };
+    quickSort(this);
 
-    quickSort(this, 0, this.length - 1);
     return this;
   };
 }
