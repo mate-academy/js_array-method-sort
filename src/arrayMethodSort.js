ce044c779de42a4e2481508a62b7453bf6635a26
YuliaSabitova <yuliasab2017@mail.com>
2025-04-06 21:13:38 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,44 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  const regex = /^[A-Z]/;
+
+  [].__proto__.sort2 = function sort2(compareFunction) {
+    let comparisonFunction = compareFunction;
+
+    if (typeof comparisonFunction !== 'function') {
+      comparisonFunction = (a, b) => {
+        const valueA = String(a);
+        const valueB = String(b);
+
+        const isUpperA = regex.test(valueA);
+        const isUpperB = regex.test(valueB);
+
+        if (isUpperA && !isUpperB) {
+          return -1;
+        }
+
+        if (!isUpperA && isUpperB) {
+          return 1;
+        }
+
+        const result = valueA.localeCompare(valueB, 'en-US', {
+          sensitivity: 'variant',
+        });
+
+        return result;
+      };
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (comparisonFunction(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
