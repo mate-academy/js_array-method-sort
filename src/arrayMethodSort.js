642dbad5c3e7e81dc8ba33b61c7e79885479ceb4
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-04-13 08:19:17 -0300

    arrayMethodSort.js

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,64 +3,56 @@
 /**
  * Implement method Sort
  */
-'use strict';
-
 function applyCustomSort() {
-	[].__proto__.sort2 = function (compareFunction) {
-		if (typeof compareFunction !== 'function') {
-			compareFunction = function (a, b) {
-				const aStr = String(a);
-				const bStr = String(b);
+  [].__proto__.sort2 = function(compareFunction) {
+    // Usar uma variável separada para a função de comparação efetiva
+    const effectiveCompare = typeof compareFunction === 'function'
+      ? compareFunction
+      : (a, b) => {
+          const aStr = String(a);
+          const bStr = String(b);
+          if (aStr < bStr) return -1;
+          if (aStr > bStr) return 1;
+          return 0;
+        };
 
-				if (aStr < bStr) return -1;
+    // Resto da implementação permanece igual
+    const elementsInfo = [];
+    for (let i = 0; i < this.length; i++) {
+      const hasOwn = Object.hasOwn(this, i);
+      elementsInfo.push({
+        value: hasOwn ? this[i] : undefined,
+        isHole: !hasOwn
+      });
+    }
 
-				if (aStr > bStr) return 1;
+    // Algoritmo de ordenação
+    for (let i = 0; i < elementsInfo.length; i++) {
+      for (let j = 0; j < elementsInfo.length - i - 1; j++) {
+        const a = elementsInfo[j].value;
+        const b = elementsInfo[j + 1].value;
+        if (effectiveCompare(a, b) > 0) {
+          const temp = elementsInfo[j];
+          elementsInfo[j] = elementsInfo[j + 1];
+          elementsInfo[j + 1] = temp;
+        }
+      }
+    }
 
-				return 0;
-			};
-		}
+    // Reconstrução do array
+    for (let i = 0; i < this.length; i++) {
+      delete this[i];
+    }
 
-		const elementsInfo = [];
-		for (let i = 0; i < this.length; i++) {
-			const hasOwn = Object.hasOwn(this, i);
+    for (let i = 0; i < elementsInfo.length; i++) {
+      const info = elementsInfo[i];
+      if (!info.isHole) {
+        this[i] = info.value;
+      }
+    }
 
-			elementsInfo.push({
-				value: hasOwn ? this[i] : undefined,
-				isHole: !hasOwn,
-			});
-		}
-
-		// Bubble sort implementation based on compareFunction
-		for (let i = 0; i < elementsInfo.length; i++) {
-			for (let j = 0; j < elementsInfo.length - i - 1; j++) {
-				const a = elementsInfo[j].value;
-
-				const b = elementsInfo[j + 1].value;
-
-				if (compareFunction(a, b) > 0) {
-					// Swap elements
-					const temp = elementsInfo[j];
-					elementsInfo[j] = elementsInfo[j + 1];
-					elementsInfo[j + 1] = temp;
-				}
-			}
-		}
-
-		// Delete all existing elements
-		for (let i = 0; i < this.length; i++) {
-			delete this[i];
-		}
-
-		// Insert sorted elements, preserving holes
-		for (let i = 0; i < elementsInfo.length; i++) {
-			const info = elementsInfo[i];
-			if (!info.isHole) {
-				this[i] = info.value;
-			}
-		}
-
-		return this;
-	};
+    return this;
+  };
 }
 
 module.exports = applyCustomSort;
