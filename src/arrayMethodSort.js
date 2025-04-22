ee5e3c551a9e06eaf8fe4a305218be44af57d240
Yevheniia Biliaieva <biliaieva99@gmail.com>
2025-04-22 12:52:22 +0300

    task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,26 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const arr = this;
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - 1; j++) {
+        const a = arr[j];
+        const b = arr[j + 1];
+
+        const shouldSwap = compareFunction
+          ? compareFunction(a, b) > 0
+          : String(a) > String(b);
+
+        if (shouldSwap) {
+          arr[j] = b;
+          arr[j + 1] = a;
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
