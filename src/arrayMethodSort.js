417745375aba36ed0b4030e0830558687fd4109b
Nazarii Korol <nazarkorol3684@gmail.com>
2025-05-21 20:01:21 +0300

    Add solution for task

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,38 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (callback) {
+    const call =
+      callback ||
+      ((a, b) => {
+        const stringA = String(a);
+        const stringB = String(b);
+
+        if (stringA > stringB) {
+          return 1;
+        }
+
+        if (stringA < stringB) {
+          return -1;
+        }
+
+        if (stringA === stringB) {
+          return 0;
+        }
+      });
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1; j++) {
+        if (call(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
