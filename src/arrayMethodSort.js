2b6441ea04cdad81394bd39324a82cd0a20347a0
Yura <yurii.st11@gmail.com>
2025-05-30 22:01:16 +0300

    add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,30 +4,33 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function (compareFunction) {
     const arr = this;
-    const compare = compareFunction || ((a, b) => {
-      const aStr = String(a);
-      const bStr = String(b);
+    const compare =
+      compareFunction ||
+      ((a, b) => {
+        const aStr = String(a);
+        const bStr = String(b);
 
-      if (aStr > bStr) {
-        return 1;
-      } else if (aStr < bStr) {
-        return -1;
-      }
-      return 0;
-    })
+        if (aStr > bStr) {
+          return 1;
+        } else if (aStr < bStr) {
+          return -1;
+        }
+
+        return 0;
+      });
 
     for (let i = 0; i < arr.length - 1; i++) {
       for (let j = 0; j < arr.length - i - 1; j++) {
-        if (compare(arr[i], arr[j + 1]) > 0) {
-          [arr[i], arr[j + 1]] = [arr[j + 1], arr[i]];
+        if (compare(arr[j], arr[j + 1]) > 0) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         }
       }
     }
 
     return arr;
-  }
+  };
 }
 
 module.exports = applyCustomSort;
