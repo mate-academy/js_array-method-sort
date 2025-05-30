09468288da232d49ff907506ef0b23b2824e5d57
Alexandre Almeida <alexandredd@duck.com>
2025-05-30 11:34:23 -0300

    task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,26 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (
+          compareFunction
+            ? compareFunction(this[i], this[j]) > 0
+            : String(this[i]) > String(this[j])
+        ) {
+          const box = this[j];
+
+          this[j] = this[i];
+          this[i] = box;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
 module.exports = applyCustomSort;
+
+applyCustomSort();
