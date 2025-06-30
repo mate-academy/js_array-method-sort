2321bd7041ddcc6a89d7f938d29257c963205a4d
mattc108 <matthewcamacho108@gmail.com>
2025-06-30 08:42:02 -0500

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,37 @@
+/* eslint-disable no-console */
 'use strict';
 
 /**
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function (compareFunction) {
     // write code here
+    for (let i = 0; i < this.length - 1; i++) {
+      let min = this[i];
+      let mIndex = i;
+
+      for (let j = i + 1; j < this.length; j++) {
+        const b = this[j];
+
+        if (compareFunction) {
+          if (compareFunction(min, b) > 0) {
+            min = b;
+            mIndex = j;
+          }
+        } else {
+          if (String(min) > String(b)) {
+            min = b;
+            mIndex = j;
+          }
+        }
+      }
+
+      console.log(this, this[i], min);
+      [this[i], this[mIndex]] = [min, this[i]];
+    }
+
+    return this;
   };
 }
 
