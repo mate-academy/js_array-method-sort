40ef6239dcc47bb803de326492dd55a5f4fe0d35
Denys Herych <fojjof2@gmail.com>
2025-07-01 22:20:01 +0200

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,30 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    // debugger;
+    let compareFunctionToUse;
+
+    if (typeof compareFunction !== 'function') {
+      compareFunctionToUse = function (a, b) {
+        return String(a).localeCompare(String(b));
+      };
+    } else {
+      compareFunctionToUse = compareFunction;
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compareFunctionToUse(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
