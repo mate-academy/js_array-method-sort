7eaea196c32f3a72fc2069a88620d1f9c47f15eb
CwaR228 <sosok2gt@gmail.com>
2025-07-03 12:41:04 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,28 @@
 /**
  * Implement method Sort
  */
+/* eslint-disable */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  Array.prototype.sort2 = function (compareFunction) {
+    const cmp =
+      compareFunction ||
+      ((a, b) => {
+        const aStr = String(a);
+        const bStr = String(b);
+        if (aStr < bStr) return -1;
+        if (aStr > bStr) return 1;
+        return 0;
+      });
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (cmp(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
