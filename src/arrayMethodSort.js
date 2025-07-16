8ef2406f6ec711efb005570a30174e267f2d1018
tvk777 <klimenko.tv7@gmail.com>
2025-07-16 13:35:10 +0300

    add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,27 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    // write code here
+    const compare =
+      compareFunction ||
+      ((a, b) => {
+        const aStr = String(a);
+        const bStr = String(b);
+
+        return aStr > bStr ? 1 : aStr < bStr ? -1 : 0;
+      });
+
+    for (let i = 0; i < this.length; i++) {
+      for (let k = i; k < this.length; k++) {
+        if (compare(this[i], this[k]) > 0) {
+          const tempValue = this[k];
+
+          this[k] = this[i];
+          this[i] = tempValue;
+        }
+      }
+    }
+
+    return this;
   };
 }
-
 module.exports = applyCustomSort;
