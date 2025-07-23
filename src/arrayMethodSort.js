aa7ad6030083d3821a57ee87249fd8f52b45eba5
danie010830 <danielgukillwar008@gmail.com>
2025-07-23 15:52:25 -0300

    correto

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,10 +3,32 @@
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    return Array.prototype.sort.call(this, compareFunction);
-    // write code here
+    const array = this;
+
+    const compare =
+      compareFunction ||
+      function (a, b) {
+        const strA = String(a);
+        const strB = String(b);
+
+        return strA > strB ? 1 : strA < strB ? -1 : 0;
+      };
+
+    for (let i = 0; i < array.length - 1; i++) {
+      for (let j = 0; j < array.length - 1 - i; j++) {
+        if (compare(array[j], array[j + 1]) > 0) {
+          const temp = array[j];
+
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+    }
+
+    return array;
   };
 }
 
