835048790b1788ea1e32273839d1b34a2c2943bd
Vasyliev Vadym <protocol222333@gmail.com>
2025-08-08 09:31:36 +0300

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,35 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    let compare;
+
+    if (compareFunction === undefined) {
+      compare = (a, b) => {
+        if (a + '' > b + '') {
+          return 1;
+        } else if (a + '' < b + '') {
+          return -1;
+        }
+
+        return 0;
+      };
+    } else {
+      compare = compareFunction;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compare(this[i], this[j]) > 0) {
+          const comparedValue = this[i];
+
+          this[i] = this[j];
+          this[j] = comparedValue;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
