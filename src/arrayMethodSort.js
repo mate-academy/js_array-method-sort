ee961f6ecd441406d1f5b6dfddc76d3b263dfe55
kirky359 <lol783176@gmail.com>
2025-08-08 18:38:24 +0300

    fix

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -25,7 +25,7 @@ function applyCustomSort() {
       };
 
     for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr.length - 1; j++) {
+      for (let j = 0; j < arr.length - 1 - i; j++) {
         if (compare(arr[j], arr[j + 1]) > 0) {
           const temp = arr[j];
 
