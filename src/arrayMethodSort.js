ac73fd442f7b939b429ba7723c8852ac4a165f7c
Vasyliev Vadym <protocol222333@gmail.com>
2025-08-08 10:18:42 +0300

    added recognition undefined

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,31 +5,48 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    let compare;
-
-    if (compareFunction === undefined) {
-      compare = (a, b) => {
-        if (a + '' > b + '') {
+    const compare =
+      compareFunction ||
+      ((a, b) => {
+        if (String(a) > String(b)) {
           return 1;
-        } else if (a + '' < b + '') {
+        } else if (String(a) < String(b)) {
           return -1;
         }
 
         return 0;
-      };
-    } else {
-      compare = compareFunction;
+      });
+
+    const definedItems = [];
+    let undefinedCount = 0;
+
+    for (const item of this) {
+      if (item === undefined) {
+        undefinedCount++;
+      } else {
+        definedItems.push(item);
+      }
     }
 
-    for (let i = 0; i < this.length; i++) {
-      for (let j = i + 1; j < this.length; j++) {
-        if (compare(this[i], this[j]) > 0) {
-          const comparedValue = this[i];
-
-          this[i] = this[j];
-          this[j] = comparedValue;
+    for (let i = 0; i < definedItems.length; i++) {
+      for (let j = i + 1; j < definedItems.length; j++) {
+        if (compare(definedItems[i], definedItems[j]) > 0) {
+          [definedItems[i], definedItems[j]] = [
+            definedItems[j],
+            definedItems[i],
+          ];
         }
       }
+    }
+
+    for (let i = 0; i < definedItems.length; i++) {
+      this[i] = definedItems[i];
+    }
+
+    const totalLength = definedItems.length + undefinedCount;
+
+    for (let i = definedItems.length; i < totalLength; i++) {
+      this[i] = undefined;
     }
 
     return this;
