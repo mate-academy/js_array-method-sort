2532db59ae95981dbb937f4148fcdd66972e4bed
TetiankaSh <shh.tetiana@gmail.com>
2025-08-11 17:04:45 +0300

    solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
   extends: '@mate-academy/eslint-config',
   env: {
-    jest: true
+    jest: true,
   },
   rules: {
-    'no-proto': 0
-  },
-  plugins: ['jest']
+  indent: ['error', 2],
+  'prettier/prettier': 'off',
+  'no-mixed-spaces-and-tabs': 'error',
+  'no-proto': 0,
+},
+
+
+  plugins: ['jest'],
 };
