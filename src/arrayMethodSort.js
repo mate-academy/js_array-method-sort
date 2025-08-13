9d2a1d2fe93772d10ba3c9b268af053f02173502
Yuriy Gumenskyi <gumenskiy.yuriy@gmail.com>
2025-08-13 19:41:25 +0300

    Solution_arrayMethodSort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,26 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    for (let i = 0; i < this.length; i++) {
+      for (let ii = 1; ii < this.length; ii++) {
+        const theSmallest = this[ii - 1];
+        let swapOrNot;
+
+        if (compareFunction === undefined) {
+          swapOrNot = Number(String(this[ii - 1]) > String(this[ii]));
+        } else {
+          swapOrNot = compareFunction(this[ii - 1], this[ii]);
+        }
+
+        if (swapOrNot > 0) {
+          this[ii - 1] = this[ii];
+          this[ii] = theSmallest;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
