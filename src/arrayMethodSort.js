2ff3291e87e13c0c34e630c23d06f8789cb4ed2c
Artur Misko <arthurmisko@gmail.com>
2025-08-21 19:28:26 +0200

    done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,30 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const arr = this;
+
+    if (typeof compareFunction !== 'function') {
+      // eslint-disable-next-line no-param-reassign
+      compareFunction = (a, b) => {
+        // eslint-disable-next-line no-param-reassign
+        a = String(a);
+        // eslint-disable-next-line no-param-reassign
+        b = String(b);
+
+        return a > b ? 1 : a < b ? -1 : 0;
+      };
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - i - 1; j++) {
+        if (compareFunction(arr[j], arr[j + 1]) > 0) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
