df8d1d25f4502b2a145c85bd5b727439d2469ce2
Yehor Mishchenko <yehormishchenkowork@gmail.com>
2025-08-23 14:50:36 +0200

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,30 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (
+    compareFunction = (a, b) => (a.toString() >= b.toString() ? 1 : -1),
+  ) {
+    const cmp =
+      compareFunction || ((a, b) => String(a).localeCompare(String(b)));
+
+    if (typeof cmp !== 'function') {
+      throw new Error('cmp повинен бути функцією');
+    }
+
+    for (let j = 0; j < this.length; j++) {
+      for (let i = 0; i < this.length - 1; i++) {
+        const cmpRes = cmp(this[i], this[i + 1]);
+
+        if (cmpRes > 0) {
+          const temp = this[i];
+
+          this[i] = this[i + 1];
+          this[i + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
-
 module.exports = applyCustomSort;
