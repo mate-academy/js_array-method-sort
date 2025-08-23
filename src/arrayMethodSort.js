c23e468a030dfb8514a9784bdc79aae3b6f8764c
Yehor Mishchenko <yehormishchenkowork@gmail.com>
2025-08-23 17:05:43 +0200

    fixed solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,18 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (
-    compareFunction = (a, b) => (a.toString() >= b.toString() ? 1 : -1),
+    compareFunction = (a, b) => {
+      const sa = String(a);
+      const sb = String(b);
+
+      return sa > sb ? 1 : sa < sb ? -1 : 0;
+    },
   ) {
     const cmp =
-      compareFunction || ((a, b) => String(a).localeCompare(String(b)));
+      compareFunction ||
+      ((a, b) => {
+        const sa = String(a);
+        const sb = String(b);
 
-    if (typeof cmp !== 'function') {
-      throw new Error('cmp повинен бути функцією');
-    }
+        return sa > sb ? 1 : sa < sb ? -1 : 0;
+      });
 
     for (let j = 0; j < this.length; j++) {
       for (let i = 0; i < this.length - 1; i++) {
-        const cmpRes = cmp(this[i], this[i + 1]);
+        let cmpRes = cmp(this[i], this[i + 1]);
+
+        if (Number.isNaN(cmpRes)) {
+          cmpRes = 0;
+        }
 
         if (cmpRes > 0) {
           const temp = this[i];
