fc27962c4dd2155ba53da851083ab4ca9917333c
unknown <plaaasio03@gmail.com>
2025-09-08 22:22:41 +0200

    Fix: custom sort2 method according to review

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,44 +3,57 @@
 /**
  * Implement method Sort
  */
+/* eslint-disable no-extend-native */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
-    const compare =
-      compareFunction ||
-      ((a, b) => {
-        const strA = String(a);
-        const strB = String(b);
-
-        if (strA < strB) {
-          return -1;
-        }
-
-        if (strA > strB) {
-          return 1;
-        }
-
+  Array.prototype.sort2 = function (compareFunction) {
+    // domyślny comparator
+    function defaultComparator(a, b) {
+      if (a === undefined && b === undefined) {
         return 0;
-      });
-
-    for (let i = 0; i < this.length; i++) {
-      let swapped = false;
-
-      for (let j = 0; j < this.length - 1 - i; j++) {
-        if (compare(this[j], this[j + 1]) > 0) {
-          const temp = this[j];
-
-          this[j] = this[j + 1];
-          this[j + 1] = temp;
-          swapped = true;
-        }
       }
 
-      if (!swapped) {
-        break;
+      if (a === undefined) {
+        return 1;
+      }
+
+      if (b === undefined) {
+        return -1;
+      }
+
+      const A = String(a);
+      const B = String(b);
+
+      if (A > B) {
+        return 1;
+      }
+
+      if (A < B) {
+        return -1;
+      }
+
+      return 0;
+    }
+
+    const compare =
+      typeof compareFunction === 'function'
+        ? compareFunction
+        : defaultComparator;
+
+    const arr = this;
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (compare(arr[j], arr[j + 1]) > 0) {
+          const temp = arr[j];
+
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
       }
     }
 
-    return this;
+    return arr;
   };
 }
 
