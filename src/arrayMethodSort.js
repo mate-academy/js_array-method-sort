73524470394ed0fc5629dad2b1a48bf0a9fd2c11
Roman <hpp1ovh@gmail.com>
2025-09-12 20:05:03 +0300

    complite task

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,9 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function (compareFunction) {
     // write code here
+    return [].__proto__.sort.call(this, compareFunction);
   };
 }
 
