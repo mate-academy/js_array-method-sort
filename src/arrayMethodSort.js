0515b5e4a4ff41520c5f4a094b0c34819c2eb1be
MarcelLeal <marcel@pgentulhos.com.br>
2025-10-09 17:00:51 -0300

    development of method sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,31 @@
 /**
  * Implement method Sort
  */
+
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    if (typeof compareFunction !== 'function') {
+      compareFunction = function(a, b) {
+
+        return String(a) > String(b) ? 1 : (String(a) < String(b) ? -1 : 0);
+      };
+    }
+
+    const arr = this;
+    const length = arr.length;
+
+    for (let i = 0; i < length - 1; i++) {
+      for (let j = 0; j < length - 1 - i; j++) {
+        if (compareFunction(arr[j], arr[j + 1]) > 0) {
+         
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
