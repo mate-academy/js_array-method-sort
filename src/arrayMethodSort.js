8704639289f805a0b05bad567cda1466b14f4e4f
MarcelLeal <marcel@pgentulhos.com.br>
2025-10-09 17:13:59 -0300

    Assignment to function parameter compareFunction

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,6 +4,15 @@
  * Implement method Sort
  */
 
+const nums = [5, 2, 9, 1];
+
+
+
+const words = ['banana', 'apple', 'cherry'];
+
+
+
+
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     if (typeof compareFunction !== 'function') {
@@ -19,7 +28,7 @@ function applyCustomSort() {
     for (let i = 0; i < length - 1; i++) {
       for (let j = 0; j < length - 1 - i; j++) {
         if (compareFunction(arr[j], arr[j + 1]) > 0) {
-         
+
           const temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
@@ -30,5 +39,13 @@ function applyCustomSort() {
     return arr;
   };
 }
+
+// nums.sort2((a, b) => a - b); // [1, 2, 5, 9]
+// words.sort2(); // default lexicographic sort
+console.log(nums);
+console.log(words);
+
+
+
 
 module.exports = applyCustomSort;
