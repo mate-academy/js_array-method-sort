d3680d07975d075e7b755a54bc60172a69a238cc
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-12 16:29:44 +0200

    Poprawka applyCustomSort – sort2 działa bez nadpisywania Array.prototype.sort
    Dodano metodę sort2 działającą jak natywny Array.sort z obsługą compareFunction i undefined, bez nadpisywania Array.prototype.sort, z optymalizacją bubble sort.

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,42 +4,38 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  // Dodajemy metodę sort2 do prototypu Array
+  // Dodajemy własną metodę sort2 do prototypu tablic
   [].__proto__.sort2 = function(compareFunction) {
-    const compare = compareFunction
-      ? compareFunction
-      : (a, b) => {
-          if (a === undefined) return 1;
-          if (b === undefined) return -1;
-          const A = String(a);
-          const B = String(b);
-          if (A < B) return -1;
-          if (A > B) return 1;
-          return 0;
-        };
+    const compare = compareFunction || ((a, b) => {
+      if (a === undefined) return 1;
+      if (b === undefined) return -1;
+      const A = String(a);
+      const B = String(b);
+      if (A < B) return -1;
+      if (A > B) return 1;
+      return 0;
+    });
 
-    for (let i = 0; i < this.length - 1; i++) {
+    const arr = this;
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
       let swapped = false;
-      for (let j = 0; j < this.length - 1 - i; j++) {
-        if (compare(this[j], this[j + 1]) > 0) {
-          const temp = this[j];
-          this[j] = this[j + 1];
-          this[j + 1] = temp;
+      for (let j = 0; j < n - 1 - i; j++) {
+        if (compare(arr[j], arr[j + 1]) > 0) {
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
           swapped = true;
         }
       }
-      if (!swapped) break; // optymalizacja: jeśli brak zamiany, kończymy
+      if (!swapped) break; // optymalizacja dla prawie posortowanych tablic
     }
-    return this;
-  };
-
-  // Delegacja standardowej metody sort do sort2
-  Array.prototype.sort = function(compareFunction) {
-    return this.sort2(compareFunction);
+    return arr;
   };
 }
 
 module.exports = applyCustomSort;
+
 
 
 
