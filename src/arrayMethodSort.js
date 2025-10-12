6278019661b263bc96ceeb7d1b849e2af0b4900e
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-12 15:48:57 +0200

    Implementacja metody sort2 w funkcji applyCustomSort
    Dodano własną implementację metody sort2 działającą jak Array.sort, z obsługą compareFunction i sortowaniem bąbelkowym.

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,12 +1,31 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const compare =
+      typeof compareFunction === 'function'
+        ? compareFunction
+        : (a, b) => {
+            const A = String(a);
+            const B = String(b);
+            if (A < B) return -1;
+            if (A > B) return 1;
+            return 0;
+          };
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compare(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
 module.exports = applyCustomSort;
+
