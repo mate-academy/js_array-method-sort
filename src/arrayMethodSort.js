0db24c80295973ea129a1fcbb597eb40132f3790
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-12 15:57:55 +0200

    Poprawa wcięć i dodanie delegacji Array.prototype.sort
    Dostosowano formatowanie do Prettier i dodano powiązanie Array.prototype.sort z własną metodą sort2.

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,6 +6,9 @@ function applyCustomSort() {
       typeof compareFunction === 'function'
         ? compareFunction
         : (a, b) => {
+            if (a === undefined) return 1;
+            if (b === undefined) return -1;
+
             const A = String(a);
             const B = String(b);
             if (A < B) return -1;
@@ -13,17 +16,25 @@ function applyCustomSort() {
             return 0;
           };
 
+    let swapped;
     for (let i = 0; i < this.length - 1; i++) {
+      swapped = false;
       for (let j = 0; j < this.length - 1 - i; j++) {
         if (compare(this[j], this[j + 1]) > 0) {
           const temp = this[j];
           this[j] = this[j + 1];
           this[j + 1] = temp;
+          swapped = true;
         }
       }
+      if (!swapped) break;
     }
 
     return this;
+  };
+
+  Array.prototype.sort = function(compareFn) {
+    return this.sort2(compareFn);
   };
 }
 
