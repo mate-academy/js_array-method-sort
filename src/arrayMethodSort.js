a4201e29e5d230031264e87718d9229b92c04505
Elvira Petrenko <elvirapetrenko@gmail.com>
2025-10-12 18:33:25 +0200

    Fix sort2: delegate Array.prototype.sort and validate comparator

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -8,21 +8,41 @@ function applyCustomSort() {
     // write code here
     const arr = this;
 
+    if (
+      compareFunction !== undefined &&
+      typeof compareFunction !== 'function'
+    ) {
+      throw new TypeError(`${compareFunction} is not a function`);
+    }
+
     for (let i = 0; i < arr.length - 1; i++) {
+      let swapped = false;
+
       for (let j = 0; j < arr.length - 1 - i; j++) {
         if (compareFunction) {
           if (compareFunction(arr[j], arr[j + 1]) > 0) {
             [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            swapped = true;
           }
         } else {
           if (String(arr[j]) > String(arr[j + 1])) {
             [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            swapped = true;
           }
         }
+      }
+
+      if (!swapped) {
+        break;
       }
     }
 
     return arr;
+  };
+
+  // eslint-disable-next-line no-extend-native
+  Array.prototype.soft = function (compareFunction) {
+    return this.sort2(compareFunction);
   };
 }
 
