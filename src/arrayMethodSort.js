0a45974c45c5072c1e39796bbe6bf30fe8f61dc7
Olena Dolna <dolnaolena2019@gmail.com>
2025-12-08 15:17:02 +0100

    counter added

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -17,18 +17,25 @@ function applyCustomSort() {
     }
 
     for (let j = 0; j < this.length; j++) {
+      let swapCounter = 0;
+
       for (let i = 0; i < this.length - 1; i++) {
         const compare = callback(this[i], this[i + 1]);
 
-        const shouldSwap =
-          typeof compare === 'number' ? compare > 0 : compare === true;
+        const shouldSwap = typeof compare === 'number' ? compare > 0 : compare;
 
         if (shouldSwap) {
+          swapCounter++;
+
           const temp = this[i];
 
           this[i] = this[i + 1];
           this[i + 1] = temp;
         }
+      }
+
+      if (swapCounter === 0) {
+        return this;
       }
     }
 
