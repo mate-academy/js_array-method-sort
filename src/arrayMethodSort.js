06e638b6fa9dec0070420d0ea3e865c83c273228
Wioleta Lip <wiolip@hotmail.com>
2025-12-27 20:29:10 +0100

    initial commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,30 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const arr = this; // "this" wskazuje na tablicę
+
+    // Jeśli nie podano compareFunction, używamy domyślnego
+    const compare = compareFunction || function(a, b) {
+      // domyślnie sortujemy elementy jako stringi
+      a = String(a);
+      b = String(b);
+      return a < b ? -1 : a > b ? 1 : 0;
+    };
+
+    // Proste sortowanie bąbelkowe
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - 1 - i; j++) {
+        if (compare(arr[j], arr[j + 1]) > 0) {
+          // zamiana miejscami
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+
+    return this; // sort() zwraca tablicę
+
   };
 }
 
