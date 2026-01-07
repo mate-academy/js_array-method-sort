ddae7766e605754f8ba99bdbcdf01e62bae3b283
Nazar <morhunnaza200516@gmail.com>
2026-01-07 19:54:30 +0200

    complate

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,31 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFn) {
+    const arr = this;
+    const n = arr.length;
+    const cmp =
+      compareFn ||
+      function (a, b) {
+        return a.toString() > b.toString()
+          ? 1
+          : a.toString() < b.toString()
+            ? -1
+            : 0;
+      };
+
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - 1 - i; j++) {
+        if (cmp(arr[j], arr[j + 1]) > 0) {
+          const temp = arr[j];
+
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
