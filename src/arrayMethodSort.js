f9f60022aeeedc192e83b7f20a2ef77b4f5927e3
Yurii Buriak <yuriiburiak8@gmail.com>
2026-01-11 12:27:57 +0100

    Implement custom Array.prototype.sort (sort2)

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,41 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    let compare = compareFunction;
+
+    if (typeof compare !== 'function') {
+      compare = (a, b) => {
+        const A = String(a);
+        const B = String(b);
+
+        if (A > B) {
+          return 1;
+        }
+
+        if (A < B) {
+          return -1;
+        }
+
+        return 0;
+      };
+    }
+
+    const arr = this;
+    const length = arr.length;
+
+    for (let i = 0; i < length - 1; i++) {
+      for (let j = 0; j < length - 1 - i; j++) {
+        if (compare(arr[j], arr[j + 1]) > 0) {
+          const temp = arr[j];
+
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
