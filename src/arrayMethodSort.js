9251faaa8f83daadf3626e763f30a2e791a65e23
Angela <angelastrechen1@mail.com>
2026-01-13 23:02:01 +0100

    Fix sort2 implementation

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,10 +4,27 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
-    return Array.prototype.sort.call(this, compareFunction);
+  [].__proto__.sort2 = function (compareFn) {
+    const arr = this;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = 0; j < arr.length - 1 - i; j++) {
+        const a = arr[j];
+        const b = arr[j + 1];
+
+        const shouldSwap = compareFn
+          ? compareFn(a, b) > 0
+          : String(a) > String(b);
+
+        if (shouldSwap) {
+          arr[j] = b;
+          arr[j + 1] = a;
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
-module.exports = applyCustomSort;
 module.exports = applyCustomSort;
