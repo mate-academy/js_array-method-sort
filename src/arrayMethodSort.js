6e9935bf144145c574dd9fcc2a86843672c9d01c
Heorhii Minesko <heorhii.minesko@gmail.com>
2026-01-25 11:31:47 +0200

    task solved

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,43 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    let sortFunction = compareFunction;
+    const records = this.map((item, i) => {
+      return { val: item, key: String(item), ind: i };
+    });
+
+    const defaultComparator = (a, b) => {
+      if (a > b) {
+        return 1;
+      }
+
+      if (a < b) {
+        return -1;
+      }
+
+      return 0;
+    };
+
+    if (sortFunction === undefined) {
+      sortFunction = defaultComparator;
+    }
+
+    for (let i = 0; i < records.length; i++) {
+      for (let j = 0; j < records.length - 1 - i; j++) {
+        const result = sortFunction(records[j].key, records[j + 1].key);
+
+        if (result > 0) {
+          [records[j], records[j + 1]] = [records[j + 1], records[j]];
+        }
+      }
+    }
+
+    for (let i = 0; i < records.length; i++) {
+      this[i] = records[i].val;
+    }
+
+    return this;
   };
 }
 
