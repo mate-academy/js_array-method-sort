891bae2861043f86b0de4ae13fee0c3bb1b2ab99
Serhii Shimko <shimkosergij2010@gmail.com>
2026-01-31 12:51:50 +0200

    completed the task

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,24 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    // const newArray = [];
+    let compair = compareFunction;
+
+    if (typeof compair !== 'function') {
+      compair = (a, b) =>
+        String(a) > String(b) ? 1 : String(a) < String(b) ? -1 : 0;
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compair(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
