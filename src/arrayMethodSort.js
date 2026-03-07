1dc987ee1d08a2db54f75b96442bbd9dbe77682a
Banderos14 <anton.shyshenko@gmail.com>
2026-03-07 12:36:17 +0100

    feat: task_solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,41 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  Array.prototype.sort2 = function (compareFunction) {
+    const compare =
+      typeof compareFunction === 'function'
+        ? compareFunction
+        : (a, b) => {
+            const left = String(a);
+            const right = String(b);
+
+            if (left > right) {
+              return 1;
+            }
+
+            if (left < right) {
+              return -1;
+            }
+
+            return 0;
+          };
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        const a = this[j];
+        const b = this[j + 1];
+
+        // Вызываем колбэк сравнения
+        // Если результат > 0, значит 'a' должно идти после 'b'
+        if (compare(a, b) > 0) {
+          // Меняем элементы местами (деструктуризация)
+          this[j] = b;
+          this[j + 1] = a;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
