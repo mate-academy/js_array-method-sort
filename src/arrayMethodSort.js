5e703ab04c5472f4f13b3ecab6f1dae6a23093fb
kateryna Habchak <kvizovapidtrimka@gmail.com>
2026-03-10 09:16:32 +0100

    add changes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,18 +4,17 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function (compareFunction) {
     const arr = this;
 
-     const compare = compareFunction || ((a, b) =>
-    String(a) > String(b) ? 1 : -1
-    );
+    const compare =
+      compareFunction || ((a, b) => (String(a) > String(b) ? 1 : -1));
 
     for (let i = 0; i < arr.length; i++) {
       for (let j = 0; j < arr.length - 1 - i; j++) {
-        if (compare(arr[j], err[j+1]) > 0) {
-
+        if (compare(arr[j], arr[j + 1]) > 0) {
           const temp = arr[j];
+
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
         }
