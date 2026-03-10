c22ccc8f4018f5855ced2436c24ca56afb578db2
kateryna Habchak <kvizovapidtrimka@gmail.com>
2026-03-10 09:06:59 +0100

    add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,24 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const arr = this;
+
+     const compare = compareFunction || ((a, b) =>
+    String(a) > String(b) ? 1 : -1
+    );
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - 1 - i; j++) {
+        if (compare(arr[j], err[j+1]) > 0) {
+
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
