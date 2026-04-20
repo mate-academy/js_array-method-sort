e3c917a3d2335407edc804a43735d6065fa116ad
Marichka <maria_volynchuk@ukr.net>
2026-04-20 20:16:16 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,43 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    let shouldSwap = false;
+    let temp = 0;
+
+    if (compareFunction === undefined) {
+      for (let i = 1; i < this.length; i++) {
+        for (let o = 0; o < this.length - 1; o++) {
+          if (String(this[o]) > String(this[o + 1])) {
+            shouldSwap = true;
+
+            if (shouldSwap) {
+              temp = this[o];
+              this[o] = this[o + 1];
+              this[o + 1] = temp;
+            }
+            shouldSwap = false;
+          }
+        }
+      }
+    } else {
+      for (let i = 1; i < this.length; i++) {
+        for (let o = 0; o < this.length - 1; o++) {
+          if (compareFunction(this[o], this[o + 1]) > 0) {
+            shouldSwap = true;
+
+            if (shouldSwap) {
+              temp = this[o];
+              this[o] = this[o + 1];
+              this[o + 1] = temp;
+            }
+            shouldSwap = false;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
