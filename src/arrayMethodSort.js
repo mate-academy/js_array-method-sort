21d68ca276364a08ba500e6f962ce74c06e1638e
Postypalska <galina13012005@gmail.com>
2026-04-24 13:46:06 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,41 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const arr = this;
+
+    // якщо функції немає — стандартна поведінка (як строки)
+    const compare =
+      compareFunction ||
+      function (a, b) {
+        const strA = String(a);
+        const strB = String(b);
+
+        if (strA > strB) {
+          return 1;
+        }
+
+        if (strA < strB) {
+          return -1;
+        }
+
+        return 0;
+      };
+
+    // bubble sort
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - 1 - i; j++) {
+        if (compare(arr[j], arr[j + 1]) > 0) {
+          // обмін місцями
+          const temp = arr[j];
+
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
