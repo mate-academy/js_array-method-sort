639a5e0d8e11be1e20d988a5ea5ab29f9070d1f3
VoloGor <goralvv@gmail.com>
2026-05-06 19:35:01 +0200

    Custom implementation of Array.prototype.sort()

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,34 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function (compareFunction) {
     // write code here
+    const arr = this;
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (compareFunction) {
+          if (compareFunction(arr[i], arr[j]) > 0) {
+            const temp = arr[i];
+
+            arr[i] = arr[j];
+            arr[j] = temp;
+          }
+        } else {
+          const strA = String(arr[i]);
+          const strB = String(arr[j]);
+
+          if (strA > strB) {
+            const temp = arr[i];
+
+            arr[i] = arr[j];
+            arr[j] = temp;
+          }
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
