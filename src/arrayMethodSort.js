92f8350f07c7d9400896f2b28fef5079eaaa7991
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-07-11 18:38:41 -0500

    Solve arrayMethodSort.js

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,38 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    if (this.length < 2) {
+      return this;
+    }
+
+    for (let idxOuter = 0; idxOuter < this.length; idxOuter++) {
+      for (let idxInner = 1; idxInner < this.length; idxInner++) {
+        const previousEl = this[idxInner - 1];
+        const currentEl = this[idxInner];
+        let resCompareFunction = 1;
+
+        if (compareFunction) {
+          resCompareFunction = compareFunction(previousEl, currentEl);
+        }
+
+        if (resCompareFunction <= 0) {
+          continue;
+        }
+
+        if (
+          compareFunction === undefined &&
+          previousEl.toString() <= currentEl.toString()
+        ) {
+          continue;
+        }
+
+        this[idxInner - 1] = currentEl;
+        this[idxInner] = previousEl;
+      }
+    }
+
+    return this;
   };
 }
 
