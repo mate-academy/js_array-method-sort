977d081e116d4d699342ba346866bb4c9f5e6ee6
Alexander Okhotnychenko <shashaohota@gmail.com>
2026-07-21 11:11:48 +0300

    sodution: array method sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,34 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction = '') {
+    const length = this.length;
+
+    for (let index = 0; index < length; index++) {
+      for (let j = 0; j < length - index - 1; j++) {
+        if (typeof compareFunction === 'function') {
+          const result = compareFunction(this[j], this[j + 1]);
+
+          if (result > 0) {
+            const temp = this[j];
+
+            this[j] = this[j + 1];
+            this[j + 1] = temp;
+          }
+        } else {
+          if (String(this[j]) > String(this[j + 1])) {
+            const temp = this[j];
+
+            this[j] = this[j + 1];
+            this[j + 1] = temp;
+          }
+        }
+      }
+    }
+
+    return this;
   };
 }
 
+applyCustomSort();
 module.exports = applyCustomSort;
