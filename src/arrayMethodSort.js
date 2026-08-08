e25dbf910c3f081264cbce05cd7134678a563436
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-08 15:58:03 +0300

    Fixed issues

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,31 +6,24 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
     for (let i = 0; i < this.length; i++) {
-      let min = this[i];
-      let middleValue = min;
+      let minIndex = i;
 
       for (let j = i + 1; j < this.length; j++) {
         if (!compareFunction) {
-          if (String(this[j]) < String(min)) {
-            min = this[j];
-            this[j] = middleValue;
-            middleValue = min;
+          if (String(this[j]) < String(this[minIndex])) {
+            minIndex = j;
           }
-        } else if (
-          typeof this[j] === 'string' &&
-          compareFunction(this[j].toLowerCase(), min.toLowerCase()) < 0
-        ) {
-          min = this[j];
-          this[j] = middleValue;
-          middleValue = min;
-        } else if (compareFunction(this[j], min) < 0) {
-          min = this[j];
-          this[j] = middleValue;
-          middleValue = min;
+        } else if (compareFunction(this[j], this[minIndex]) < 0) {
+          minIndex = j;
         }
       }
 
-      this[i] = min;
+      if (minIndex !== i) {
+        let temp = this[i];
+
+        this[i] = this[minIndex];
+        this[minIndex] = temp;
+      }
     }
 
     return this;
