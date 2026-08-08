9ebd418d88130283df52a649b640941ff84cc4e0
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-08 14:52:08 +0300

    Done

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,40 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction = () => undefined) {
+    for (let i = 0; i < this.length; i++) {
+      let min = this[i];
+      let middleValue = min;
+
+      for (let j = i + 1; j < this.length; j++) {
+        if (
+          String(this[j]) < String(min) &&
+          compareFunction(this[j], min) === undefined
+        ) {
+          min = this[j];
+          this[j] = middleValue;
+          middleValue = min;
+        } else if (
+          typeof this[j] === 'string' &&
+          compareFunction(min.toLowerCase(), this[j].toLowerCase()) === 1
+        ) {
+          min = this[j];
+          this[j] = middleValue;
+          middleValue = min;
+        } else if (
+          typeof this[j] !== 'string' &&
+          compareFunction(this[j], min) <= 1
+        ) {
+          min = this[j];
+          this[j] = middleValue;
+          middleValue = min;
+        }
+      }
+
+      this[i] = min;
+    }
+
+    return this;
   };
 }
 
