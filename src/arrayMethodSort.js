7de8b37c60ed2061deb70d32a1bd4251afa90d4f
Gregory Lyon <ggylyon@gmail.com>
2026-08-11 14:31:15 +0300

    Added the solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,22 +6,22 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
     for (let i = 0; i < this.length; i++) {
-      for (let j = 0; j < this.length; j++) {
-        const tempI = this[i];
-        const tempJ = this[j];
+      for (let j = 0; j < this.length - 1; j++) {
+        const tempFirst = this[j];
+        const tempSecond = this[j + 1];
 
         if (!compareFunction) {
-          if (String(tempI) < String(tempJ)) {
-            this[i] = tempJ;
-            this[j] = tempI;
+          if (String(tempFirst) > String(tempSecond)) {
+            this[j] = tempSecond;
+            this[j + 1] = tempFirst;
           }
-
-          continue;
         }
 
-        if (compareFunction(tempI, tempJ) < 0) {
-          this[i] = tempJ;
-          this[j] = tempI;
+        if (compareFunction) {
+          if (compareFunction(tempFirst, tempSecond) > 0) {
+            this[j] = tempSecond;
+            this[j + 1] = tempFirst;
+          }
         }
       }
     }
