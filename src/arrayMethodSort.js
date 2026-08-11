c2304b89cc41a4b0805f2963ee0566442d08db83
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2026-08-11 12:17:04 +0300

    Completed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -7,7 +7,7 @@ function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
-        if (compareFunction(this[i], this[j] > 0)) {
+        if (compareFunction(this[i], this[j]) > 0) {
           const a = this[i];
 
           this[i] = this[j];
