499746799c1a2f2eff22a10f72eca7c6c48e9c15
HiljSV <hiljsv@gmail.com>
2026-08-12 23:50:32 +0300

    Custom sort implementation completed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,24 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const compareAsStrings = (first, second) =>
+      String(first) > String(second) ? 1 : -1;
+    const compare = compareFunction || compareAsStrings;
+
+    for (let pass = 0; pass < this.length - 1; pass++) {
+      for (let index = 0; index < this.length - 1 - pass; index++) {
+        const current = this[index];
+        const next = this[index + 1];
+
+        if (compare(current, next) > 0) {
+          this[index] = next;
+          this[index + 1] = current;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
