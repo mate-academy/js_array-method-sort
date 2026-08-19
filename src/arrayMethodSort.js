d5d4a227a6f4155b3711490e5f3af71b61df9550
Vladimir Kolosov <12vova1212ukr@gmail.com>
2026-08-19 22:23:58 +0300

    the task is completed, the tests are passed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,38 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    let compare = compareFunction;
+
+    if (compare === undefined) {
+      compare = (a, b) => {
+        const aString = String(a);
+        const bString = String(b);
+
+        if (aString < bString) {
+          return -1;
+        }
+
+        if (aString > bString) {
+          return 1;
+        }
+
+        return 0;
+      };
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - 1; j++) {
+        if (compare(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
