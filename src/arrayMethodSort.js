15f5bcec413552abad8943e3c75b21d9a03c6e20
KolodnytskaV <vikakolodnytska@gmail.com>
2026-09-10 10:38:29 +0200

    solution2

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,6 +5,21 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
+    const cmp = compareFunction || function(a, b) {
+      const sa = String(a);
+      const sb = String(b);
+
+      if (sa > sb) {
+        return 1;
+      }
+
+      if (sa < sb){
+        return -1;
+      }
+
+      return 0;
+    }
+
     for (let i = 0;i < this.length - 1; i++) {
       for (let j = 0; j < this.length; j++) {
         if (compareFunction(this[j], this[j + 1]) > 0) {
