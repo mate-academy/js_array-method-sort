24182df70656703fae2d065aa72d920e7560021a
KolodnytskaV <vikakolodnytska@gmail.com>
2026-09-10 10:43:07 +0200

    changes

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -21,7 +21,7 @@ function applyCustomSort() {
     }
 
     for (let i = 0;i < this.length - 1; i++) {
-      for (let j = 0; j < this.length; j++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
         if (compareFunction(this[j], this[j + 1]) > 0) {
           const temp = this[j];
           this[j] = this[j + 1];
